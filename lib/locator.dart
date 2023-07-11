1f98095f3b455ca952948bb4b345a1b0cbd7eefe
mocodesmo <jmorteza@hotmail.com>
2023-07-11 23:16:23 +0530

    bump release version

--- lib/locator.dart
+++ lib/locator.dart
@@ -21,7 +21,7 @@ import 'package:bb_mobile/settings/bloc/settings_cubit.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
-const bbVersion = '0.1.3';
+const bbVersion = '0.1.4';
 
 GetIt locator = GetIt.instance;
 
