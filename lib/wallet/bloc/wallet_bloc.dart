82f5b4b26596c44edac93ccd48c151a7163e91a7
mocodesmo <jmorteza@hotmail.com>
2023-10-14 15:58:15 +0530

    walletbloc fix

--- lib/wallet/bloc/wallet_bloc.dart
+++ lib/wallet/bloc/wallet_bloc.dart
@@ -400,14 +400,13 @@ class WalletBloc extends Bloc<WalletEvent, WalletState> {
                 storageWallet = storageWallet.copyWith(
                   lastBackupTested: eventWallet.lastBackupTested,
                 );
-              final err = await walletRepository.updateWallet(
-                wallet: storageWallet,
-                hiveStore: hiveStorage,
-              );
-              if (err != null) locator<Logger>().log(err.toString());
           }
+        final err = await walletRepository.updateWallet(
+          wallet: storageWallet!,
+          hiveStore: hiveStorage,
+        );
+        if (err != null) locator<Logger>().log(err.toString());
       }
-
       emit(state.copyWith(wallet: event.wallet));
     }
   }
