a22f470a9ee9bcdc99ab96750d92268a717d8c54
i5hi <vishalmenon.92@gmail.com>
2023-10-31 17:51:15 +0530

    Updated to 0.1.92 in locator

--- lib/locator.dart
+++ lib/locator.dart
@@ -29,7 +29,7 @@ import 'package:bb_mobile/settings/bloc/settings_cubit.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
-const bbVersion = '0.1.91';
+const bbVersion = '0.1.92';
 
 GetIt locator = GetIt.instance;
 
