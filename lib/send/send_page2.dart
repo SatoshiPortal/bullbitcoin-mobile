b4155607664c01632ad977fe45be8405ea808b8a
mocodesmo <jmorteza@hotmail.com>
2024-01-18 15:09:06 +0530

    init widgets for new send flow

--- lib/send/send_page2.dart
+++ lib/send/send_page2.dart
@@ -8,3 +8,75 @@ class SendPage2 extends StatelessWidget {
     return const Scaffold(body: Placeholder());
   }
 }
+
+class TopBar extends StatelessWidget {
+  const TopBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class WalletSelectionDropDown extends StatelessWidget {
+  const WalletSelectionDropDown({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class AddressField extends StatelessWidget {
+  const AddressField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class AmountField extends StatelessWidget {
+  const AmountField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class NetworkFees extends StatelessWidget {
+  const NetworkFees({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class SendButton extends StatelessWidget {
+  const SendButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class HighFeeWarning extends StatelessWidget {
+  const HighFeeWarning({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class PaymentSent extends StatelessWidget {
+  const PaymentSent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
