dcf8f09f90a257ae709850facd3435a025872251
mocodesmo <jmorteza@hotmail.com>
2024-01-20 18:13:33 +0530

    send - new wallets dropdown widget

--- lib/send/send_page2.dart
+++ lib/send/send_page2.dart
@@ -1,11 +1,122 @@
+import 'package:bb_mobile/_pkg/barcode.dart';
+import 'package:bb_mobile/_pkg/bull_bitcoin_api.dart';
+import 'package:bb_mobile/_pkg/file_storage.dart';
+import 'package:bb_mobile/_pkg/mempool_api.dart';
+import 'package:bb_mobile/_pkg/storage/hive.dart';
+import 'package:bb_mobile/_pkg/storage/secure_storage.dart';
+import 'package:bb_mobile/_pkg/wallet/address.dart';
+import 'package:bb_mobile/_pkg/wallet/create.dart';
+import 'package:bb_mobile/_pkg/wallet/repository.dart';
+import 'package:bb_mobile/_pkg/wallet/sensitive/create.dart';
+import 'package:bb_mobile/_pkg/wallet/sensitive/repository.dart';
+import 'package:bb_mobile/_pkg/wallet/sensitive/transaction.dart';
+import 'package:bb_mobile/_pkg/wallet/transaction.dart';
+import 'package:bb_mobile/_ui/app_bar.dart';
+import 'package:bb_mobile/_ui/components/text.dart';
+import 'package:bb_mobile/currency/bloc/currency_cubit.dart';
+import 'package:bb_mobile/home/bloc/home_cubit.dart';
+import 'package:bb_mobile/locator.dart';
+import 'package:bb_mobile/network/bloc/network_cubit.dart';
+import 'package:bb_mobile/network_fees/bloc/network_fees_cubit.dart';
+import 'package:bb_mobile/send/bloc/send_cubit.dart';
+import 'package:bb_mobile/send/send_page.dart';
+import 'package:bb_mobile/settings/bloc/settings_cubit.dart';
+import 'package:bb_mobile/styles.dart';
+import 'package:bb_mobile/wallet/bloc/wallet_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
 
 class SendPage2 extends StatelessWidget {
   const SendPage2({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Placeholder());
+    final send = SendCubit(
+      hiveStorage: locator<HiveStorage>(),
+      secureStorage: locator<SecureStorage>(),
+      walletAddress: locator<WalletAddress>(),
+      walletTx: locator<WalletTx>(),
+      walletSensTx: locator<WalletSensitiveTx>(),
+      walletCreate: locator<WalletCreate>(),
+      walletSensCreate: locator<WalletSensitiveCreate>(),
+      barcode: locator<Barcode>(),
+      settingsCubit: locator<SettingsCubit>(),
+      bullBitcoinAPI: locator<BullBitcoinAPI>(),
+      mempoolAPI: locator<MempoolAPI>(),
+      fileStorage: locator<FileStorage>(),
+      walletRepository: locator<WalletRepository>(),
+      walletSensRepository: locator<WalletSensitiveRepository>(),
+      networkCubit: locator<NetworkCubit>(),
+      networkFeesCubit: NetworkFeesCubit(
+        hiveStorage: locator<HiveStorage>(),
+        mempoolAPI: locator<MempoolAPI>(),
+        networkCubit: locator<NetworkCubit>(),
+        defaultNetworkFeesCubit: context.read<NetworkFeesCubit>(),
+      ),
+      currencyCubit: CurrencyCubit(
+        hiveStorage: locator<HiveStorage>(),
+        bbAPI: locator<BullBitcoinAPI>(),
+        defaultCurrencyCubit: context.read<CurrencyCubit>(),
+      ),
+    );
+
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider.value(value: send),
+        BlocProvider.value(value: send.currencyCubit),
+        BlocProvider.value(value: send.networkFeesCubit),
+        BlocProvider.value(value: locator<HomeCubit>()),
+      ],
+      child: Scaffold(
+        appBar: AppBar(
+          flexibleSpace: const TopBar(),
+          automaticallyImplyLeading: false,
+        ),
+        body: const _WalletProvider(child: _Screen()),
+      ),
+    );
+  }
+}
+
+class _WalletProvider extends StatelessWidget {
+  const _WalletProvider({required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final send = context.select((SendCubit _) => _.state.selectedWalletBloc);
+
+    if (send == null) return child;
+    return BlocProvider.value(value: send, child: child);
+  }
+}
+
+class _Screen extends StatelessWidget {
+  const _Screen();
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Gap(24),
+            WalletSelectionDropDown(),
+            Gap(16),
+            AddressField(),
+            Gap(16),
+            AmountField(),
+            // NetworkFees(),
+            // SendButton(),
+          ],
+        ),
+      ),
+    );
   }
 }
 
@@ -14,7 +125,12 @@ class TopBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BBAppBar(
+      text: 'Send Bitcoin',
+      onBack: () {
+        context.pop();
+      },
+    );
   }
 }
 
@@ -23,7 +139,73 @@ class WalletSelectionDropDown extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final network = context.select((NetworkCubit _) => _.state.getBBNetwork());
+    final walletBlocs = context.select((HomeCubit _) => _.state.walletBlocsFromNetwork(network));
+    final selectedWalletBloc = context.select((SendCubit _) => _.state.selectedWalletBloc);
+
+    final walletBloc = selectedWalletBloc ?? walletBlocs.first;
+
+    return Center(
+      child: SizedBox(
+        width: 250,
+        height: 45,
+        child: Material(
+          elevation: 2,
+          clipBehavior: Clip.antiAlias,
+          borderRadius: BorderRadius.circular(4),
+          child: DropdownButtonFormField<WalletBloc>(
+            padding: EdgeInsets.zero,
+            elevation: 4,
+            borderRadius: BorderRadius.circular(4),
+            alignment: Alignment.center,
+            isExpanded: true,
+            decoration: InputDecoration(
+              // alignLabelWithHint: false,
+              enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: NewColours.lightGray,
+                ),
+                borderRadius: BorderRadius.circular(4),
+              ),
+              border: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: NewColours.lightGray,
+                ),
+                borderRadius: BorderRadius.circular(4),
+              ),
+              filled: true,
+              fillColor: NewColours.offWhite,
+              focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: NewColours.lightGray,
+                ),
+                borderRadius: BorderRadius.circular(4),
+              ),
+              contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+              floatingLabelAlignment: FloatingLabelAlignment.center,
+            ),
+            value: walletBloc,
+            onChanged: (value) {
+              if (value == null) return;
+              context.read<SendCubit>().updateSelectedWalletBloc(value);
+            },
+            items: walletBlocs.map((wallet) {
+              final name = wallet.state.wallet!.name ?? wallet.state.wallet!.sourceFingerprint;
+              return DropdownMenuItem<WalletBloc>(
+                value: wallet,
+                alignment: Alignment.center,
+                child: Center(
+                  child: BBText.body(
+                    name,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+    );
   }
 }
 
@@ -32,7 +214,7 @@ class AddressField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const EnterAddress();
   }
 }
 
@@ -41,7 +223,7 @@ class AmountField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const AmountEntry();
   }
 }
 
