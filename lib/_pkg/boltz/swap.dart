dbd09ea9b0c493de034ae5fb48a6ea9949f97f7b
mocodesmo <jmorteza@hotmail.com>
2024-01-31 13:14:39 +0530

    pkg boltz - add all methods

--- lib/_pkg/boltz/swap.dart
+++ lib/_pkg/boltz/swap.dart
@@ -2,6 +2,21 @@ import 'package:bb_mobile/_pkg/error.dart';
 import 'package:boltz_dart/boltz_dart.dart';
 
 class SwapBoltz {
+  static Future<(AllFees?, Err?)> estimateFee({
+    required String boltzUrl,
+    required int outputAmount,
+  }) async {
+    try {
+      final res = await AllSwapFees.estimateFee(
+        boltzUrl: boltzUrl,
+        outputAmount: outputAmount,
+      );
+      return (res, null);
+    } catch (e) {
+      return (null, Err(e.toString()));
+    }
+  }
+
   Future<(BtcLnSwap?, Err?)> swap({
     required String mnemonic,
     required int index,
@@ -15,6 +30,29 @@ class SwapBoltz {
         mnemonic: mnemonic,
         index: index,
         invoice: invoice,
+        network: network,
+        electrumUrl: electrumUrl,
+        boltzUrl: boltzUrl,
+      );
+      return (res, null);
+    } catch (e) {
+      return (null, Err(e.toString()));
+    }
+  }
+
+  Future<(BtcLnSwap?, Err?)> reverse({
+    required String mnemonic,
+    required int index,
+    required int outAmount,
+    required Chain network,
+    required String electrumUrl,
+    required String boltzUrl,
+  }) async {
+    try {
+      final res = await BtcLnSwap.newReverse(
+        mnemonic: mnemonic,
+        index: index,
+        outAmount: outAmount,
         network: network,
         electrumUrl: electrumUrl,
         boltzUrl: boltzUrl,
