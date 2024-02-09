b2ef5eb797fbb55f114ec035b498d506fd847b84
mocodesmo <jmorteza@hotmail.com>
2024-02-09 20:42:47 +0530

    setup stream logic for swap status

--- lib/_pkg/boltz/swap.dart
+++ lib/_pkg/boltz/swap.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
 import 'package:bb_mobile/_model/transaction.dart';
 import 'package:bb_mobile/_pkg/error.dart';
 import 'package:boltz_dart/boltz_dart.dart';
 
 class SwapBoltz {
+  final List<(String, StreamSubscription)> _subscriptions = [];
+
   static Future<(AllFees?, Err?)> getFeesAndLimits({
     required String boltzUrl,
     required int outAmount,
@@ -71,20 +75,49 @@ class SwapBoltz {
 
   Future<Err?> watchSwap({
     required String swapId,
-    required Function(SwapStatus) onConfirmed,
+    required Function({
+      required String id,
+      required SwapStatus status,
+    }) onConfirmed,
   }) async {
     try {
       final api = await BoltzApi.newBoltzApi();
-      // final status = api.getSwapStatusStream(swapId);
-      //   .getSwapStatusStream(swapId);
-      // BoltzApi().getSwapStatusStream(swapId).listen((event) {
-      //   if (event.status == SwapStatus.confirmed) {
-      //     onConfirmed(true);
-      //   }
-      // });
+      final exists = _subscriptions.any((element) => element.$1 == swapId);
+      if (exists) throw 'Already watching swap $swapId';
+
+      _subscriptions.add(
+        (
+          swapId,
+          api.getSwapStatusStream(swapId).listen((event) {
+            onConfirmed(id: swapId, status: event.status);
+          })
+        ),
+      );
+
       return null;
     } catch (e) {
       return Err(e.toString());
     }
+  }
+
+  Err? closeStream(String id) {
+    try {
+      final exists = _subscriptions.any((element) => element.$1 == id);
+      if (!exists) throw 'No subscription for swap $id';
+
+      final sub = _subscriptions.firstWhere((element) => element.$1 == id).$2;
+      sub.cancel();
+      _subscriptions.removeWhere((element) => element.$1 == id);
+      return null;
+    } catch (e) {
+      return Err(e.toString());
+    }
+  }
+
+  void dispose() {
+    for (final sub in _subscriptions) {
+      sub.$2.cancel();
+    }
+    _subscriptions.clear();
   }
 }
