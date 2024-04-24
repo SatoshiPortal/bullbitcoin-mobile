aa744f7431560121729184db99558964377cd73c
mocodesmo <jmorteza@hotmail.com>
2024-04-24 12:35:17 +0530

    sync fix

--- lib/_pkg/wallet/lwk/sync.dart
+++ lib/_pkg/wallet/lwk/sync.dart
@@ -8,11 +8,14 @@ void _syncLwkIsolate(List<dynamic> args) async {
   final sendPort = args[0] as SendPort;
   final lwkWallet = args[1] as lwk.Wallet;
   final blockChain = args[2] as String;
-
+  print(5);
   try {
     await lwkWallet.sync(electrumUrl: blockChain);
+    print(6);
     sendPort.send(lwkWallet);
+    print(7);
   } catch (e) {
+    print('8' + e.toString());
     sendPort.send(
       Err(
         e.toString(),
@@ -33,10 +36,13 @@ class LWKSync {
   }) async {
     try {
       final completer = Completer<(lwk.Wallet?, Err?)>();
+      print(00);
       _receivePort = ReceivePort();
+      print(01);
       _isolate =
           await Isolate.spawn(_syncLwkIsolate, [_receivePort!.sendPort, lwkWallet, blockChain]);
 
+      print(02);
       _receivePort!.listen((message) {
         if (message is lwk.Wallet) {
           completer.complete((message, null));
@@ -82,3 +88,6 @@ class LWKSync {
     _receivePort?.close();
   }
 }
+
+
+// Illegal argument in isolate message: object is unsendable - Library:'package:flutter_rust_bridge/src/rust_arc/_common.dart' Class: RustArc (see restrictions listed at `SendPort.send()` documentation for more information)  <- _kStaticData in Instance of 'MutexLwkWolletWollet' (from package:lwk_dart/src/generated/api/wallet.dart)  <- ptr in Instance of 'Wallet' (from package:lwk_dart/src/generated/api/wallet.dart)  <- _List len:3 (from dart:core)  <- Instance(length:3) of '_GrowableList' (from dart:core)
