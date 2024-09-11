d667ca1ae44a6825ad7701a88d136e7e963b3798
saiy2k <saiy2k@gmail.com>
2024-09-11 12:44:18 +0530

    Chain Swap progress: Text for txnLockupFailed and refund

--- lib/swap/swap_page_progress.dart
+++ lib/swap/swap_page_progress.dart
@@ -5,7 +5,6 @@ import 'package:bb_mobile/_ui/components/text.dart';
 import 'package:bb_mobile/currency/bloc/currency_cubit.dart';
 import 'package:bb_mobile/network/bloc/network_cubit.dart';
 import 'package:bb_mobile/send/bloc/send_cubit.dart';
-import 'package:bb_mobile/styles.dart';
 import 'package:bb_mobile/swap/create_swap_bloc/swap_cubit.dart';
 import 'package:bb_mobile/swap/watcher_bloc/watchtxs_bloc.dart';
 import 'package:bb_mobile/swap/watcher_bloc/watchtxs_state.dart';
@@ -18,9 +17,9 @@ import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 
 class ChainSwapProgressWidget extends StatefulWidget {
-  ChainSwapProgressWidget({super.key, this.isReceive = false});
+  const ChainSwapProgressWidget({super.key, this.isReceive = false});
 
-  bool isReceive;
+  final bool isReceive;
 
   @override
   State<ChainSwapProgressWidget> createState() =>
@@ -67,42 +66,27 @@ class _ChainSwapProgressWidgetState extends State<ChainSwapProgressWidget> {
     if (swapTx?.isChainReceive() == false) {
       tx = context.select((SendCubit _) => _.state.tx);
     }
-    // final amt = fiatAmt.toStringAsFixed(2);
 
     return BlocListener<WatchTxsBloc, WatchTxsState>(
       listenWhen: (previous, current) =>
           current.updatedSwapTx?.id == swapTx?.id,
-      // previous.updatedSwapTx != current.updatedSwapTx &&
-      // current.updatedSwapTx != null,
       listener: (context, state) {
-        // if (swapTx == null) return;
         final updatedSwap = state.updatedSwapTx!;
-        // if (updatedSwap.id != swapTx?.id) return;
         String labelLocal = 'Broadcasting...';
         const bool successLocal = false;
-        const bool failureLocal = false;
+        bool failureLocal = false;
         if (updatedSwap.status?.status == boltz.SwapStatus.swapCreated) {
           labelLocal = 'Broadcasting...';
         } else if (updatedSwap.status?.status == boltz.SwapStatus.txnMempool ||
             updatedSwap.status?.status == boltz.SwapStatus.txnConfirmed ||
             updatedSwap.status?.status == boltz.SwapStatus.txnServerMempool) {
-          // labelLocal = 'Our tx in mempool (1/3)';
           labelLocal = 'Swap created.\nThis will get settled in a while.';
+        } else if (updatedSwap.status?.status ==
+            boltz.SwapStatus.txnLockupFailed) {
+          labelLocal = 'Swap failed.\nInitiating refund';
+          failureLocal = true;
         }
-        // } else if (updatedSwap.status?.status == SwapStatus.txnConfirmed) {
-        //   labelLocal = 'User Lockup Confirmed!';
-        // } else if (updatedSwap.status?.status == SwapStatus.txnServerMempool) {
-        //   labelLocal = 'Server Lockup Broadcasted (2/3)';
-        // } else if (updatedSwap.status?.status ==
-        //     SwapStatus.txnServerConfirmed) {
-        //   labelLocal = 'Claiming...(3/3)';
-        // } else if (updatedSwap.status?.status == SwapStatus.txnClaimed) {
-        //   labelLocal = 'Success';
-        //   successLocal = true;
-        // } else if (updatedSwap.status?.status == SwapStatus.txnLockupFailed) {
-        //   labelLocal = 'Lockup failed. Initiating refund...';
-        //   failureLocal = true;
-        // }
+
         setState(() {
           swapTx = updatedSwap;
           label = labelLocal;
@@ -125,13 +109,6 @@ class _ChainSwapProgressWidgetState extends State<ChainSwapProgressWidget> {
             label,
             textAlign: TextAlign.center,
           ),
-          // if (success) const SendTick(sent: true),
-          //if (refund)
-          //  const FaIcon(
-          //    FontAwesomeIcons.triangleExclamation,
-          //    color: Colors.red,
-          //    size: 50,
-          //  ),
           const Gap(16),
           BBText.body(amtStr),
           const Gap(4),
@@ -148,11 +125,6 @@ class _ChainSwapProgressWidgetState extends State<ChainSwapProgressWidget> {
             ),
           ),
           const Gap(24),
-          // if (swapTx!.claimTxid == null) ...[
-          //   // const Gap(24),
-          //   _OnChainWarning(swapTx: swapTx!),
-          // ],
-          // const Gap(40),
           if (tx != null)
             BBButton.big(
               label: 'View Transaction',
@@ -165,54 +137,6 @@ class _ChainSwapProgressWidgetState extends State<ChainSwapProgressWidget> {
           const Gap(24),
         ],
       ),
-    );
-  }
-}
-
-class _OnChainWarning extends StatelessWidget {
-  const _OnChainWarning({required this.swapTx});
-
-  final SwapTx swapTx;
-
-  @override
-  Widget build(BuildContext context) {
-    // if (swapTx.isLiquid()) return const SizedBox.shrink();
-
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          FontAwesomeIcons.triangleExclamation,
-          color: context.colour.primary,
-          size: 20,
-        ),
-        const Gap(8),
-        if (swapTx.isChainSelf()) ...[
-          const BBText.bodySmall(
-            'This will take a while.',
-            isRed: true,
-            fontSize: 12,
-          ),
-        ] else if (swapTx.isChainReceive()) ...[
-          const SizedBox(
-            width: 250,
-            child: BBText.bodySmall(
-              'Sender has made the onchain payment. It will take onchain confirmation until you can claim. This can take a while',
-              isRed: true,
-              fontSize: 12,
-            ),
-          ),
-        ] else if (swapTx.isChainSend()) ...[
-          const SizedBox(
-            width: 250,
-            child: BBText.bodySmall(
-              'You have made the lockup transaction. It will take onchain confirmation until you can pay the receiver. This can take a while.',
-              isRed: true,
-              fontSize: 12,
-            ),
-          ),
-        ],
-      ],
     );
   }
 }
