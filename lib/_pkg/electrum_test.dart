d0893c4d0594dd614c9d6f8f226dab143f6d2001
saiy2k <saiy2k@gmail.com>
2024-09-12 19:05:29 +0530

    Electrum live test logic

---
+++ lib/_pkg/electrum_test.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+Future<bool> isElectrumLive(String electrumUrl) async {
+  try {
+    final split = electrumUrl.split(':');
+    final port = int.tryParse(split.last) ?? 0;
+    split.removeAt(0); // removes 'ssl'
+    split.removeLast(); // removes the port number
+    final url = split.join(':').split('//').last; // to remove the slashes
+
+    final Completer<bool> completer = Completer();
+
+    final SecureSocket socket = await SecureSocket.connect(url, port);
+    final Map<String, dynamic> request = {
+      'jsonrpc': '2.0',
+      'id': '1',
+      'method': 'server.version',
+      'params': [],
+    };
+    socket.write(jsonEncode(request) + '\n');
+
+    socket.listen((data) {
+      final String response = String.fromCharCodes(data);
+      final Map<String, dynamic> json =
+          jsonDecode(response) as Map<String, dynamic>;
+      if (json['id'] == '1' &&
+          json['jsonrpc'] == '2.0' &&
+          json['result'] is List) {
+        completer.complete(true);
+      } else {
+        completer.complete(false);
+      }
+    });
+
+    final result = await completer.future;
+    socket.close();
+
+    return result;
+  } catch (e) {
+    print('Error: $e');
+    return false;
+  }
+}
