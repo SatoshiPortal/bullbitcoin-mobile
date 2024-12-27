4ee2974c836aa5ff1edff8dd41d3b560e8063f00
mocodesmo <morteza4048@gmail.com>
2024-12-28 00:00:50 +0530

    BARCODE - fixes camera crash if phone has no camera

--- lib/_pkg/barcode.dart
+++ lib/_pkg/barcode.dart
@@ -1,8 +1,21 @@
 import 'package:bb_mobile/_pkg/error.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class Barcode {
+  Future<bool> hasCameraPermission() async {
+    final status = await Permission.camera.status;
+    if (status.isGranted) return true;
+
+    final result = await Permission.camera.request();
+    return result.isGranted;
+  }
+
   Future<(String?, Err?)> scan() async {
+    if (!await hasCameraPermission()) {
+      return (null, Err('Camera permission denied'));
+    }
+
     try {
       final res = await FlutterBarcodeScanner.scanBarcode(
         '#ff6666',
