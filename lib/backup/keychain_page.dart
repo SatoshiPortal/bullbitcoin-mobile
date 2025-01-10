18d23d67ee7a182a523d1f8de90326c1de19981b
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-01-09 19:11:34 -0500

    code cleanup

--- lib/backup/keychain_page.dart
+++ lib/backup/keychain_page.dart
@@ -5,6 +5,7 @@ import 'package:bb_mobile/home/bloc/home_cubit.dart';
 import 'package:bb_mobile/locator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 
 class KeychainBackupPage extends StatelessWidget {
@@ -25,6 +26,7 @@ class KeychainBackupPage extends StatelessWidget {
         backgroundColor: Colors.amber,
         appBar: AppBar(
           automaticallyImplyLeading: false,
+          elevation: 0,
           flexibleSpace: BBAppBar(
             text: 'Keychain Backup',
             onBack: () => context.pop(),
@@ -55,53 +57,59 @@ class KeychainBackupPage extends StatelessWidget {
           child: BlocBuilder<KeychainCubit, KeychainState>(
             builder: (context, state) {
               final cubit = context.read<KeychainCubit>();
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    children: [
-                      SelectableText('Backup Key: $backupKey'),
-                      SelectableText('Backup ID: $backupId'),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      SizedBox(
-                        width: 100,
-                        child: TextFormField(
-                          decoration:
-                              const InputDecoration(labelText: 'Enter PIN'),
-                          keyboardType: TextInputType.number,
-                          maxLength: 6,
-                          onChanged: (value) => cubit.updateSecret(value),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 100,
-                        child: TextFormField(
-                          decoration:
-                              const InputDecoration(labelText: 'Confirm PIN'),
-                          keyboardType: TextInputType.number,
-                          obscureText: true,
-                          maxLength: 6,
-                          onChanged: (value) => cubit.confirmSecret(value),
-                        ),
-                      ),
-                    ],
-                  ),
-                  if (state.secretConfirmed)
-                    ElevatedButton(
-                      onPressed: () =>
-                          cubit.clickSecureKey(backupId, backupKey),
-                      child: const Text('Secure my backup key'),
+              return Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SelectableText('Backup Key: $backupKey'),
+                        Gap(8),
+                        SelectableText('Backup ID: $backupId'),
+                      ],
                     ),
-                  if (!state.secretConfirmed)
-                    const Text(
-                      'PINs do not match! Please confirm your PIN.',
-                      style: TextStyle(color: Colors.red),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                          width: double.infinity,
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Enter PIN',
+                            ),
+                            keyboardType: TextInputType.number,
+                            maxLength: 6,
+                            onChanged: (value) => cubit.updateSecret(value),
+                          ),
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          child: TextFormField(
+                            decoration:
+                                const InputDecoration(labelText: 'Confirm PIN'),
+                            keyboardType: TextInputType.number,
+                            obscureText: true,
+                            maxLength: 6,
+                            onChanged: (value) => cubit.confirmSecret(value),
+                          ),
+                        ),
+                      ],
                     ),
-                ],
+                    if (state.secretConfirmed)
+                      ElevatedButton(
+                        onPressed: () =>
+                            cubit.clickSecureKey(backupId, backupKey),
+                        child: const Text('Secure my backup key'),
+                      ),
+                    if (!state.secretConfirmed)
+                      const Text(
+                        'PINs do not match! Please confirm your PIN.',
+                        style: TextStyle(color: Colors.red),
+                      ),
+                  ],
+                ),
               );
             },
           ),
