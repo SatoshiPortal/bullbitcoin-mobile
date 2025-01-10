93bc3e2b9bb37dffbb42c246c4836ce9497ed691
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-01-09 19:09:22 -0500

    refactor: replaced Gdrive with GoogleDriveStorage for drive functions

--- lib/backup/bloc/cloud_cubit.dart
+++ lib/backup/bloc/cloud_cubit.dart
@@ -1,9 +1,11 @@
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:bb_mobile/_pkg/gdrive.dart';
 import 'package:bb_mobile/backup/bloc/cloud_state.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hex/hex.dart';
+import 'package:recoverbull_dart/recoverbull_dart.dart';
 
 class CloudCubit extends Cubit<CloudState> {
   final String backupPath;
@@ -18,37 +20,38 @@ class CloudCubit extends Cubit<CloudState> {
 
   Future<void> connectAndStoreBackup() async {
     try {
-      final gdrive = await Gdrive.connect();
-
-      if (gdrive != null) {
-        emit(state.copyWith(gdrive: gdrive));
-        print("User has logged in" + "Sign in");
+      final googleSignInAccount = await GoogleDriveApi.google.signIn();
+      final googleDriveStorage = await GoogleDriveStorage.connect(
+        googleSignInAccount,
+        "bullbitcoin/backups",
+      );
+      if (googleSignInAccount != null) {
+        emit(state.copyWith(googleDriveStorage: googleDriveStorage));
+        debugPrint("User has logged in");
       } else {
-        print("User has not logged in" + "Sign in");
+        debugPrint("User has not logged in");
       }
     } catch (e) {
-      print(e);
+      debugPrint("GoogleDriveStorage.connect Error: $e");
     }
 
-    if (state.gdrive == null) {
-      emit(state.copyWith(toast: 'not connected'));
+    if (state.googleDriveStorage == null) {
+      emit(state.copyWith(toast: 'User has not logged in'));
       return;
     }
 
     final backup = File(backupPath);
     final content = await backup.readAsString();
-
-    final bool isCreated = await state.gdrive!.write(
-      filename: backupName,
-      content: json.decode(content) as Map<dynamic, dynamic>,
-    );
+    final decoded = HEX.decode(content);
+    final isCreated =
+        await state.googleDriveStorage?.writeMetaData(decoded, backupName);
 
     if (isCreated == false) {
-      emit(state.copyWith(toast: "Not created"));
+      emit(state.copyWith(toast: "Failed to backup file to google drive."));
     } else {
       emit(state.copyWith(toast: "File created successfully."));
     }
   }
 
-  void disconnect() => Gdrive.disconnect();
+  void disconnect() => GoogleDriveApi.disconnect();
 }
