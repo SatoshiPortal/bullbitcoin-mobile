71d2ae68cb8efc33b3cc36e09ef156d21177c4c5
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-01-21 21:45:37 -0500

    feat(wallet_settings): Implemented methods to create, load and update BIP85 derivation paths

--- lib/wallet_settings/bloc/wallet_settings_cubit.dart
+++ lib/wallet_settings/bloc/wallet_settings_cubit.dart
@@ -491,33 +491,85 @@ class WalletSettingsCubit extends Cubit<WalletSettingsState> {
     }
   }
 
-  Future<void> addNewBIP85BackupKey(String label) async {
+  void loadBIP85Derivations() {
+    emit(state.copyWith(bip85Derivations: _wallet.bip85Derivations));
+  }
+
+  Future<void> createNewBIP85BackupKeyClicked(String label) async {
+    emit(state.copyWith(updatingBip85Derivations: true));
     try {
-      emit(state.copyWith(savingFile: true, errSavingFile: ''));
       final wallet = _wallet;
-      final newBIP85Path = wallet.generateNextBIP85Path();
+      final path = wallet.generateNextBIP85Path();
 
-      //TODO; find a way to get the label from the wallet
-      final updatedWallet = WalletUpdate().updateBIP85Paths(
-        wallet,
-        newBIP85Path,
-        "${label}_bip85Path_${wallet.bip85Derivations.entries.length + 1}",
+      await _appWalletsRepository.getWalletServiceById(wallet.id)?.updateWallet(
+        WalletUpdate().updateBIP85Paths(wallet, path, label),
+        updateTypes: [UpdateWalletTypes.bip85Paths],
       );
+      final updatedWallet = _appWalletsRepository.getWalletById(wallet.id);
+      if (updatedWallet == null) {
+        emit(
+          state.copyWith(
+            errUpdatingBip85Derivations: 'Failed to update the wallet',
+            updatingBip85Derivations: false,
+          ),
+        );
+        return;
+      }
 
-      await _appWalletsRepository
-          .getWalletServiceById(updatedWallet.id)
-          ?.updateWallet(
-        updatedWallet,
-        updateTypes: [
-          UpdateWalletTypes.bip85Paths,
-        ],
+      emit(
+        state.copyWith(
+          bip85Derivations: updatedWallet.bip85Derivations,
+          updatingBip85Derivations: false,
+        ),
       );
-
-      emit(state.copyWith(savingFile: false, savedFile: true));
-      await Future.delayed(2.seconds);
-      emit(state.copyWith(savedFile: false));
     } catch (e) {
-      emit(state.copyWith(errSavingFile: e.toString(), savingFile: false));
+      emit(
+        state.copyWith(
+          errUpdatingBip85Derivations: e.toString(),
+          updatingBip85Derivations: false,
+        ),
+      );
+    }
+  }
+
+  Future<void> updateBIP85LabelClicked(String path, String newLabel) async {
+    emit(
+      state.copyWith(
+        updatingBip85Derivations: true,
+        errUpdatingBip85Derivations: '',
+      ),
+    );
+
+    try {
+      final wallet = _wallet;
+      await _appWalletsRepository.getWalletServiceById(wallet.id)?.updateWallet(
+        WalletUpdate().updateBIP85Paths(wallet, path, newLabel),
+        updateTypes: [UpdateWalletTypes.bip85Paths],
+      );
+
+      final updatedWallet = _appWalletsRepository.getWalletById(wallet.id);
+      if (updatedWallet == null) {
+        emit(
+          state.copyWith(
+            errUpdatingBip85Derivations: 'Failed to update the wallet',
+            updatingBip85Derivations: false,
+          ),
+        );
+        return;
+      }
+      emit(
+        state.copyWith(
+          bip85Derivations: updatedWallet.bip85Derivations,
+          updatingBip85Derivations: false,
+        ),
+      );
+    } catch (e) {
+      emit(
+        state.copyWith(
+          errUpdatingBip85Derivations: e.toString(),
+          updatingBip85Derivations: false,
+        ),
+      );
     }
   }
 
