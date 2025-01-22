2e978ca134d51efab1345a1d1f6f72459cb3e7ae
i5hi <vishalmenon.92@gmail.com>
2025-01-22 20:44:13 +0530

    fixes: various build issues

--- lib/locator.dart
+++ lib/locator.dart
@@ -58,7 +58,7 @@ import 'package:flutter_animate/flutter_animate.dart';
 import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 
-const bbVersion = '0.4.0';
+const bbVersion = '0.4.0-06';
 
 GetIt locator = GetIt.instance;
 
