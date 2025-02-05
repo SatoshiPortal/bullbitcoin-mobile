863033ec4633c65d6c9725436cd537ecc9a1c679
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-02-05 04:23:15 -0500

    refactor(backup): removed unused fields

--- lib/_model/backup.dart
+++ lib/_model/backup.dart
@@ -1,4 +1,3 @@
-import 'package:bb_mobile/_model/bip329_label.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'backup.freezed.dart';
@@ -9,24 +8,15 @@ class Backup with _$Backup {
   const factory Backup({
     @Default(1) int version,
     @Default('') String name,
-    @Default('') String layer,
     @Default('') String network,
-    @Default('') String script,
-    @Default('') String type,
     @Default(<String>[]) List<String> mnemonic,
     @Default('') String passphrase,
     @Default('') String mnemonicFingerPrint,
-    @Default(<Bip329Label>[]) List<Bip329Label> labels,
-    @Default(<String>[]) List<String> descriptors,
   }) = _Backup;
 
   factory Backup.fromJson(Map<String, dynamic> json) => _$BackupFromJson(json);
 
   const Backup._();
 
-  bool get isEmpty =>
-      mnemonic.isEmpty &&
-      passphrase.isEmpty &&
-      labels.isEmpty &&
-      descriptors.isEmpty;
+  bool get isEmpty => mnemonic.isEmpty && passphrase.isEmpty;
 }
