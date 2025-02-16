247c204416cb8596bd59c860f939b458b35a1971
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-02-16 13:46:00 -0500

    feat: check for testnet wallets and toggle network if necessary

--- lib/home/bloc/home_bloc.dart
+++ lib/home/bloc/home_bloc.dart
@@ -1,10 +1,14 @@
 import 'dart:async';
 
+import 'package:bb_mobile/_model/wallet.dart';
 import 'package:bb_mobile/_repository/app_wallets_repository.dart';
 import 'package:bb_mobile/_repository/network_repository.dart';
 import 'package:bb_mobile/_repository/wallet_service.dart';
 import 'package:bb_mobile/home/bloc/home_event.dart';
 import 'package:bb_mobile/home/bloc/home_state.dart';
+import 'package:bb_mobile/locator.dart';
+import 'package:bb_mobile/network/bloc/event.dart';
+import 'package:bb_mobile/network/bloc/network_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -116,8 +120,19 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
     Emitter<HomeState> emit,
   ) async {
     emit(state.copyWith(loadingWallets: true));
+
     await _appWalletsRepository.getWalletsFromStorage();
     final wallets = _appWalletsRepository.allWallets;
+
+    // Check if any wallet is testnet and switch network if needed
+    if (wallets.isNotEmpty) {
+      final hasTestnetWallet = wallets.any((w) => w.isTestnet());
+      final currentNetwork = locator<NetworkBloc>().state.getBBNetwork();
+
+      if (hasTestnetWallet && currentNetwork != BBNetwork.Testnet) {
+        locator<NetworkBloc>().add(ToggleTestnet());
+      }
+    }
     emit(
       state.copyWith(
         wallets: wallets.map((_) => WalletServiceData(wallet: _)).toList(),
