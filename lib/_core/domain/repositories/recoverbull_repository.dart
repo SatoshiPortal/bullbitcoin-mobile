60587fca57141b8d0479e204834ed4c5daa2826e
ethicnology <ethicnology@pm.me>
2025-03-13 10:41:22 -0400

    tmp: validate recoverbull approach to the new architecture

---
+++ lib/_core/domain/repositories/recoverbull_repository.dart
@@ -0,0 +1,25 @@
+abstract class RecoverBullRepository {
+  // Returning BullBackup could be helpful for the frontend that
+  Future<String> createBackupFile(String xprv);
+
+  void restoreBackupFile(String backupFile, String backupKey);
+
+  Future<void> storeBackupKey(
+    String identifier, // encoded as hex in the file
+    String password, // utf8 encoded (argon2 will decode it as utf8)
+    String salt, // encoded as hex in the file
+    String backupKey,
+  );
+
+  Future<String> fetchBackupKey(
+    String identifier,
+    String password,
+    String salt,
+  );
+
+  Future<void> trashBackupKey(
+    String identifier,
+    String password,
+    String salt,
+  );
+}
