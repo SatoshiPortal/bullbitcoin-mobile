4f4beece0fa0496bd608e572faa334a78aa6a716
i5hi <vishalmenon.92@gmail.com>
2025-03-15 19:52:02 +0530

    Bumped to 0.4.3

--- lib/locator.dart
+++ lib/locator.dart
@@ -55,7 +55,7 @@ import 'package:flutter_animate/flutter_animate.dart';
 import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 
-const bbVersion = '0.4.2';
+const bbVersion = '0.4.3';
 
 GetIt locator = GetIt.instance;
 
