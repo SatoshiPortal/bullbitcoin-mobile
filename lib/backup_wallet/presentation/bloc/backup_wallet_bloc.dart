6f927a3cd75191fb6d3970e769ae0bb8f8671421
StaxoLotl <BitcoinStaxolotl@proton.me>
2025-03-17 07:40:43 -0400

    Refactor/recoverbull backup wallet (#530)
    * tmp: validate approach in the new architecture (rebased)
    
    * refactor: enhance TorDataSource with readiness check and async kill method
    
    * feat: register TorDataSource as a singleton with readiness check
    
    * feat: implement TorRepository
    
    * feat: add InitializeTorUseCase and integrate with AppStartupBloc
    
    * feat: register TorRepository with dependency on TorDataSource
    
    * feat: integrate InitializeTorUseCase into AppStartupLocator
    
    * feat: add GetDefaultWalletUseCase and register in CoreLocator
    
    * feat: integrate TorRepository into RecoverBullRemoteDataSourceImpl
    
    * feat: integrate TorRepository for socket management in RecoverBullRemoteDataSourceImpl
    
    * feat: update backup and restore functionality in RecoverBullRepositoryImpl
    
    * feat: rename createBackupFile parameter to masterFingerprint
    
    * feat: replace CreateRecoverBullBackupUsecase with CreateEncryptedBackupUsecase for backup functionality
    
    * feat: add BackupWalletLocator for dependency injection of backup-related use cases and blocs
    
    * feat: implement BackupWalletBloc
    
    * feat: add Bip85 data source and repository
    
    * feat: update createBackupFile method to accept structured parameters
    
    * feat: refactor RecoverBullRepositoryImpl to remove unused data sources
    
    * refactor(RecoverBullRepositoryImpl): removed external data sources
    
    * feat: add backupKey creation logic & exception handling
    
    * feat: register additional repositories in BackupWalletLocator
    
    * feat: add StoreBackupKeyUsecase for storing backup keys
    
    * chore: update package versions and sha256 checksums in pubspec.lock
    
    * refactor: remove CreateRecoverBullBackupUsecase implementation
    
    * refactor: update restoreBackupFile method to use entity models
    
    ---------
    
    Co-authored-by: ethicnology <ethicnology@pm.me>

---
+++ lib/backup_wallet/presentation/bloc/backup_wallet_bloc.dart
@@ -0,0 +1,19 @@
+import 'package:bb_mobile/_core/domain/usecases/get_default_wallet_use_case.dart';
+import 'package:bb_mobile/backup_wallet/domain/usecases/create_encrypted_backup_usecase.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'backup_wallet_event.dart';
+part 'backup_wallet_state.dart';
+part 'backup_wallet_bloc.freezed.dart';
+
+class BackupWalletBloc extends Bloc<BackupWalletEvent, BackupWalletState> {
+  final CreateEncryptedBackupUsecase createEncryptedBackupUsecase;
+  final GetDefaultWalletUseCase getDefaultWalletUseCase;
+  BackupWalletBloc({
+    required this.createEncryptedBackupUsecase,
+    required this.getDefaultWalletUseCase,
+  }) : super(const BackupWalletState()) {
+    on<BackupWalletEvent>((event, emit) {});
+  }
+}
