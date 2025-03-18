20ab20c44aa1fdeef8ec365e5c9213cab9537b08
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-03-17 21:40:44 -0400

    feat: wallet backup functionality for file system and Google Drive

--- lib/backup_wallet/presentation/bloc/backup_wallet_state.dart
+++ lib/backup_wallet/presentation/bloc/backup_wallet_state.dart
@@ -3,9 +3,12 @@ part of 'backup_wallet_bloc.dart';
 @freezed
 sealed class BackupWalletState with _$BackupWalletState {
   const factory BackupWalletState({
+    @Default('') String filePath,
     @Default(false) bool isSubmitting,
-    @Default(false) bool isSuccess,
-    @Default(false) bool isFailure,
     @Default('') String error,
   }) = _BackupWalletState;
+  factory BackupWalletState.error(String error) => BackupWalletState(
+        isSubmitting: false,
+        error: error,
+      );
 }
