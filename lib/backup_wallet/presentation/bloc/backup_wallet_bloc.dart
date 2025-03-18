9486c3507b279812b4f97fe8befcab3b5cdc0878
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-03-17 21:40:44 -0400

    feat: wallet backup functionality for file system and Google Drive

--- lib/backup_wallet/presentation/bloc/backup_wallet_bloc.dart
+++ lib/backup_wallet/presentation/bloc/backup_wallet_bloc.dart
@@ -1,11 +1,16 @@
+import 'dart:async';
+
+import 'package:bb_mobile/_core/domain/usecases/google_drive/connect_google_drive_usecase.dart';
+import 'package:bb_mobile/_core/domain/usecases/google_drive/disconnect_google_drive_usecase.dart';
+import 'package:bb_mobile/_core/domain/usecases/google_drive/fetch_latest_backup_usecase.dart';
 import 'package:bb_mobile/_core/domain/usecases/get_default_wallet_use_case.dart';
 import 'package:bb_mobile/backup_wallet/domain/usecases/create_encrypted_vault_usecase.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+part 'backup_wallet_bloc.freezed.dart';
 part 'backup_wallet_event.dart';
 part 'backup_wallet_state.dart';
-part 'backup_wallet_bloc.freezed.dart';
 
 class BackupWalletBloc extends Bloc<BackupWalletEvent, BackupWalletState> {
   final CreateEncryptedVaultUsecase createEncryptedVaultUsecase;
