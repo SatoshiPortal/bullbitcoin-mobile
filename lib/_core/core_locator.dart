b9a238511cb14dfe4ac34c2b9b086f8922d220cb
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-03-18 14:15:43 -0400

    refactor: rename file & class names to usecase

--- lib/_core/core_locator.dart
+++ lib/_core/core_locator.dart
@@ -54,7 +54,10 @@ import 'package:bb_mobile/_core/domain/usecases/send_with_payjoin_usecase.dart';
 import 'package:bb_mobile/_core/domain/usecases/google_drive/connect_google_drive_usecase.dart';
 import 'package:bb_mobile/_core/domain/usecases/google_drive/disconnect_google_drive_usecase.dart';
 import 'package:bb_mobile/_core/domain/usecases/google_drive/fetch_latest_backup_usecase.dart';
-import 'package:bb_mobile/_core/domain/usecases/pick_file_use_case.dart';
+
+import 'package:bb_mobile/_core/domain/usecases/receive_with_payjoin_use_case.dart';
+import 'package:bb_mobile/_core/domain/usecases/select_file_path_usecase.dart';
+import 'package:bb_mobile/_core/domain/usecases/send_with_payjoin_use_case.dart';
 import 'package:bb_mobile/_utils/constants.dart';
 import 'package:bb_mobile/locator.dart';
 import 'package:bb_mobile/receive/domain/usecases/create_receive_swap_use_case.dart';
@@ -344,6 +347,18 @@ class CoreLocator {
       () => BuildPsbtUsecase(
         payjoinRepository: locator<PayjoinRepository>(),
         walletManagerService: locator<WalletManagerService>(),
+      ),
+    );
+    // Google Drive use cases
+    locator.registerFactory<ConnectToGoogleDriveUsecase>(
+      () => ConnectToGoogleDriveUsecase(locator<GoogleDriveRepository>()),
+    );
+    locator.registerFactory<DisconnectFromGoogleDriveUsecase>(
+      () => DisconnectFromGoogleDriveUsecase(locator<GoogleDriveRepository>()),
+    );
+    locator.registerFactory<FetchLatestBackupUsecase>(
+      () => FetchLatestBackupUsecase(
+        locator<GoogleDriveRepository>(),
       ),
     );
   }
