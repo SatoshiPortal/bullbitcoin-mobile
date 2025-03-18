aee8a8eeef51ff5889ed62bcbfe6afd118e3eed0
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-03-18 13:24:59 -0400

    feat: update backup wallet state with new status and provider types

--- lib/backup_wallet/presentation/bloc/backup_wallet_state.dart
+++ lib/backup_wallet/presentation/bloc/backup_wallet_state.dart
@@ -1,14 +1,29 @@
 part of 'backup_wallet_bloc.dart';
 
 @freezed
+sealed class BackupWalletStatus with _$BackupWalletStatus {
+  const factory BackupWalletStatus.initial() = _Initial;
+  const factory BackupWalletStatus.loading() = _Loading;
+  const factory BackupWalletStatus.success() = _Success;
+  const factory BackupWalletStatus.failure(String? message) = _Failure;
+}
+
+@freezed
+sealed class BackupProvider with _$BackupProvider {
+  const factory BackupProvider.googleDrive() = _GoogleDrive;
+  const factory BackupProvider.iCloud() = _ICloud;
+  const factory BackupProvider.fileSystem(String filePath) = _FileSystem;
+}
+
+@freezed
 sealed class BackupWalletState with _$BackupWalletState {
   const factory BackupWalletState({
-    @Default('') String filePath,
-    @Default(false) bool isSubmitting,
-    @Default('') String error,
+    @Default(BackupProvider.googleDrive()) BackupProvider backupProvider,
+    @Default('') String encryted,
+    @Default(BackupWalletStatus.initial) BackupWalletStatus status,
   }) = _BackupWalletState;
+
   factory BackupWalletState.error(String error) => BackupWalletState(
-        isSubmitting: false,
-        error: error,
+        status: BackupWalletStatus.failure(error),
       );
 }
