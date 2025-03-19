d711aab2792fd195dc948dfb2a4abcfb045c1e8e
ethicnology <ethicnology@pm.me>
2025-03-19 07:43:07 -0400

    feat: store backup key into key server usecase

--- lib/recoverbull/domain/usecases/store_backup_key_into_server_usecase.dart
+++ lib/recoverbull/domain/usecases/store_backup_key_into_server_usecase.dart
@@ -1,4 +1,8 @@
 import 'package:bb_mobile/_core/domain/repositories/recoverbull_repository.dart';
+import 'package:bb_mobile/recoverbull/recoverbull_password_validator.dart';
+import 'package:flutter/foundation.dart';
+import 'package:hex/hex.dart';
+import 'package:recoverbull/recoverbull.dart';
 
 class StoreBackupKeyIntoServerUsecase {
   final RecoverBullRepository _recoverBullRepository;
@@ -10,15 +14,36 @@ class StoreBackupKeyIntoServerUsecase {
   Future<void> execute({
     required String password,
     required String backupFile,
+    required String backupKey,
   }) async {
-    // TODO: implem
-    // TODO: ensure that the backup key can decipher the backup
+    try {
+      // Ensure backupFile has a valid format
+      final isValidBackupFile = BullBackup.isValid(backupFile);
+      if (!isValidBackupFile) throw 'Invalid backup file';
 
-    // return _recoverBullRepository.storeBackupKey(
-    //   identifier,
-    //   password,
-    //   salt,
-    //   backupKey,
-    // );
+      // Ensure backupKey is hex encoded
+      try {
+        HEX.decode(backupKey);
+      } catch (e) {
+        throw '$StoreBackupKeyIntoServerUsecase: backup key should be hex encoded';
+      }
+
+      // Ensure password is not too common
+      if (RecoverBullPasswordValidator.isInCommonPasswordList(password)) {
+        throw '$StoreBackupKeyIntoServerUsecase: password is too common';
+      }
+
+      final bullBackup = BullBackup.fromJson(backupFile);
+
+      await _recoverBullRepository.storeBackupKey(
+        HEX.encode(bullBackup.id),
+        password,
+        HEX.encode(bullBackup.salt),
+        backupKey,
+      );
+    } catch (e) {
+      debugPrint('$StoreBackupKeyIntoServerUsecase: $e');
+      rethrow;
+    }
   }
 }
