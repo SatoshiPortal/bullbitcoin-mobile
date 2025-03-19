be9618ae38f3b42da64b26c24f8d0389e778e37c
ethicnology <ethicnology@pm.me>
2025-03-19 15:17:20 -0400

    feat: RestoreEncryptedVaultFromPasswordUsecase

--- lib/recoverbull/domain/usecases/restore_encrypted_vault_from_password_use_case.dart
+++ lib/recoverbull/domain/usecases/restore_encrypted_vault_from_password_use_case.dart
@@ -1,15 +1,22 @@
 import 'package:bb_mobile/_core/domain/repositories/recoverbull_repository.dart';
+import 'package:bb_mobile/_core/domain/repositories/wallet_metadata_repository.dart';
+import 'package:bb_mobile/_core/domain/services/wallet_manager_service.dart';
+import 'package:bb_mobile/recoverbull/domain/usecases/restore_encrypted_vault_from_backup_key_usecase.dart';
 import 'package:flutter/foundation.dart';
 import 'package:hex/hex.dart';
 import 'package:recoverbull/recoverbull.dart';
 
-// If the key server is up
+/// If the key server is up
 class RestoreEncryptedVaultFromPasswordUsecase {
-  final RecoverBullRepository _recoverBullRepository;
+  final RecoverBullRepository recoverBullRepository;
+  final WalletManagerService walletManagerService;
+  final WalletMetadataRepository walletMetadataRepository;
 
   RestoreEncryptedVaultFromPasswordUsecase({
-    required RecoverBullRepository recoverBullRepository,
-  }) : _recoverBullRepository = recoverBullRepository;
+    required this.recoverBullRepository,
+    required this.walletManagerService,
+    required this.walletMetadataRepository,
+  });
 
   Future<void> execute(String backupFile, String password) async {
     try {
@@ -18,20 +25,19 @@ class RestoreEncryptedVaultFromPasswordUsecase {
 
       final bullBackup = BullBackup.fromJson(backupFile);
 
-      final backupKey = await _recoverBullRepository.fetchBackupKey(
+      final backupKey = await recoverBullRepository.fetchBackupKey(
         HEX.encode(bullBackup.id),
         password,
         HEX.encode(bullBackup.salt),
       );
 
-      final jsonBackup = _recoverBullRepository.restoreBackupFile(
-        backupFile,
-        backupKey,
-      );
-
-      // TODO: import wallets
+      await RestoreEncryptedVaultFromBackupKeyUsecase(
+        recoverBullRepository: recoverBullRepository,
+        walletManagerService: walletManagerService,
+        walletMetadataRepository: walletMetadataRepository,
+      ).execute(backupFile: backupFile, backupKey: backupKey);
     } catch (e) {
-      debugPrint('error creating encrypted backup: $e');
+      debugPrint('$RestoreEncryptedVaultFromPasswordUsecase: $e');
       rethrow;
     }
   }
