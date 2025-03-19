5c74c30edc166fd4433472bbe7f57ddd6cdcb122
ethicnology <ethicnology@pm.me>
2025-03-19 08:25:06 -0400

    feat: DeriveBackupKeyFromDefaultWalletUsecase

--- lib/recoverbull/domain/usecases/derive_backup_key_from_default_wallet_usecase.dart
+++ lib/recoverbull/domain/usecases/derive_backup_key_from_default_wallet_usecase.dart
@@ -1,23 +1,48 @@
 import 'package:bb_mobile/_core/domain/repositories/recoverbull_repository.dart';
+import 'package:bb_mobile/_core/domain/repositories/seed_repository.dart';
+import 'package:bb_mobile/_core/domain/repositories/wallet_metadata_repository.dart';
+import 'package:bb_mobile/_utils/bip32_derivation.dart';
+import 'package:bb_mobile/_utils/bip85_derivation.dart';
 import 'package:flutter/foundation.dart';
+import 'package:recoverbull/recoverbull.dart';
 
 class DeriveBackupKeyFromDefaultWalletUsecase {
-  final RecoverBullRepository _recoverBullRepository;
+  final RecoverBullRepository recoverBullRepository;
+  final SeedRepository seedRepository;
+  final WalletMetadataRepository walletMetadataRepository;
 
   DeriveBackupKeyFromDefaultWalletUsecase({
-    required RecoverBullRepository recoverBullRepository,
-  }) : _recoverBullRepository = recoverBullRepository;
+    required this.recoverBullRepository,
+    required this.seedRepository,
+    required this.walletMetadataRepository,
+  });
 
-  Future<void> execute(String backupFile, String backupKey) async {
+  Future<String> execute(String backupFile) async {
     try {
-      final jsonBackup = _recoverBullRepository.restoreBackupFile(
-        backupFile,
-        backupKey,
+      final isValidBackupFile = BullBackup.isValid(backupFile);
+      if (!isValidBackupFile) throw 'Invalid backup file';
+
+      // The default wallet is used to derive the backup key
+      final defaultMetadata = await walletMetadataRepository.getDefault();
+      final defaultFingerprint = defaultMetadata.masterFingerprint;
+      final defaultSeed = await seedRepository.get(defaultFingerprint);
+      final defaultXprv = Bip32Derivation.getXprvFromSeed(
+        defaultSeed.bytes,
+        defaultMetadata.network,
       );
 
-      // TODO: import wallets
+      final bullBackup = BullBackup.fromJson(backupFile);
+
+      if (bullBackup.path == null) {
+        throw '$DeriveBackupKeyFromDefaultWalletUsecase: Bip85 path is missing from the backup file';
+      }
+
+      final derivedBackupKey =
+          Bip85Derivation.deriveBackupKey(defaultXprv, bullBackup.path!);
+
+      return derivedBackupKey;
     } catch (e) {
-      debugPrint('error creating encrypted backup: $e');
+      debugPrint('$DeriveBackupKeyFromDefaultWalletUsecase: $e');
       rethrow;
     }
   }
