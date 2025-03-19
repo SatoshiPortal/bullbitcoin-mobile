1289556ca602b40642817ea3359af87e048b342d
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-03-19 09:49:39 -0400

    refactor: enhance TorRepository implementation with initialization checks and start method

--- lib/_core/data/repositories/tor_repository_impl.dart
+++ lib/_core/data/repositories/tor_repository_impl.dart
@@ -1,32 +1,50 @@
 import 'dart:io';
 import 'package:bb_mobile/_core/data/datasources/tor_datasource.dart';
 import 'package:bb_mobile/_core/domain/repositories/tor_repository.dart';
+import 'package:flutter/rendering.dart';
 import 'package:recoverbull/recoverbull.dart';
 
 class TorRepositoryImpl implements TorRepository {
   final TorDatasource _torDatasource;
+  bool _initialized = false;
 
   TorRepositoryImpl(this._torDatasource);
 
+  Future<void> _ensureInitialized() async {
+    if (!_initialized) {
+      await start();
+      _initialized = true;
+    }
+  }
+
   @override
   Future<bool> isTorReady() async {
-    return await _torDatasource.isReady;
+    try {
+      await _ensureInitialized();
+      return _torDatasource.isReady;
+    } catch (e) {
+      return false;
+    }
   }
 
   @override
   Future<SOCKSSocket> createSocket() async {
-    // Make sure Tor is ready
+    await _ensureInitialized();
+
     final isReady = await isTorReady();
     if (!isReady) {
       throw Exception('Tor is not ready');
     }
 
-    // Get the Tor proxy port
-    final proxyPort = _torDatasource.port;
-
     return await SOCKSSocket.create(
       proxyHost: InternetAddress.loopbackIPv4.address,
-      proxyPort: proxyPort,
+      proxyPort: _torDatasource.port,
     );
+  }
+
+  @override
+  Future<void> start() async {
+    await _torDatasource.start();
+    debugPrint('Tor started at port: ${_torDatasource.port}');
   }
 }
