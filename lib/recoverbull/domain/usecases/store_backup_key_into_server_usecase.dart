72a97d1b60da216ad6da8a5d6f88e3d0cd074c30
ethicnology <ethicnology@pm.me>
2025-03-19 08:29:00 -0400

    refactor(StoreBackupKeyIntoServerUsecase): use DeriveBackupKeyFromDefaultWalletUsecase to avoid code duplication

--- lib/recoverbull/domain/usecases/store_backup_key_into_server_usecase.dart
+++ lib/recoverbull/domain/usecases/store_backup_key_into_server_usecase.dart
@@ -1,8 +1,7 @@
 import 'package:bb_mobile/_core/domain/repositories/recoverbull_repository.dart';
 import 'package:bb_mobile/_core/domain/repositories/seed_repository.dart';
 import 'package:bb_mobile/_core/domain/repositories/wallet_metadata_repository.dart';
-import 'package:bb_mobile/_utils/bip32_derivation.dart';
-import 'package:bb_mobile/_utils/bip85_derivation.dart';
+import 'package:bb_mobile/recoverbull/domain/usecases/derive_backup_key_from_default_wallet_usecase.dart';
 import 'package:bb_mobile/recoverbull/recoverbull_password_validator.dart';
 import 'package:flutter/foundation.dart';
 import 'package:hex/hex.dart';
@@ -34,29 +33,18 @@ class StoreBackupKeyIntoServerUsecase {
         throw '$StoreBackupKeyIntoServerUsecase: password is too common';
       }
 
-      final bullBackup = BullBackup.fromJson(backupFile);
-
-      if (bullBackup.path == null) {
-        throw '$StoreBackupKeyIntoServerUsecase: Bip85 path is missing from the backup file';
-      }
-
-      // The default wallet is used to derive the backup key
-      final defaultMetadata = await walletMetadataRepository.getDefault();
-      final defaultFingerprint = defaultMetadata.masterFingerprint;
-      final defaultSeed = await seedRepository.get(defaultFingerprint);
-      final defaultXprv = Bip32Derivation.getXprvFromSeed(
-        defaultSeed.bytes,
-        defaultMetadata.network,
-      );
-
-      // Derive the backup key using BIP85
-      final derivedBackupKey =
-          Bip85Derivation.deriveBackupKey(defaultXprv, bullBackup.path!);
+      final derivedBackupKey = await DeriveBackupKeyFromDefaultWalletUsecase(
+        recoverBullRepository: recoverBullRepository,
+        seedRepository: seedRepository,
+        walletMetadataRepository: walletMetadataRepository,
+      ).execute(backupFile);
 
       // Ensure the given backup key is derived from the default wallet
       if (backupKey != derivedBackupKey) {
         throw '$StoreBackupKeyIntoServerUsecase: the given backup key is not derived from the current default wallet';
       }
+
+      final bullBackup = BullBackup.fromJson(backupFile);
 
       await recoverBullRepository.storeBackupKey(
         HEX.encode(bullBackup.id),
