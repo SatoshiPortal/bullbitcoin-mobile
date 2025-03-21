7a1cde7a27199f9235801747b263e774d11887c2
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-03-21 01:46:51 -0400

    feat: register BackupKeyService in KeyServerLocator and update use cases

--- lib/key_server/key_server_locator.dart
+++ lib/key_server/key_server_locator.dart
@@ -1,6 +1,8 @@
 import 'package:bb_mobile/_core/domain/repositories/recoverbull_repository.dart';
 import 'package:bb_mobile/_core/domain/repositories/seed_repository.dart';
 import 'package:bb_mobile/_core/domain/repositories/wallet_metadata_repository.dart';
+import 'package:bb_mobile/key_server/data/services/backup_key_service.dart';
+import 'package:bb_mobile/key_server/domain/services/backup_key_service.dart';
 import 'package:bb_mobile/key_server/domain/usecases/check_key_server_connection_usecase.dart';
 import 'package:bb_mobile/key_server/domain/usecases/derive_backup_key_from_default_wallet_usecase.dart';
 import 'package:bb_mobile/key_server/domain/usecases/restore_backup_key_from_password_usecase.dart';
@@ -11,12 +13,18 @@ import 'package:bb_mobile/locator.dart';
 
 class KeyServerLocator {
   static void setup() {
+    // Registering services
+    locator.registerLazySingleton<BackupKeyService>(
+      () => BackupKeyServiceImpl(
+        seedRepository: locator<SeedRepository>(),
+        walletMetadataRepository: locator<WalletMetadataRepository>(),
+      ),
+    );
     // Use cases
     locator.registerFactory<StoreBackupKeyIntoServerUsecase>(
       () => StoreBackupKeyIntoServerUsecase(
         recoverBullRepository: locator<RecoverBullRepository>(),
-        seedRepository: locator<SeedRepository>(),
-        walletMetadataRepository: locator<WalletMetadataRepository>(),
+        backupService: locator<BackupKeyService>(),
       ),
     );
 
@@ -34,9 +42,7 @@ class KeyServerLocator {
 
     locator.registerFactory<DeriveBackupKeyFromDefaultWalletUsecase>(
       () => DeriveBackupKeyFromDefaultWalletUsecase(
-        seedRepository: locator<SeedRepository>(),
-        walletMetadataRepository: locator<WalletMetadataRepository>(),
-        recoverBullRepository: locator<RecoverBullRepository>(),
+        backupKeyService: locator<BackupKeyService>(),
       ),
     );
 
