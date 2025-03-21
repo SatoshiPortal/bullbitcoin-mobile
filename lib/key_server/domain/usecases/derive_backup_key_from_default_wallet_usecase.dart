20a3bec3a88fcc52fe1b60c05a7db038572bfcb4
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-03-21 01:45:08 -0400

    feat: refactor DeriveBackupKeyFromDefaultWalletUsecase to use BackupKeyService

--- lib/key_server/domain/usecases/derive_backup_key_from_default_wallet_usecase.dart
+++ lib/key_server/domain/usecases/derive_backup_key_from_default_wallet_usecase.dart
@@ -1,46 +1,25 @@
-import 'package:bb_mobile/_core/domain/repositories/recoverbull_repository.dart';
-import 'package:bb_mobile/_core/domain/repositories/seed_repository.dart';
-import 'package:bb_mobile/_core/domain/repositories/wallet_metadata_repository.dart';
-import 'package:bb_mobile/_utils/bip32_derivation.dart';
-import 'package:bb_mobile/_utils/bip85_derivation.dart';
+import 'package:bb_mobile/key_server/domain/model/errors/key_server_error.dart';
+import 'package:bb_mobile/key_server/domain/services/backup_key_service.dart';
 import 'package:flutter/foundation.dart';
 import 'package:recoverbull/recoverbull.dart';
 
 class DeriveBackupKeyFromDefaultWalletUsecase {
-  final RecoverBullRepository recoverBullRepository;
-  final SeedRepository seedRepository;
-  final WalletMetadataRepository walletMetadataRepository;
+  final BackupKeyService _backupKeyService;
 
   DeriveBackupKeyFromDefaultWalletUsecase({
-    required this.recoverBullRepository,
-    required this.seedRepository,
-    required this.walletMetadataRepository,
-  });
+    required BackupKeyService backupKeyService,
+  }) : _backupKeyService = backupKeyService;
 
   Future<String> execute({required String backupFileAsString}) async {
     try {
-      final isValidBackupFile = BullBackup.isValid(backupFile);
-      if (!isValidBackupFile) throw 'Invalid backup file';
-
-      // The default wallet is used to derive the backup key
-      final defaultMetadata = await walletMetadataRepository.getDefault();
-      final defaultFingerprint = defaultMetadata.masterFingerprint;
-      final defaultSeed = await seedRepository.get(defaultFingerprint);
-      final defaultXprv = Bip32Derivation.getXprvFromSeed(
-        defaultSeed.bytes,
-        defaultMetadata.network,
-      );
-
-      final bullBackup = BullBackup.fromJson(backupFile);
-
-      if (bullBackup.path == null) {
-        throw '$DeriveBackupKeyFromDefaultWalletUsecase: Bip85 path is missing from the backup file';
+      if (!BullBackup.isValid(backupFileAsString)) {
+        throw const BackupFileInvalidError();
       }
+      final bullBackup = BullBackup.fromJson(backupFileAsString);
 
-      final derivedBackupKey =
-          Bip85Derivation.deriveBackupKey(defaultXprv, bullBackup.path!);
-
-      return derivedBackupKey;
+      return await _backupKeyService.deriveBackupKeyFromDefaultSeed(
+        path: bullBackup.path,
+      );
     } catch (e) {
       debugPrint('$DeriveBackupKeyFromDefaultWalletUsecase: $e');
       rethrow;
