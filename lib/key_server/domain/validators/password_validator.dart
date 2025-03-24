c84e0d28526df37bd7e5a52232408027bd56c85b
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-03-24 07:38:09 -0400

    fix: update import path for most common passwords in password validator

--- lib/key_server/domain/validators/password_validator.dart
+++ lib/key_server/domain/validators/password_validator.dart
@@ -1,4 +1,4 @@
-import 'package:bb_mobile/key_server/data/most_common_passwords.dart';
+import 'package:bb_mobile/key_server/data/constants/most_common_passwords.dart';
 
 class RecoverBullPasswordValidator {
   /// Returns `true` if the given password is part of the top most common passwords
