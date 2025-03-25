c300c8d11490e2d3021965e2500268dcd8d35169
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-03-25 04:24:52 -0400

    feat: add keychain recovery screens

--- lib/key_server/ui/key_server_flow.dart
+++ lib/key_server/ui/key_server_flow.dart
@@ -1,15 +1,20 @@
 import 'package:bb_mobile/_ui/components/loading/progress_screen.dart';
 import 'package:bb_mobile/_ui/components/template/screen_template.dart'
     show StackedPage;
+import 'package:bb_mobile/backup_settings/ui/backup_settings_router.dart';
 import 'package:bb_mobile/key_server/presentation/bloc/key_server_cubit.dart';
 import 'package:bb_mobile/key_server/ui/screens/confirm_screen.dart';
 import 'package:bb_mobile/key_server/ui/screens/enter_screen.dart';
-import 'package:bb_mobile/key_server/ui/screens/recovery_screen.dart';
+import 'package:bb_mobile/key_server/ui/screens/recover_with_backup_key_screen.dart';
+import 'package:bb_mobile/key_server/ui/screens/recover_with_secret_screen.dart';
+import 'package:bb_mobile/key_server/ui/widgets/error_screen.dart';
 import 'package:bb_mobile/locator.dart';
+import 'package:bb_mobile/settings/ui/settings_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart'
-    show BlocBuilder, BlocListener, BlocProvider, ReadContext;
+    show BlocBuilder, BlocProvider, ReadContext;
+import 'package:go_router/go_router.dart';
 
 class KeyLoadingScreen extends StatelessWidget {
   const KeyLoadingScreen({super.key});
@@ -22,8 +27,8 @@ class KeyLoadingScreen extends StatelessWidget {
   }
 }
 
-class KeySuccessScreen extends StatelessWidget {
-  const KeySuccessScreen({super.key});
+class BackupSuccessScreen extends StatelessWidget {
+  const BackupSuccessScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -33,38 +38,110 @@ class KeySuccessScreen extends StatelessWidget {
       title: 'Backup completed!',
       isLoading: false,
       buttonText: 'Test Backup',
-      onTap: () => context.read<KeyServerCubit>().updateKeyServerState(
-            flow: CurrentKeyServerFlow.recovery,
-          ),
+      onTap: () => context.pushNamed(
+        BackupSettingsSubroute.encryptedVaultRecoverFlow.name,
+        extra: false,
+      ),
+    );
+  }
+}
+
+class RecoverSuccessScreen extends StatelessWidget {
+  const RecoverSuccessScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ProgressScreen(
+      title: 'Test completed successfully!',
+      description: 'You are able to recover access to a lost Bitcoin wallet',
+      isLoading: false,
+      buttonText: 'Done',
+      onTap: () => context.go(
+        SettingsSubroute.backupSettings.path,
+        extra: false,
+      ),
+    );
+  }
+}
+
+class RecoverTestSuccessScreen extends StatelessWidget {
+  const RecoverTestSuccessScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ProgressScreen(
+      title: 'Test completed successfully!',
+      description: 'You are able to recover access to a lost Bitcoin wallet',
+      isLoading: false,
+      buttonText: 'Done',
+      onTap: () => context.go(
+        SettingsSubroute.backupSettings.path,
+        extra: false,
+      ),
     );
   }
 }
 
 class KeyServerFlow extends StatelessWidget {
-  const KeyServerFlow({super.key, this.encrypted});
+  const KeyServerFlow({super.key, this.encrypted, this.currentFlow});
   final String? encrypted;
+  final String? currentFlow;
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) =>
-          locator<KeyServerCubit>()..updateKeyServerState(encrypted: encrypted),
+    return BlocProvider.value(
+      value: locator<KeyServerCubit>()
+        ..updateKeyServerState(
+          encrypted: encrypted,
+          flow: CurrentKeyServerFlow.fromString(currentFlow ?? ''),
+        ),
       child: BlocBuilder<KeyServerCubit, KeyServerState>(
+        buildWhen: (previous, current) =>
+            previous.currentFlow != current.currentFlow ||
+            previous.status != current.status ||
+            previous.secret != current.secret ||
+            previous.secretStatus != current.secretStatus,
         builder: (context, state) {
           if (state.status == const KeyServerOperationStatus.loading()) {
             return const KeyLoadingScreen();
           }
 
+          if (state.status.maybeWhen(
+            failure: (_) => true,
+            orElse: () => false,
+          )) {
+            return ErrorScreen(
+              message: state.status.maybeWhen(
+                failure: (message) => message,
+                orElse: () => 'An error occurred',
+              ),
+              title: 'Oops! Something went wrong',
+              onRetry: () {
+                context.read<KeyServerCubit>().clearError();
+                context.read<KeyServerCubit>().updateKeyServerState(
+                      flow: CurrentKeyServerFlow.enter,
+                    );
+              },
+            );
+          }
+
           if (state.status == const KeyServerOperationStatus.success() &&
               state.secretStatus == SecretStatus.stored) {
-            return const KeySuccessScreen();
+            return const BackupSuccessScreen();
+          }
+          if (state.status == const KeyServerOperationStatus.success() &&
+              state.secretStatus == SecretStatus.recovered) {
+            return const BackupSuccessScreen();
           }
 
           return switch (state.currentFlow) {
             CurrentKeyServerFlow.enter => const EnterScreen(),
             CurrentKeyServerFlow.confirm => const ConfirmScreen(),
-            CurrentKeyServerFlow.recovery => const RecoverScreen(),
+            CurrentKeyServerFlow.recovery => const RecoverWithSecretScreen(),
             CurrentKeyServerFlow.delete => const EnterScreen(),
+            // TODO: Handle this case.
+            CurrentKeyServerFlow.recoveryWithBackupKey =>
+              const RecoverWithBackupKeyScreen(),
           };
         },
       ),
@@ -90,7 +167,7 @@ class PageLayout extends StatelessWidget {
           bottomHeight ?? MediaQuery.of(context).size.height * 0.11,
       bottomChild: bottomChild,
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: children,
