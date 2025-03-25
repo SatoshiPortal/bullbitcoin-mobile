2b7fd197bf420e83681ff1ea46dab6f6bbe58dbb
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-03-25 03:10:12 -0400

    feat: rename SelectFilePathUsecase to SelectFileFromPathUsecase

--- lib/_core/domain/usecases/select_file_path_usecase.dart
+++ lib/_core/domain/usecases/select_file_path_usecase.dart
@@ -1,9 +1,9 @@
 import 'package:bb_mobile/_core/domain/repositories/file_system_repository.dart';
 
-class SelectFilePathUsecase {
+class SelectFileFromPathUsecase {
   final FileSystemRepository _fileRepository;
 
-  SelectFilePathUsecase(this._fileRepository);
+  SelectFileFromPathUsecase(this._fileRepository);
 
   Future<String?> execute() async {
     return await _fileRepository.pickFile();
