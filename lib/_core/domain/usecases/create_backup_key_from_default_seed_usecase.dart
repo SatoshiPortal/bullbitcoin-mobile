1f2cfeab29db3a9693826a723c7bc830813020b0
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-03-25 03:08:44 -0400

    feat: updated use cases for creating backup key from default seed and fetching backup from file system

---
+++ lib/_core/domain/usecases/create_backup_key_from_default_seed_usecase.dart
@@ -0,0 +1,45 @@
+import 'package:bb_mobile/_core/domain/repositories/seed_repository.dart';
+import 'package:bb_mobile/_core/domain/repositories/wallet_metadata_repository.dart';
+import 'package:bb_mobile/_utils/bip32_derivation.dart';
+import 'package:bb_mobile/_utils/bip85_derivation.dart';
+import 'package:flutter/foundation.dart';
+
+class CreateBackupKeyFromDefaultSeedUsecase {
+  final SeedRepository seedRepository;
+  final WalletMetadataRepository walletMetadataRepository;
+
+  CreateBackupKeyFromDefaultSeedUsecase({
+    required this.seedRepository,
+    required this.walletMetadataRepository,
+  });
+
+  Future<String> execute() async {
+    try {
+      // The default wallet is used to derive the backup key
+      final defaultMetadata = await walletMetadataRepository.getDefault();
+
+      final defaultFingerprint = defaultMetadata.masterFingerprint;
+      final defaultSeedExists = await seedRepository.exists(defaultFingerprint);
+      if (!defaultSeedExists) {
+        throw 'CreateBackupKeyFromDefaultSeedUsecase: Default seed not found for fingerprint: $defaultFingerprint';
+      }
+      final defaultSeed = await seedRepository.get(defaultFingerprint);
+
+      final defaultXprv = Bip32Derivation.getXprvFromSeed(
+        defaultSeed.bytes,
+        defaultMetadata.network,
+      );
+
+      // Derive the backup key using BIP85
+      final derivationPath = Bip85Derivation.generateBackupKeyPath();
+
+      final backupKey =
+          Bip85Derivation.deriveBackupKey(defaultXprv, derivationPath);
+
+      return backupKey;
+    } catch (e) {
+      debugPrint('$CreateBackupKeyFromDefaultSeedUsecase: $e');
+      rethrow;
+    }
+  }
+}
