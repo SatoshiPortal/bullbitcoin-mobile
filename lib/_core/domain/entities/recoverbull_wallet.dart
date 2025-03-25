4a7e7144011534db1907f0995aefeadab7be0695
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-03-25 11:59:17 -0400

    feat: update RecoverBullWallet to use mnemonic&passphrase instead of seed

--- lib/_core/domain/entities/recoverbull_wallet.dart
+++ lib/_core/domain/entities/recoverbull_wallet.dart
@@ -1,25 +1,13 @@
 import 'package:bb_mobile/_core/domain/entities/wallet_metadata.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:hex/hex.dart';
 
 part 'recoverbull_wallet.freezed.dart';
 part 'recoverbull_wallet.g.dart';
 
-@JsonSerializable()
-class HexConverter implements JsonConverter<List<int>, String> {
-  const HexConverter();
-
-  @override
-  List<int> fromJson(String json) => HEX.decode(json);
-
-  @override
-  String toJson(List<int> object) => HEX.encode(object);
-}
-
 @freezed
 class RecoverBullWallet with _$RecoverBullWallet {
   const factory RecoverBullWallet({
-    @HexConverter() required List<int> seed,
+    @Default((<String>[], '')) (List<String>, String?) mnemonicPassphrase,
     required WalletMetadata metadata,
   }) = _RecoverBullWallet;
 
