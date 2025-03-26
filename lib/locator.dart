e55054a02a49606f9f2ea325900da7f963c3f489
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-03-26 10:58:11 -0400

    chore: update import for BackupSettingsLocator

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,7 +1,7 @@
 import 'package:bb_mobile/_core/core_locator.dart';
 import 'package:bb_mobile/app_startup/app_startup_locator.dart';
 import 'package:bb_mobile/app_unlock/app_unlock_locator.dart';
-import 'package:bb_mobile/backup_settings/backup_wallet_settings_locator.dart';
+import 'package:bb_mobile/backup_settings/backup_settings_locator.dart';
 import 'package:bb_mobile/backup_wallet/backup_wallet_locator.dart';
 import 'package:bb_mobile/bitcoin_price/bitcoin_price_locator.dart';
 import 'package:bb_mobile/home/home_locator.dart';
