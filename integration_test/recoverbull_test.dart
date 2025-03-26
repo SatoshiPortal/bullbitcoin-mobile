098efcf611c3ca4d561bd6b25783198096e9d83e
ethicnology <ethicnology@pm.me>
2025-03-26 15:06:41 -0400

    tests: remove errors

--- integration_test/recoverbull_test.dart
+++ integration_test/recoverbull_test.dart
@@ -1,13 +1,11 @@
 import 'package:bb_mobile/_core/domain/entities/settings.dart';
-import 'package:bb_mobile/_core/domain/repositories/seed_repository.dart';
-import 'package:bb_mobile/_core/domain/repositories/wallet_metadata_repository.dart';
 import 'package:bb_mobile/_core/domain/services/wallet_manager_service.dart';
+import 'package:bb_mobile/backup_wallet/domain/usecases/create_encrypted_vault_usecase.dart';
 import 'package:bb_mobile/key_server/domain/usecases/derive_backup_key_from_default_wallet_usecase.dart';
 import 'package:bb_mobile/key_server/domain/usecases/restore_backup_key_from_password_usecase.dart';
 import 'package:bb_mobile/key_server/domain/usecases/store_backup_key_into_server_usecase.dart';
 import 'package:bb_mobile/locator.dart';
-import 'package:bb_mobile/recoverbull/domain/usecases/create_encrypted_vault_usecase.dart';
-import 'package:bb_mobile/recoverbull/domain/usecases/restore_encrypted_vault_from_backup_key_usecase.dart';
+import 'package:bb_mobile/recover_wallet/domain/usecases/restore_encrypted_vault_from_backup_key_usecase.dart';
 import 'package:bb_mobile/settings/domain/usecases/set_environment_usecase.dart';
 import 'package:flutter/foundation.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -38,7 +36,7 @@ void main() {
     final backupFile = await locator<CreateEncryptedVaultUsecase>().execute();
 
     final backupKey = await locator<DeriveBackupKeyFromDefaultWalletUsecase>()
-        .execute(backupFile);
+        .execute(backupFile: backupFile);
 
     await locator<StoreBackupKeyIntoServerUsecase>().execute(
       password: password,
@@ -47,14 +45,16 @@ void main() {
     );
 
     await locator<RestoreBackupKeyFromPasswordUsecase>().execute(
-      backupFile,
-      password,
+      backupFile: backupFile,
+      password: password,
     );
 
     await locator<RestoreEncryptedVaultFromBackupKeyUsecase>().execute(
       backupFile: backupFile,
       backupKey: backupKey,
     );
+
+    // TODO: make TOR work with this test
 
     expect(true, true);
   });
