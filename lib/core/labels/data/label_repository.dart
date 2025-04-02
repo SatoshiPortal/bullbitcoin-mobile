103c1e923e89083cdad7ee08b53cb3e6ee86221e
Vishal Menon <vishalmenon.92@gmail.com>
2025-04-02 18:19:09 +0530

    feat: updated label repository

--- lib/core/labels/data/label_repository.dart
+++ lib/core/labels/data/label_repository.dart
@@ -1,49 +1,41 @@
-// import 'package:bb_mobile/core/labels/data/label_storage_datasource.dart';
-// import 'package:bb_mobile/core/labels/domain/label_entity.dart';
+import 'package:bb_mobile/core/labels/data/label_storage_datasource.dart';
+import 'package:bb_mobile/core/labels/domain/label_entity.dart';
 
-// class LabelRepository {
-//   final LabelStorageDatasource _mainLabelStorage;
-//   final LabelStorageDatasource _refLabelStorage;
+class LabelRepository {
+  final LabelStorageDatasource _labelStorageDatasource;
 
-//   LabelRepository({
-//     required LabelStorageDatasource mainLabelStorage,
-//     required LabelStorageDatasource refLabelStorage,
-//   })  : _mainLabelStorage = mainLabelStorage,
-//         _refLabelStorage = refLabelStorage;
+  LabelRepository({
+    required LabelStorageDatasource labelStorageDatasource,
+  }) : _labelStorageDatasource = labelStorageDatasource;
 
-//   Future<void> createLabel(Label label) async {
-//     try {
-//       await _labelStorage.create(label);
-//     } catch (e) {
-//       rethrow;
-//     }
-//   }
+  Future<void> createLabel(Label label) async {
+    await _labelStorageDatasource.create(label);
+  }
 
-//   Future<void> deleteLabelForRef(
-//     String label,
-//     String ref,
-//     LabelType type,
-//   ) async {
-//     try {
-//       await _labelStorage.deleteLabelForRef(label, ref, type);
-//     } catch (e) {
-//       rethrow;
-//     }
-//   }
+  Future<List<Label>?> findLabelsByName(String labelText) async {
+    final labelModels = await _labelStorageDatasource.readByLabel(labelText);
+    if (labelModels == null || labelModels.isEmpty) {
+      return null;
+    }
 
-//   Future<List<Label>?> getAllLabels() async {
-//     try {
-//       return await _labelStorage.readAll();
-//     } catch (e) {
-//       rethrow;
-//     }
-//   }
+    return labelModels.map((model) => model.toEntity()).toList();
+  }
 
-//   Future<List<Label>?> getLabelsByRef(String ref) async {
-//     try {
-//       return await _labelStorage.readByRef(ref);
-//     } catch (e) {
-//       rethrow;
-//     }
-//   }
-// }
+  Future<List<Label>?> findLabelsByRef(String ref) async {
+    final labelModels = await _labelStorageDatasource.readByRef(ref);
+    if (labelModels == null || labelModels.isEmpty) {
+      return null;
+    }
+
+    return labelModels.map((model) => model.toEntity()).toList();
+  }
+
+  Future<void> deleteLabel(Label label) async {
+    await _labelStorageDatasource.deleteLabel(label);
+  }
+
+  Future<List<Label>> getAllLabels() async {
+    final labelModels = await _labelStorageDatasource.readAll();
+    return labelModels.map((model) => model.toEntity()).toList();
+  }
+}
