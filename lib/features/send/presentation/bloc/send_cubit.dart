fd1d5e076e734d9b95f069fc746e336789cc9476
mocodesmo <morteza4048@gmail.com>
2025-04-03 15:58:33 +0530

    feat: implement SendCubit and SendState for transaction handling

---
+++ lib/features/send/presentation/bloc/send_cubit.dart
@@ -0,0 +1,65 @@
+import 'package:bb_mobile/core/wallet/domain/entity/utxo.dart';
+import 'package:bb_mobile/features/send/presentation/bloc/send_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SendCubit extends Cubit<SendState> {
+  SendCubit() : super(const SendState());
+
+  void addressChanged(String address) {
+    emit(state.copyWith(addressOrInvoice: address));
+  }
+
+  void sendTypeChanged(SendType sendType) {
+    emit(state.copyWith(sendType: sendType));
+  }
+
+  void amountCurrencyChanged(String currency) {
+    emit(state.copyWith(fiatCurrencyCode: currency));
+  }
+
+  void amountChanged(String amount) {
+    emit(state.copyWith(amount: amount));
+  }
+
+  void maxAmountChanged() {
+    // emit(state.copyWith(amount: state.wallet?.balance.toString() ?? ''));
+  }
+
+  void noteChanged(String note) {
+    emit(state.copyWith(label: note));
+  }
+
+  void loadUtxos() {
+    // emit(state.copyWith(utxos: utxos));
+  }
+
+  void utxoSelected(Utxo utxo) {
+    final selectedUtxos = List.of(state.selectedUtxos);
+    if (selectedUtxos.contains(utxo)) {
+      selectedUtxos.remove(utxo);
+    } else {
+      selectedUtxos.add(utxo);
+    }
+    emit(state.copyWith(selectedUtxos: selectedUtxos));
+  }
+
+  void replaceByFeeChanged(bool replaceByFee) {
+    emit(state.copyWith(replaceByFee: replaceByFee));
+  }
+
+  void loadFees() {
+    // emit(state.copyWith(fees: fees));
+  }
+
+  void feesChanged(int feeRate) {
+    // emit(state.copyWith(feeRate: feeRate));
+  }
+
+  void createTransaction() {
+    // emit(state.copyWith(transaction: transaction));
+  }
+
+  void confirmTransaction() {
+    // emit(state.copyWith(transaction: transaction));
+  }
+}
--- lib/features/send/presentation/bloc/send_state.dart
+++ lib/features/send/presentation/bloc/send_state.dart
@@ -2,6 +2,7 @@ import 'package:bb_mobile/core/settings/domain/entity/settings.dart';
 import 'package:bb_mobile/core/swaps/domain/entity/swap.dart';
 import 'package:bb_mobile/core/wallet/domain/entity/utxo.dart';
 import 'package:bb_mobile/core/wallet/domain/entity/wallet.dart';
+import 'package:bb_mobile/core/wallet/domain/entity/wallet_transaction.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'send_state.freezed.dart';
@@ -12,21 +13,37 @@ enum SendType {
   liquid,
 }
 
+enum SendStep {
+  address,
+  amount,
+  confirm,
+  sending,
+  sent,
+}
+
 @freezed
 class SendState with _$SendState {
   const factory SendState({
-    required SendType sendType,
-    required Wallet wallet,
+    // address
+    @Default(SendStep.address) SendStep step,
+    @Default(SendType.lightning) SendType sendType,
     @Default('') String addressOrInvoice,
+    // amount
+    Wallet? wallet,
     @Default('') String amount,
     @Default(BitcoinUnit.sats) BitcoinUnit bitcoinUnit,
     @Default([]) List<String> fiatCurrencyCodes,
     @Default('') String fiatCurrencyCode,
     @Default(0) double exchangeRate,
     @Default('') String label,
+    // confirm
     @Default([]) List<Utxo> utxos,
     @Default([]) List<Utxo> selectedUtxos,
     @Default(false) bool replaceByFee,
+    @Default([]) List<int> feesList,
+    @Default(0) int feeRate,
+    //
+    WalletTransaction? transaction,
     LnSendSwap? swap,
     Object? error,
   }) = _SendState;
