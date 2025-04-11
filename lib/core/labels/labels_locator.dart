89d0d4914cf7303aaf94ee0ebe5789d1fe15dbe4
ethicnology <ethicnology@pm.me>
2025-04-11 09:04:41 -0400

    refactor: LabelStorageDatasource

--- lib/core/labels/labels_locator.dart
+++ lib/core/labels/labels_locator.dart
@@ -1,22 +1,16 @@
 import 'package:bb_mobile/core/labels/data/label_repository.dart';
 import 'package:bb_mobile/core/labels/data/label_storage_datasource.dart';
 import 'package:bb_mobile/core/labels/domain/create_label_usecase.dart';
-import 'package:bb_mobile/core/storage/data/datasources/key_value_storage/impl/hive_storage_datasource_impl.dart';
-import 'package:bb_mobile/core/utils/constants.dart';
+import 'package:bb_mobile/core/storage/data/datasources/key_value_storage/impl/shared_preferences_datasource_impl.dart';
 import 'package:bb_mobile/core/wallet/domain/repositories/wallet_repository.dart';
 import 'package:bb_mobile/locator.dart';
-import 'package:hive/hive.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LabelsLocator {
   static Future<void> registerDatasources() async {
-    final labelsBox = await Hive.openBox<String>(HiveBoxNameConstants.labels);
-    final labelsByRefBox =
-        await Hive.openBox<String>(HiveBoxNameConstants.labelsByRef);
-
     locator.registerLazySingleton<LabelStorageDatasource>(
       () => LabelStorageDatasource(
-        mainLabelStorage: HiveStorageDatasourceImpl<String>(labelsBox),
-        refLabelStorage: HiveStorageDatasourceImpl<String>(labelsByRefBox),
+        labelStorage: SharedPreferencesDatasourceImpl(SharedPreferencesAsync()),
       ),
     );
   }
