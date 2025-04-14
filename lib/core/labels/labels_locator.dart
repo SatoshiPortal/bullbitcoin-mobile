b6b6d84194032c268ac19946251ec3345eb957b3
ethicnology <ethicnology@pm.me>
2025-04-14 15:15:11 -0400

    refactor: LabelStorageDatasource

--- lib/core/labels/labels_locator.dart
+++ lib/core/labels/labels_locator.dart
@@ -1,17 +1,12 @@
 import 'package:bb_mobile/core/labels/data/label_repository.dart';
 import 'package:bb_mobile/core/labels/data/label_storage_datasource.dart';
 import 'package:bb_mobile/core/labels/domain/create_label_usecase.dart';
-import 'package:bb_mobile/core/storage/data/datasources/key_value_storage/impl/shared_preferences_datasource_impl.dart';
-import 'package:bb_mobile/core/wallet/domain/repositories/wallet_repository.dart';
 import 'package:bb_mobile/locator.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class LabelsLocator {
   static Future<void> registerDatasources() async {
     locator.registerLazySingleton<LabelStorageDatasource>(
-      () => LabelStorageDatasource(
-        labelStorage: SharedPreferencesDatasourceImpl(SharedPreferencesAsync()),
-      ),
+      () => LabelStorageDatasource(),
     );
   }
 
@@ -25,10 +20,7 @@ class LabelsLocator {
 
   static void registerUseCases() {
     locator.registerFactory<CreateLabelUsecase>(
-      () => CreateLabelUsecase(
-        labelRepository: locator<LabelRepository>(),
-        walletRepository: locator<WalletRepository>(),
-      ),
+      () => CreateLabelUsecase(labelRepository: locator<LabelRepository>()),
     );
   }
 }
