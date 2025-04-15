fcd06e1ff8499bb23658d277482334f71d2500c1
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-04-15 12:20:37 -0400

    fix: add isTorEnabled parameter to loadingInProgress state

--- lib/features/app_startup/presentation/bloc/app_startup_state.dart
+++ lib/features/app_startup/presentation/bloc/app_startup_state.dart
@@ -3,8 +3,9 @@ part of 'app_startup_bloc.dart';
 @freezed
 sealed class AppStartupState with _$AppStartupState {
   const factory AppStartupState.initial() = AppStartupInitial;
-  const factory AppStartupState.loadingInProgress() =
-      AppStartupLoadingInProgress;
+  const factory AppStartupState.loadingInProgress({
+    @Default(false) bool isTorEnabled,
+  }) = AppStartupLoadingInProgress;
   const factory AppStartupState.success({
     @Default(false) bool isPinCodeSet,
     @Default(false) bool hasDefaultWallets,
