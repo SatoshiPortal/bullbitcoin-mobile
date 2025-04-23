266367b662bd7076ed1d02b154f5b7ba2cca2fbd
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-04-23 08:45:08 -0400

    feat(electrum): update execute method to return success status for server settings update

--- lib/core/electrum/domain/usecases/update_electrum_server_settings_usecase.dart
+++ lib/core/electrum/domain/usecases/update_electrum_server_settings_usecase.dart
@@ -1,15 +1,26 @@
 import 'package:bb_mobile/core/electrum/domain/entity/electrum_server.dart';
 import 'package:bb_mobile/core/electrum/domain/repositories/electrum_server_repository.dart';
+import 'package:flutter/foundation.dart';
 
 class UpdateElectrumServerSettingsUsecase {
   final ElectrumServerRepository _electrumServerRepository;
+
   const UpdateElectrumServerSettingsUsecase({
     required ElectrumServerRepository electrumServerRepository,
   }) : _electrumServerRepository = electrumServerRepository;
 
-  Future<void> execute({
+  Future<bool> execute({
     required ElectrumServer electrumServer,
   }) async {
-    await _electrumServerRepository.setElectrumServer(electrumServer);
+    try {
+      // Update server in repository
+      await _electrumServerRepository.setElectrumServer(electrumServer);
+      debugPrint('Successfully updated Electrum server settings');
+
+      return true;
+    } catch (e) {
+      debugPrint('Error updating Electrum server settings: $e');
+      return false;
+    }
   }
 }
