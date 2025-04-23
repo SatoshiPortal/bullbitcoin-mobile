674291c07101ae756215bb3c2d5d4582576382c4
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-04-23 08:46:01 -0400

    feat(electrum): register updateElectrumServerSettings use case in locator

--- lib/features/electrum_settings/electrum_settings_locator.dart
+++ lib/features/electrum_settings/electrum_settings_locator.dart
@@ -1,5 +1,6 @@
 import 'package:bb_mobile/core/electrum/domain/usecases/check_electrum_status_usecase.dart';
 import 'package:bb_mobile/core/electrum/domain/usecases/get_all_electrum_servers_usecase.dart';
+import 'package:bb_mobile/core/electrum/domain/usecases/update_electrum_server_settings_usecase.dart';
 
 import 'package:bb_mobile/features/electrum_settings/presentation/bloc/electrum_settings_bloc.dart';
 import 'package:bb_mobile/locator.dart';
@@ -11,6 +12,8 @@ class ElectrumSettingsLocator {
       () => ElectrumSettingsBloc(
         getAllElectrumServers: locator<GetAllElectrumServersUsecase>(),
         checkElectrumStatusUsecase: locator<CheckElectrumStatusUsecase>(),
+        updateElectrumServerSettings:
+            locator<UpdateElectrumServerSettingsUsecase>(),
       ),
     );
   }
