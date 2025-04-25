2fc33a7e5a202db6cb903ddb41105eb63ae2d992
ethicnology <ethicnology@pm.me>
2025-04-24 20:37:54 -0400

    refactor: add WalletMetadatas table to Sqlite and adapt generic function

--- lib/core/storage/sqlite_datasource.dart
+++ lib/core/storage/sqlite_datasource.dart
@@ -1,10 +1,11 @@
 import 'package:bb_mobile/core/transaction/data/models/transactions_table.dart';
+import 'package:bb_mobile/core/wallet/data/models/wallet_metadata_table.dart';
 import 'package:drift/drift.dart';
 import 'package:drift_flutter/drift_flutter.dart';
 
 part 'sqlite_datasource.g.dart';
 
-@DriftDatabase(tables: [Transactions])
+@DriftDatabase(tables: [Transactions, WalletMetadatas])
 class SqliteDatasource extends _$SqliteDatasource {
   SqliteDatasource([QueryExecutor? executor])
       : super(executor ?? _openConnection());
