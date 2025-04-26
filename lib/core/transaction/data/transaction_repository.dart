1b4e9ad04a5be63faece29bf02325df9914c2a64
ethicnology <ethicnology@pm.me>
2025-04-26 07:16:22 -0400

    refactor: replace extension by the manager

--- lib/core/transaction/data/transaction_repository.dart
+++ lib/core/transaction/data/transaction_repository.dart
@@ -2,9 +2,9 @@ import 'dart:convert';
 
 import 'package:bb_mobile/core/storage/sqlite_datasource.dart';
 import 'package:bb_mobile/core/transaction/data/electrum_service.dart';
-import 'package:bb_mobile/core/transaction/data/models/transaction_db_extension.dart';
 import 'package:bb_mobile/core/transaction/data/models/transaction_mapper.dart';
 import 'package:bb_mobile/core/transaction/domain/entities/tx.dart';
+import 'package:bb_mobile/locator.dart';
 
 class TransactionRepository {
   final _electrum = ElectrumService(host: 'blockstream.info', port: 700);
@@ -12,7 +12,10 @@ class TransactionRepository {
   TransactionRepository();
 
   Future<Tx> fetch({required String txid}) async {
-    final cachedTransaction = await TransactionDb.fetch(txid);
+    final sqlite = locator<SqliteDatasource>();
+    final cachedTransaction = await sqlite.managers.transactions
+        .filter((e) => e.txid(txid))
+        .getSingleOrNull();
 
     if (cachedTransaction != null) {
       return TransactionMapper.fromSqlite(cachedTransaction);
@@ -22,19 +25,18 @@ class TransactionRepository {
     final txBytes = await _electrum.getTransaction(txid);
     final tx = await TransactionMapper.fromBytes(txBytes);
 
-    // Map to the dataclass
-    final transaction = Transaction(
-      txid: tx.txid,
-      version: tx.version,
-      size: tx.size.toString(),
-      vsize: tx.vsize.toString(),
-      locktime: tx.locktime,
-      vin: json.encode(tx.vin.map((e) => e.toJson()).toList()),
-      vout: json.encode(tx.vout.map((e) => e.toJson()).toList()),
+    // Cache the transaction
+    await sqlite.managers.transactions.create(
+      (t) => t(
+        txid: tx.txid,
+        version: tx.version,
+        size: tx.size.toString(),
+        vsize: tx.vsize.toString(),
+        locktime: tx.locktime,
+        vin: json.encode(tx.vin.map((e) => e.toJson()).toList()),
+        vout: json.encode(tx.vout.map((e) => e.toJson()).toList()),
+      ),
     );
-
-    // Store the fetched transaction
-    await transaction.store();
 
     return tx;
   }
