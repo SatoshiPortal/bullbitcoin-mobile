29f0f7248f42b9d72bedb65ecf95667d6b6f1445
ethicnology <ethicnology@pm.me>
2025-04-26 10:17:11 -0400

    tests: refactor

--- integration_test/sqlite_wallet_metadata_test.dart
+++ integration_test/sqlite_wallet_metadata_test.dart
@@ -1,33 +1,40 @@
 import 'package:bb_mobile/core/storage/sqlite_datasource.dart';
-import 'package:bb_mobile/core/wallet/data/datasources/wallet_metadata_datasource.dart';
-import 'package:bb_mobile/core/wallet/data/models/wallet_metadata_extension.dart';
-import 'package:bb_mobile/core/wallet/data/models/wallet_metadata_model.dart';
+import 'package:bb_mobile/core/wallet/domain/entity/wallet.dart';
+import 'package:bb_mobile/core/wallet/wallet_metadata_service.dart';
 import 'package:bb_mobile/locator.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  locator.registerLazySingleton<SqliteDatasource>(() => SqliteDatasource());
+  final sqlite = locator<SqliteDatasource>();
 
-  setUpAll(() {
-    locator.registerLazySingleton<SqliteDatasource>(() => SqliteDatasource());
-    locator.registerLazySingleton<WalletMetadataDatasource>(
-      () => WalletMetadataDatasource(
-        sqliteDatasource: locator<SqliteDatasource>(),
-      ),
-    );
-  });
-
-  tearDownAll(() {});
   group('WalletMetadata Sqlite Integration Tests', () {
     test('', () async {
+      const fingerprint = 'master';
+      const isBitcoin = true;
+      const isMainnet = true;
+      const isTestnet = false;
+      const isLiquid = false;
+      const scriptType = ScriptType.bip84;
+
       final metadata = WalletMetadataModel(
+        masterFingerprint: fingerprint,
+        id: WalletMetadataService.encodeOrigin(
+          fingerprint: fingerprint,
+          isBitcoin: isBitcoin,
+          isMainnet: isMainnet,
+          isTestnet: isTestnet,
+          isLiquid: isLiquid,
+          scriptType: scriptType,
+        ),
         xpubFingerprint: 'abc12345',
-        isBitcoin: true,
-        isLiquid: false,
-        isMainnet: true,
-        isTestnet: false,
-        scriptType: 'bip84',
+        isBitcoin: isBitcoin,
+        isLiquid: isLiquid,
+        isMainnet: isMainnet,
+        isTestnet: isTestnet,
+        scriptType: scriptType.name,
         xpub: 'xpub6CUGRUonZSQ4TWtTMmzXdrXDtypWKiKp5i1Lsfk...',
         externalPublicDescriptor: 'wpkh([abcd1234/84h/0h/0h]xpub.../0/*)',
         internalPublicDescriptor: 'wpkh([abcd1234/84h/0h/0h]xpub.../1/*)',
@@ -41,21 +48,23 @@ void main() {
         syncedAt: DateTime.now(),
       );
 
-      final metadataSource = locator<WalletMetadataDatasource>();
-
       // Store a metadata
-      await metadataSource.store(metadata);
+      await sqlite.into(sqlite.walletMetadatas).insert(metadata);
 
       // Fetch one
-      final fetchedMetadata = await metadataSource.get(metadata.id);
+      final fetchedMetadata = await sqlite.managers.walletMetadatas
+          .filter((e) => e.id(metadata.id))
+          .getSingleOrNull();
       expect(fetchedMetadata, isNotNull);
       expect(fetchedMetadata!.id, metadata.id);
 
       // Delete the only one
-      await metadataSource.delete(metadata.origin);
+      await sqlite.managers.walletMetadatas
+          .filter((f) => f.id(metadata.id))
+          .delete();
 
       // Fetch all
-      final fetchedMetadatas = await metadataSource.getAll();
+      final fetchedMetadatas = await sqlite.managers.walletMetadatas.get();
       expect(fetchedMetadatas, isEmpty);
     });
   });
