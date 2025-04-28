fd65576b0934c2cf3866609046ee5116319f4e81
ethicnology <ethicnology@pm.me>
2025-04-28 12:49:54 -0400

    feat: `Labels` table and `LabelModel`

--- lib/core/storage/sqlite_datasource.dart
+++ lib/core/storage/sqlite_datasource.dart
@@ -1,3 +1,4 @@
+import 'package:bb_mobile/core/labels/data/labels_table.dart';
 import 'package:bb_mobile/core/transaction/data/models/transactions_table.dart';
 import 'package:bb_mobile/core/wallet/data/models/wallet_metadata_table.dart';
 import 'package:drift/drift.dart';
@@ -5,7 +6,7 @@ import 'package:drift_flutter/drift_flutter.dart';
 
 part 'sqlite_datasource.g.dart';
 
-@DriftDatabase(tables: [Transactions, WalletMetadatas])
+@DriftDatabase(tables: [Transactions, WalletMetadatas, Labels])
 class SqliteDatasource extends _$SqliteDatasource {
   SqliteDatasource([QueryExecutor? executor])
       : super(executor ?? _openConnection());
