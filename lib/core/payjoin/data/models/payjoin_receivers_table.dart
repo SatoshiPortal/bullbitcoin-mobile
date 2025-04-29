e1434a7c8244b431e511c591e73d9d6ca10559ab
ethicnology <ethicnology@pm.me>
2025-04-29 15:05:06 -0400

    feat: `PayjoinReceivers` table and `PayjoinReceiverModel`

---
+++ lib/core/payjoin/data/models/payjoin_receivers_table.dart
@@ -0,0 +1,23 @@
+import 'package:drift/drift.dart';
+
+@DataClassName('PayjoinReceiverModel')
+class PayjoinReceivers extends Table {
+  TextColumn get id => text()();
+  TextColumn get address => text()();
+  BoolColumn get isTestnet => boolean()();
+  TextColumn get receiver => text()();
+  TextColumn get walletId => text()();
+  TextColumn get pjUri => text()();
+  Int64Column get maxFeeRateSatPerVb => int64()();
+  IntColumn get expireAt => integer()();
+  BlobColumn get originalTxBytes => blob().nullable()();
+  TextColumn get originalTxId => text().nullable()();
+  Int64Column get amountSat => int64().nullable()();
+  TextColumn get proposalPsbt => text().nullable()();
+  TextColumn get txId => text().nullable()();
+  BoolColumn get isExpired => boolean()();
+  BoolColumn get isCompleted => boolean()();
+
+  @override
+  Set<Column> get primaryKey => {id};
+}
