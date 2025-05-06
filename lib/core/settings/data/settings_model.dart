ee47a83e5253b4dc4f91c48a26ab1e5013ea6ebf
ethicnology <ethicnology@pm.me>
2025-05-06 15:30:00 -0400

    feat: SettingsModel

---
+++ lib/core/settings/data/settings_model.dart
@@ -0,0 +1,19 @@
+import 'package:bb_mobile/core/settings/domain/settings_entity.dart';
+
+class SettingsModel {
+  final int id;
+  final Environment environment;
+  final BitcoinUnit bitcoinUnit;
+  final Language language;
+  final String currency;
+  final bool hideAmounts;
+
+  const SettingsModel({
+    required this.id,
+    required this.environment,
+    required this.bitcoinUnit,
+    required this.language,
+    required this.currency,
+    required this.hideAmounts,
+  });
+}
