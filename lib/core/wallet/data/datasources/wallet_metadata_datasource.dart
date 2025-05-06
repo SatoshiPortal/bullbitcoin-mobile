df8f4816343587cc08a8efce263b68312f58695e
ethicnology <ethicnology@pm.me>
2025-05-06 14:55:07 -0400

    feat: WalletMetadataDatasource

---
+++ lib/core/wallet/data/datasources/wallet_metadata_datasource.dart
@@ -0,0 +1,30 @@
+import 'package:bb_mobile/core/storage/sqlite_database.dart';
+import 'package:bb_mobile/core/wallet/data/models/wallet_metadata_model.dart';
+
+class WalletMetadataDatasource {
+  final SqliteDatabase _sqlite;
+
+  WalletMetadataDatasource({required SqliteDatabase sqliteDatasource})
+    : _sqlite = sqliteDatasource;
+
+  Future<void> store(WalletMetadataModel metadata) async {
+    await _sqlite
+        .into(_sqlite.walletMetadatas)
+        .insertOnConflictUpdate(metadata.toSqlite());
+  }
+
+  Future<WalletMetadataModel?> fetch(String walletId) async {
+    final row =
+        await _sqlite.managers.walletMetadatas
+            .filter((e) => e.id(walletId))
+            .getSingleOrNull();
+
+    if (row == null) return null;
+    return WalletMetadataModelMapper.fromSqlite(row);
+  }
+
+  Future<List<WalletMetadataModel>> fetchAll() async {
+    final rows = await _sqlite.managers.walletMetadatas.get();
+    return rows.map((e) => WalletMetadataModelMapper.fromSqlite(e)).toList();
+  }
+}
