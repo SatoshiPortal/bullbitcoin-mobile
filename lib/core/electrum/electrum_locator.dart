06652f7add74ba96140a557feb24a94ab33a7379
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-05-06 10:46:17 -0400

    chore: code cleanup

--- lib/core/electrum/electrum_locator.dart
+++ lib/core/electrum/electrum_locator.dart
@@ -2,7 +2,7 @@ import 'package:bb_mobile/core/electrum/data/datasources/electrum_server_storage
 import 'package:bb_mobile/core/electrum/data/repository/electrum_server_repository_impl.dart';
 import 'package:bb_mobile/core/electrum/domain/usecases/check_electrum_server_connectivity_usecase.dart';
 import 'package:bb_mobile/core/electrum/domain/usecases/get_all_electrum_servers_usecase.dart';
-import 'package:bb_mobile/core/electrum/domain/usecases/get_best_available_server_usecase.dart';
+import 'package:bb_mobile/core/electrum/domain/usecases/get_prioritized_server_usecase.dart';
 import 'package:bb_mobile/core/electrum/domain/usecases/update_electrum_server_settings_usecase.dart';
 import 'package:bb_mobile/core/storage/sqlite_database.dart';
 import 'package:bb_mobile/locator.dart';
