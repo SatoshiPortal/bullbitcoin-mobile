269450c2b878b9ed0029e20032b90f70208c5dae
ethicnology <ethicnology@pm.me>
2025-05-07 10:14:47 -0400

    refactor: re-order and verify swap mapping to sqlite

--- lib/core/storage/tables/swaps_table.dart
+++ lib/core/storage/tables/swaps_table.dart
@@ -11,21 +11,18 @@ class Swaps extends Table {
   BoolColumn get isTestnet => boolean()();
   IntColumn get keyIndex => integer()();
   IntColumn get creationTime => integer()();
-
   IntColumn get completionTime => integer().nullable()();
   TextColumn get receiveWalletId => text().nullable()();
   TextColumn get sendWalletId => text().nullable()();
   TextColumn get invoice => text().nullable()();
   TextColumn get paymentAddress => text().nullable()();
   IntColumn get paymentAmount => integer().nullable()();
-
   TextColumn get receiveAddress => text().nullable()();
   TextColumn get receiveTxid => text().nullable()();
   TextColumn get sendTxid => text().nullable()();
   TextColumn get preimage => text().nullable()();
   TextColumn get refundAddress => text().nullable()();
   TextColumn get refundTxid => text().nullable()();
-
   IntColumn get boltzFees => integer().nullable()();
   IntColumn get lockupFees => integer().nullable()();
   IntColumn get claimFees => integer().nullable()();
