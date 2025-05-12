00a64385585b3c724f2a66bbd475c3ee922888f5
ethicnology <ethicnology@pm.me>
2025-05-12 11:27:42 -0400

    refactor: computeOriginFromOldWallet

---
+++ lib/core/storage/migrations/hive_to_sqlite/migrate_utils.dart
@@ -0,0 +1,35 @@
+import 'package:bb_mobile/core/storage/migrations/hive_to_sqlite/old_wallet.dart'
+    show BBNetwork, BaseWalletType, Wallet;
+import 'package:bb_mobile/core/wallet/domain/entities/wallet.dart'
+    show Network, ScriptType;
+import 'package:bb_mobile/core/wallet/wallet_metadata_service.dart'
+    show WalletMetadataService;
+
+String computeOriginFromOldWallet(Wallet wallet) {
+  final bbNetwork = wallet.network;
+  final bbWalletType = wallet.baseWalletType;
+
+  Network? network;
+  if (bbNetwork == BBNetwork.Mainnet &&
+      bbWalletType == BaseWalletType.Bitcoin) {
+    network = Network.bitcoinMainnet;
+  } else if (bbNetwork == BBNetwork.Mainnet &&
+      bbWalletType == BaseWalletType.Liquid) {
+    network = Network.liquidMainnet;
+  } else if (bbNetwork == BBNetwork.Testnet &&
+      bbWalletType == BaseWalletType.Bitcoin) {
+    network = Network.bitcoinTestnet;
+  } else if (bbNetwork == BBNetwork.Testnet &&
+      bbWalletType == BaseWalletType.Liquid) {
+    network = Network.liquidTestnet;
+  } else {
+    final msg = 'Unsupported network: $bbNetwork $bbWalletType';
+    throw Exception(msg);
+  }
+
+  return WalletMetadataService.encodeOrigin(
+    fingerprint: wallet.sourceFingerprint,
+    network: network,
+    scriptType: ScriptType.fromName(wallet.scriptType.name),
+  );
+}
