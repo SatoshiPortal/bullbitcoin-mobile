8032a886281f98ac51e28648a54b94b73d082c7f
Vishal Menon <vishalmenon.92@gmail.com>
2025-05-14 23:59:02 +0530

    isV4 true for v4
    Co-authored-by: ethicnology <ethicnology@pm.me>

--- lib/core/storage/migrations/004_legacy/legacy_migration.dart
+++ lib/core/storage/migrations/004_legacy/legacy_migration.dart
@@ -15,7 +15,7 @@ Future<bool> legacyMigrateToV4(String fromVersion) async {
     await doMigration0_3to0_4();
     return true;
   } else if (fromVersion.startsWith('0.4')) {
-    return false;
+    return true;
   }
   return false;
 }
