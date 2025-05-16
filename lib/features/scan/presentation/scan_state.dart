91c164fbba9f5ca3b7f643ce2420b6c99d6e75be
ethicnology <ethicnology@pm.me>
2025-05-15 21:10:29 -0400

    feat: bbqr integration prototype

--- lib/features/scan/presentation/scan_state.dart
+++ lib/features/scan/presentation/scan_state.dart
@@ -1,4 +1,5 @@
 import 'package:bb_mobile/core/utils/payment_request.dart';
+import 'package:bb_mobile/features/scan/bbqr_service.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'scan_state.freezed.dart';
@@ -6,10 +7,13 @@ part 'scan_state.freezed.dart';
 @freezed
 abstract class ScanState with _$ScanState {
   const factory ScanState({
-    required bool isStreaming,
-    required String data,
+    @Default('') String data,
     PaymentRequest? paymentRequest,
+    @Default(false) bool isCollectingBbqr,
+    BbqrOptions? bbqrOptions,
+    @Default({}) Map<int, String> bbqr,
+    @Default('') String bbqrPsbt,
   }) = _ScanState;
 
-  factory ScanState.initial() => const ScanState(isStreaming: false, data: '');
+  factory ScanState.initial() => const ScanState();
 }
