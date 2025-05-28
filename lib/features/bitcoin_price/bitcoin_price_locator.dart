43fca8245ea3482dbccd42fecfce62fd1ed5e1af
kumulynja <b@kumuly.dev>
2025-05-28 12:16:35 +0200

    add default fiat currency selection to settings and apply in the rest of the app

--- lib/features/bitcoin_price/bitcoin_price_locator.dart
+++ lib/features/bitcoin_price/bitcoin_price_locator.dart
@@ -1,6 +1,7 @@
 import 'package:bb_mobile/core/exchange/domain/usecases/convert_sats_to_currency_amount_usecase.dart';
 import 'package:bb_mobile/core/exchange/domain/usecases/get_available_currencies_usecase.dart';
 import 'package:bb_mobile/core/settings/domain/get_settings_usecase.dart';
+import 'package:bb_mobile/core/settings/domain/watch_currency_changes_usecase.dart';
 import 'package:bb_mobile/features/bitcoin_price/presentation/bloc/bitcoin_price_bloc.dart';
 import 'package:bb_mobile/locator.dart';
 
@@ -13,6 +14,7 @@ class BitcoinPriceLocator {
         getSettingsUsecase: locator<GetSettingsUsecase>(),
         convertSatsToCurrencyAmountUsecase:
             locator<ConvertSatsToCurrencyAmountUsecase>(),
+        watchCurrencyChangesUsecase: locator<WatchCurrencyChangesUsecase>(),
       ),
     );
   }
