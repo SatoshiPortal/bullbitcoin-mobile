556c0df23f9ad1b6fd463f61796e43b941a01238
Vishal Menon <vishalmenon.92@gmail.com>
2025-06-02 11:50:16 +0530

    feat: added order datasources with tests. added test for user datasource.

---
+++ lib/core/exchange/data/models/order_model.dart
@@ -0,0 +1,189 @@
+enum FiatCurrency { cad, eur, mxn }
+
+enum Network { lightning, bitcoin, liquid }
+
+extension FiatCurrencyExtension on FiatCurrency {
+  String get apiValue {
+    switch (this) {
+      case FiatCurrency.cad:
+        return 'CAD';
+      case FiatCurrency.eur:
+        return 'EUR';
+      case FiatCurrency.mxn:
+        return 'MXN';
+    }
+  }
+
+  static FiatCurrency fromApiValue(String value) {
+    switch (value.toUpperCase()) {
+      case 'CAD':
+        return FiatCurrency.cad;
+      case 'EUR':
+        return FiatCurrency.eur;
+      case 'MXN':
+        return FiatCurrency.mxn;
+      default:
+        throw Exception('Unknown FiatCurrency: $value');
+    }
+  }
+}
+
+extension NetworkExtension on Network {
+  String get apiValue {
+    switch (this) {
+      case Network.lightning:
+        return 'lightning';
+      case Network.bitcoin:
+        return 'bitcoin';
+      case Network.liquid:
+        return 'liquid';
+    }
+  }
+
+  static Network fromApiValue(String value) {
+    switch (value.toLowerCase()) {
+      case 'lightning':
+        return Network.lightning;
+      case 'bitcoin':
+        return Network.bitcoin;
+      case 'liquid':
+        return Network.liquid;
+      default:
+        throw Exception('Unknown Network: $value');
+    }
+  }
+}
+
+class OrderModel {
+  final String orderId;
+  final String orderType;
+  final String orderSubtype;
+  final int orderNumber;
+  final double exchangeRateAmount;
+  final String exchangeRateCurrency;
+  final double indexRateAmount;
+  final String indexRateCurrency;
+  final double payinAmount;
+  final String payinCurrency;
+  final double payoutAmount;
+  final String payoutCurrency;
+  final String orderStatus;
+  final String payinStatus;
+  final String payoutStatus;
+  final String? scheduledPayoutTime;
+  final String createdAt;
+  final String? completedAt;
+  final Map<String, dynamic>? message;
+  final String? sentAt;
+  final String payinMethod;
+  final String payoutMethod;
+  final String triggerType;
+  final String confirmationDeadline;
+  final dynamic unbatchedBuyOnchainFees;
+  final String? bitcoinTransactionId;
+  final String? lnUrl;
+  final String? lightningVoucherExpiresAt;
+  final bool isPPBitcoinOutUpdatable;
+  final dynamic payinAmountChanged;
+
+  OrderModel({
+    required this.orderId,
+    required this.orderType,
+    required this.orderSubtype,
+    required this.orderNumber,
+    required this.exchangeRateAmount,
+    required this.exchangeRateCurrency,
+    required this.indexRateAmount,
+    required this.indexRateCurrency,
+    required this.payinAmount,
+    required this.payinCurrency,
+    required this.payoutAmount,
+    required this.payoutCurrency,
+    required this.orderStatus,
+    required this.payinStatus,
+    required this.payoutStatus,
+    required this.scheduledPayoutTime,
+    required this.createdAt,
+    required this.completedAt,
+    required this.message,
+    required this.sentAt,
+    required this.payinMethod,
+    required this.payoutMethod,
+    required this.triggerType,
+    required this.confirmationDeadline,
+    required this.unbatchedBuyOnchainFees,
+    required this.bitcoinTransactionId,
+    required this.lnUrl,
+    required this.lightningVoucherExpiresAt,
+    required this.isPPBitcoinOutUpdatable,
+    required this.payinAmountChanged,
+  });
+
+  factory OrderModel.fromJson(Map<String, dynamic> json) {
+    return OrderModel(
+      orderId: json['orderId'] as String,
+      orderType: json['orderType'] as String,
+      orderSubtype: json['orderSubtype'] as String,
+      orderNumber: json['orderNumber'] as int,
+      exchangeRateAmount: (json['exchangeRateAmount'] as num).toDouble(),
+      exchangeRateCurrency: json['exchangeRateCurrency'] as String,
+      indexRateAmount: (json['indexRateAmount'] as num).toDouble(),
+      indexRateCurrency: json['indexRateCurrency'] as String,
+      payinAmount: (json['payinAmount'] as num).toDouble(),
+      payinCurrency: json['payinCurrency'] as String,
+      payoutAmount: (json['payoutAmount'] as num).toDouble(),
+      payoutCurrency: json['payoutCurrency'] as String,
+      orderStatus: json['orderStatus'] as String,
+      payinStatus: json['payinStatus'] as String,
+      payoutStatus: json['payoutStatus'] as String,
+      scheduledPayoutTime: json['scheduledPayoutTime'] as String?,
+      createdAt: json['createdAt'] as String,
+      completedAt: json['completedAt'] as String?,
+      message: json['message'] as Map<String, dynamic>?,
+      sentAt: json['sentAt'] as String?,
+      payinMethod: json['payinMethod'] as String,
+      payoutMethod: json['payoutMethod'] as String,
+      triggerType: json['triggerType'] as String,
+      confirmationDeadline: json['confirmationDeadline'] as String,
+      unbatchedBuyOnchainFees: json['unbatchedBuyOnchainFees'],
+      bitcoinTransactionId: json['bitcoinTransactionId'] as String?,
+      lnUrl: json['lnUrl'] as String?,
+      lightningVoucherExpiresAt: json['lightningVoucherExpiresAt'] as String?,
+      isPPBitcoinOutUpdatable: json['isPPBitcoinOutUpdatable'] as bool,
+      payinAmountChanged: json['payinAmountChanged'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'orderId': orderId,
+    'orderType': orderType,
+    'orderSubtype': orderSubtype,
+    'orderNumber': orderNumber,
+    'exchangeRateAmount': exchangeRateAmount,
+    'exchangeRateCurrency': exchangeRateCurrency,
+    'indexRateAmount': indexRateAmount,
+    'indexRateCurrency': indexRateCurrency,
+    'payinAmount': payinAmount,
+    'payinCurrency': payinCurrency,
+    'payoutAmount': payoutAmount,
+    'payoutCurrency': payoutCurrency,
+    'orderStatus': orderStatus,
+    'payinStatus': payinStatus,
+    'payoutStatus': payoutStatus,
+    'scheduledPayoutTime': scheduledPayoutTime,
+    'createdAt': createdAt,
+    'completedAt': completedAt,
+    'message': message,
+    'sentAt': sentAt,
+    'payinMethod': payinMethod,
+    'payoutMethod': payoutMethod,
+    'triggerType': triggerType,
+    'confirmationDeadline': confirmationDeadline,
+    'unbatchedBuyOnchainFees': unbatchedBuyOnchainFees,
+    'bitcoinTransactionId': bitcoinTransactionId,
+    'lnUrl': lnUrl,
+    'lightningVoucherExpiresAt': lightningVoucherExpiresAt,
+    'isPPBitcoinOutUpdatable': isPPBitcoinOutUpdatable,
+    'payinAmountChanged': payinAmountChanged,
+  };
+}
