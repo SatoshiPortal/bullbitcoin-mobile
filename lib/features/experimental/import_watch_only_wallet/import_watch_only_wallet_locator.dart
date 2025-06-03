19ed6ee5ea4fd54a1d57d4d68f95f07f00561292
ethicnology <ethicnology@pm.me>
2025-06-02 20:54:52 -0400

    refactor: move import watch only wallet files into experimental

--- lib/features/experimental/import_watch_only_wallet/import_watch_only_wallet_locator.dart
+++ lib/features/experimental/import_watch_only_wallet/import_watch_only_wallet_locator.dart
@@ -1,6 +1,6 @@
 import 'package:bb_mobile/core/settings/data/settings_repository.dart';
 import 'package:bb_mobile/core/wallet/domain/repositories/wallet_repository.dart';
-import 'package:bb_mobile/features/import_watch_only_wallet/domain/usecases/import_xpub_use_case.dart';
+import 'package:bb_mobile/features/experimental/import_watch_only_wallet/domain/usecases/import_xpub_use_case.dart';
 import 'package:bb_mobile/locator.dart';
 
 class ImportWatchOnlyWalletLocator {
