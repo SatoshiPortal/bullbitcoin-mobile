6b53f50a0e2d21c0ae8687172c0920cf9737dee2
ethicnology <ethicnology@pm.me>
2025-06-05 12:18:07 -0400

    refactor(WalletSendButton): superuser can click send with a watch-only wallet

--- lib/features/wallet/ui/widgets/wallet_bottom_buttons.dart
+++ lib/features/wallet/ui/widgets/wallet_bottom_buttons.dart
@@ -1,9 +1,11 @@
 import 'package:bb_mobile/core/wallet/domain/entities/wallet.dart';
 import 'package:bb_mobile/features/receive/ui/receive_router.dart';
 import 'package:bb_mobile/features/send/ui/send_router.dart';
+import 'package:bb_mobile/features/settings/presentation/bloc/settings_cubit.dart';
 import 'package:bb_mobile/ui/components/buttons/button.dart';
 import 'package:bb_mobile/ui/themes/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 
@@ -14,6 +16,10 @@ class WalletBottomButtons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isSuperuser = context.select(
+      (SettingsCubit cubit) => cubit.state.isSuperuser ?? false,
+    );
+
     return Row(
       // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
@@ -51,8 +57,9 @@ class WalletBottomButtons extends StatelessWidget {
             bgColor: context.colour.secondary,
             textColor: context.colour.onPrimary,
             disabled:
-                wallet?.source == WalletSource.xpub ||
-                wallet?.source == WalletSource.coldcard,
+                (wallet?.source == WalletSource.xpub ||
+                    wallet?.source == WalletSource.coldcard) &&
+                !isSuperuser,
           ),
         ),
       ],
