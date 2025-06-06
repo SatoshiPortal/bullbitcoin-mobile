840bc630403a3b8d8b164445fd146c39ab97535d
ethicnology <ethicnology@pm.me>
2025-06-05 20:45:24 -0400

    feat: Add ScanSignedTxRouter to ExperimentalRouterConfig

--- lib/features/experimental/experimental_router.dart
+++ lib/features/experimental/experimental_router.dart
@@ -1,10 +1,15 @@
 import 'package:bb_mobile/features/experimental/import_watch_only_wallet/import_watch_only_router.dart';
 import 'package:bb_mobile/features/experimental/psbt_flow/psbt_router.dart';
+import 'package:bb_mobile/features/experimental/scan_signed_tx/scan_signed_tx_router.dart';
 import 'package:go_router/go_router.dart';
 
 class ExperimentalRouterConfig {
   static final route = ShellRoute(
     builder: (context, state, child) => child,
-    routes: [ImportWatchOnlyRouterConfig.route, PsbtRouterConfig.route],
+    routes: [
+      ImportWatchOnlyRouterConfig.route,
+      PsbtRouterConfig.route,
+      ScanSignedTxRouterConfig.route,
+    ],
   );
 }
