856ce02b825e651e5791ed8fb5ef85448250d284
ethicnology <ethicnology@pm.me>
2025-06-10 15:28:30 -0400

    feat: request_identifier_cubit

---
+++ lib/features/send/request_identifier/request_identifier_cubit.dart
@@ -0,0 +1,33 @@
+import 'package:bb_mobile/core/utils/payment_request.dart';
+import 'package:bb_mobile/features/send/request_identifier/request_identifier_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class RequestIdentifierCubit extends Cubit<RequestIdentifierState> {
+  RequestIdentifierCubit() : super(const RequestIdentifierState());
+
+  void onScanned(String data) {
+    if (data.isEmpty) return;
+
+    try {
+      PaymentRequest.parse(data);
+      emit(state.copyWith(redirect: RequestIdentifierRedirect.toSend));
+    } catch (e) {
+      emit(state.copyWith(error: e.toString()));
+    }
+  }
+
+  void updateRawRequest(String data) {
+    if (data.isEmpty) return;
+    emit(state.copyWith(rawRequest: data));
+  }
+
+  void validatePaymentRequest() {
+    if (state.rawRequest.isEmpty) return;
+    try {
+      PaymentRequest.parse(state.rawRequest);
+      emit(state.copyWith(redirect: RequestIdentifierRedirect.toSend));
+    } catch (e) {
+      emit(state.copyWith(error: e.toString()));
+    }
+  }
+}
