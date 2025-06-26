dff3d4cea5a9dc7ec40d2bdf7790f29004a179fa
ethicnology <ethicnology@pm.me>
2025-06-26 15:54:12 -0400

    refactor: simplify WatchOnlyWalletEntity by using directly WatchOnly from Satoshifier

--- lib/features/experimental/import_watch_only_wallet/watch_only_wallet_entity.dart
+++ lib/features/experimental/import_watch_only_wallet/watch_only_wallet_entity.dart
@@ -1,40 +1,15 @@
-import 'package:bb_mobile/core/utils/bip32_derivation.dart';
-import 'package:bb_mobile/core/wallet/domain/entities/wallet.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:satoshifier/satoshifier.dart';
 
 part 'watch_only_wallet_entity.freezed.dart';
 
 @freezed
 abstract class WatchOnlyWalletEntity with _$WatchOnlyWalletEntity {
   const factory WatchOnlyWalletEntity({
-    required String pubkey,
-    required String fingerprint,
-    required ScriptType type,
+    required WatchOnly watchOnly,
     @Default('') String label,
+    @Default('') String masterFingerprint,
   }) = _WatchOnlyWalletEntity;
 
   const WatchOnlyWalletEntity._();
-
-  factory WatchOnlyWalletEntity.from(
-    String extendedPublicKey, {
-    String label = '',
-  }) {
-    return WatchOnlyWalletEntity(
-      pubkey: extendedPublicKey,
-      fingerprint: _getPubkeyFingerprint(extendedPublicKey),
-      type: ScriptType.fromExtendedPublicKey(extendedPublicKey),
-      label: label,
-    );
-  }
-
-  static String _getPubkeyFingerprint(String pubkey) {
-    final bip32Xpub = Bip32Derivation.getBip32Xpub(pubkey);
-    return bip32Xpub.fingerprintHex;
-  }
-
-  bool get canGenerateValidPsbt {
-    // If the fingerprint is generated from the pubkey
-    // it means the wallet is NOT valid to generate PSBT
-    return _getPubkeyFingerprint(pubkey) != fingerprint;
-  }
 }
