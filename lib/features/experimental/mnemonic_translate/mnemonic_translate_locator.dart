e6bd12ef42ef0155e1254a79eec2251ec65bb3cf
ethicnology <ethicnology@pm.me>
2025-06-28 10:48:32 -0400

    feat: add mnemonic translate locator

---
+++ lib/features/experimental/mnemonic_translate/mnemonic_translate_locator.dart
@@ -0,0 +1,19 @@
+import 'package:bb_mobile/core/seed/data/repository/seed_repository.dart';
+import 'package:bb_mobile/core/wallet/domain/repositories/wallet_repository.dart';
+import 'package:bb_mobile/features/experimental/mnemonic_translate/domain/usecases/get_default_mnemonic_usecase.dart';
+import 'package:bb_mobile/locator.dart';
+
+class MnemonicTranslateLocator {
+  static void setup() {
+    registerUsecases();
+  }
+
+  static void registerUsecases() {
+    locator.registerFactory<GetDefaultMnemonicUsecase>(
+      () => GetDefaultMnemonicUsecase(
+        walletRepository: locator<WalletRepository>(),
+        seedRepository: locator<SeedRepository>(),
+      ),
+    );
+  }
+}
