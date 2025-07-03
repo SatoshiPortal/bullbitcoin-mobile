1376e7bbbd20281788283469817c40688c1d79b4
ethicnology <ethicnology@pm.me>
2025-07-03 10:36:01 -0400

    feat: add union type support for watch-only wallet entities

--- lib/features/experimental/import_watch_only_wallet/watch_only_wallet_entity.dart
+++ lib/features/experimental/import_watch_only_wallet/watch_only_wallet_entity.dart
@@ -1,67 +1,85 @@
 import 'package:bb_mobile/core/wallet/domain/entities/wallet.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:satoshifier/satoshifier.dart';
+import 'package:satoshifier/satoshifier.dart' as satoshifier;
 
 part 'watch_only_wallet_entity.freezed.dart';
 
 @freezed
 abstract class WatchOnlyWalletEntity with _$WatchOnlyWalletEntity {
-  const factory WatchOnlyWalletEntity({
-    required WatchOnly watchOnly,
-    required WalletSource source,
+  const factory WatchOnlyWalletEntity.descriptor({
+    required satoshifier.WatchOnlyDescriptor watchOnlyDescriptor,
+    @Default(WalletSource.descriptors) WalletSource source,
     @Default('') String label,
-    @Default('') String masterFingerprint,
-  }) = _WatchOnlyWalletEntity;
+  }) = WatchOnlyDescriptorEntity;
 
-  factory WatchOnlyWalletEntity.fromSatoshifier({
-    required WatchOnly watchOnly,
-    String label = '',
-    String masterFingerprint = '',
-  }) {
-    return WatchOnlyWalletEntity(
-      watchOnly: watchOnly,
-      source:
-          watchOnly.canGenerateValidPsbt
-              ? WalletSource.descriptors
-              : WalletSource.xpub,
-      label: label,
-      masterFingerprint: masterFingerprint,
-    );
-  }
-
-  static Future<WatchOnlyWalletEntity> parse(String input) async {
-    try {
-      final watchOnly = await Satoshifier.parse(input);
-
-      if (watchOnly is! WatchOnly) {
-        throw 'Unsupported watch only format';
-      }
-
-      final masterFingerprint =
-          watchOnly.masterFingerprint.isNotEmpty
-              ? watchOnly.masterFingerprint
-              : watchOnly.pubkeyFingerprint;
-
-      final source =
-          watchOnly.canGenerateValidPsbt
-              ? WalletSource.descriptors
-              : WalletSource.xpub;
-
-      return WatchOnlyWalletEntity(
-        watchOnly: watchOnly,
-        masterFingerprint: masterFingerprint,
-        source: source,
-      );
-    } catch (e) {
-      rethrow;
-    }
-  }
+  const factory WatchOnlyWalletEntity.xpub({
+    required satoshifier.WatchOnlyXpub watchOnlyXpub,
+    @Default(WalletSource.xpub) WalletSource source,
+    @Default('') String label,
+  }) = WatchOnlyXpubEntity;
 
   const WatchOnlyWalletEntity._();
 
-  Descriptor get descriptor => watchOnly.descriptor;
-  ExtendedPubkey? get extendedPublicKey => watchOnly.extendedPubkey;
-  String get pubkeyFingerprint => watchOnly.pubkeyFingerprint;
-  bool get canGenerateValidPsbt =>
-      masterFingerprint.isNotEmpty && masterFingerprint != pubkeyFingerprint;
+  T when<T>({
+    required T Function(
+      satoshifier.WatchOnlyDescriptor watchOnlyDescriptor,
+      WalletSource source,
+      String label,
+    )
+    descriptor,
+    required T Function(
+      satoshifier.WatchOnlyXpub watchOnlyXpub,
+      WalletSource source,
+      String label,
+    )
+    xpub,
+  }) {
+    if (this is WatchOnlyDescriptorEntity) {
+      final entity = this as WatchOnlyDescriptorEntity;
+      return descriptor(
+        entity.watchOnlyDescriptor,
+        entity.source,
+        entity.label,
+      );
+    } else if (this is WatchOnlyXpubEntity) {
+      final entity = this as WatchOnlyXpubEntity;
+      return xpub(entity.watchOnlyXpub, entity.source, entity.label);
+    }
+    throw UnimplementedError();
+  }
+
+  static Future<WatchOnlyWalletEntity> parse(String value) async {
+    final satoshified = await satoshifier.Satoshifier.parse(value);
+    if (satoshified is satoshifier.WatchOnlyDescriptor) {
+      return WatchOnlyWalletEntity.descriptor(watchOnlyDescriptor: satoshified);
+    } else if (satoshified is satoshifier.WatchOnlyXpub) {
+      return WatchOnlyWalletEntity.xpub(watchOnlyXpub: satoshified);
+    }
+    throw Exception('Unsupported watch only format');
+  }
+
+  String get pubkey => when(
+    descriptor: (x, _, _) => x.descriptor.pubkey,
+    xpub: (x, _, _) => x.extendedPubkey.pubBase58,
+  );
+
+  Network get network => when(
+    descriptor: (x, _, _) => Network.fromName(x.descriptor.network.name),
+    xpub: (x, _, _) => Network.fromName(x.extendedPubkey.network.name),
+  );
+
+  ScriptType get scriptType => when(
+    descriptor: (x, _, _) => ScriptType.fromName(x.descriptor.derivation.name),
+    xpub: (x, _, _) => ScriptType.fromName(x.extendedPubkey.derivation.name),
+  );
+}
+
+extension WatchOnlyXpubEntityExtension on WatchOnlyXpubEntity {
+  satoshifier.ExtendedPubkey get extendedPubkey => watchOnlyXpub.extendedPubkey;
+}
+
+extension WatchOnlyDescriptorEntityExtension on WatchOnlyDescriptorEntity {
+  satoshifier.Descriptor get descriptor => watchOnlyDescriptor.descriptor;
+  String get masterFingerprint => watchOnlyDescriptor.masterFingerprint;
+  String get pubkeyFingerprint => watchOnlyDescriptor.pubkeyFingerprint;
 }
