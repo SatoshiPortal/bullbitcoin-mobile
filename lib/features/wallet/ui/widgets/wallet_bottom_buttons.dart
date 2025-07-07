911e30cfca39a2366d84ab683129cdd685e3a3af
ethicnology <ethicnology@pm.me>
2025-07-07 09:52:21 -0400

    refactor: remove exception made for xpub before the last descriptor import

--- lib/features/wallet/ui/widgets/wallet_bottom_buttons.dart
+++ lib/features/wallet/ui/widgets/wallet_bottom_buttons.dart
@@ -4,9 +4,7 @@ import 'package:bb_mobile/core/wallet/domain/entities/wallet.dart';
 import 'package:bb_mobile/core/widgets/buttons/button.dart';
 import 'package:bb_mobile/features/receive/ui/receive_router.dart';
 import 'package:bb_mobile/features/send/ui/send_router.dart';
-import 'package:bb_mobile/features/settings/presentation/bloc/settings_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 
@@ -17,10 +15,6 @@ class WalletBottomButtons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final isSuperuser = context.select(
-      (SettingsCubit cubit) => cubit.state.isSuperuser ?? false,
-    );
-
     return Row(
       // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
@@ -59,7 +53,7 @@ class WalletBottomButtons extends StatelessWidget {
                 ),
             bgColor: context.colour.secondary,
             textColor: context.colour.onPrimary,
-            disabled: wallet?.signer == SignerEntity.none && !isSuperuser,
+            disabled: wallet?.signer == SignerEntity.none,
           ),
         ),
       ],
