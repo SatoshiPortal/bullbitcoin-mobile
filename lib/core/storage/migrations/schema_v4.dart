4669ab51fb3e8a7b565c9037f7e9b0a2c526557c
ethicnology <ethicnology@pm.me>
2025-07-15 13:03:01 -0400

    refactor: optimize migration

--- lib/core/storage/migrations/schema_v4.dart
+++ lib/core/storage/migrations/schema_v4.dart
@@ -15,34 +15,17 @@ class SchemaV4 {
     TableInfo<WalletMetadatas, dynamic> walletMetadatas,
   ) async {
     // Rename source column to signer
-    const sqlRename =
-        'ALTER TABLE wallet_metadatas RENAME COLUMN source TO signer';
-    await db.customStatement(sqlRename);
+    await db.customStatement(
+      'ALTER TABLE wallet_metadatas RENAME COLUMN source TO signer',
+    );
 
-    // Select all metadatas
-    const sqlSelect = 'SELECT id, signer FROM wallet_metadatas';
-    final metadatas = await db.customSelect(sqlSelect).get();
-
-    // Map old source values to new signer enum values
-    for (final metadata in metadatas) {
-      final id = metadata.read<String>('id');
-      final source = metadata.read<String>('signer'); // renamed source
-
-      String signer;
-      switch (source) {
-        case 'mnemonic':
-          signer = Signer.local.name;
-        case 'descriptors':
-          signer = Signer.remote.name;
-        default:
-          signer = Signer.none.name;
-      }
-
-      // Update the new signer value
-      await db.customUpdate(
-        'UPDATE wallet_metadatas SET signer = ? WHERE id = ?',
-        variables: [Variable.withString(signer), Variable.withString(id)],
-      );
-    }
+    // Map old wallet_source values to new signer enum values
+    await db.customStatement('''
+    UPDATE wallet_metadatas
+    SET signer = CASE signer
+      WHEN 'mnemonic' THEN 'local'
+      WHEN 'descriptors' THEN 'remote'
+      ELSE 'none'
+    END''');
   }
 }
