fa2b734b23c3ed17e71fc19cf8acc30cb1b760e4
ethicnology <ethicnology@pm.me>
2025-07-16 10:35:27 -0400

    feat: ColdCard PushTx

--- lib/features/experimental/broadcast_signed_tx/presentation/broadcast_signed_tx_cubit.dart
+++ lib/features/experimental/broadcast_signed_tx/presentation/broadcast_signed_tx_cubit.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+
 import 'package:bb_mobile/core/bbqr/bbqr_service.dart';
 import 'package:bb_mobile/core/blockchain/domain/usecases/broadcast_bitcoin_transaction_usecase.dart';
+import 'package:bb_mobile/core/transaction/domain/entities/tx.dart';
 import 'package:bb_mobile/features/experimental/broadcast_signed_tx/presentation/broadcast_signed_tx_state.dart';
-import 'package:bdk_flutter/bdk_flutter.dart';
 import 'package:convert/convert.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_nfc_kit/flutter_nfc_kit.dart';
 
 class BroadcastSignedTxCubit extends Cubit<BroadcastSignedTxState> {
   final BroadcastBitcoinTransactionUsecase _broadcastBitcoinTransactionUsecase;
@@ -14,12 +17,55 @@ class BroadcastSignedTxCubit extends Cubit<BroadcastSignedTxState> {
   }) : _broadcastBitcoinTransactionUsecase = broadcastBitcoinTransactionUsecase,
        super(BroadcastSignedTxState(bbqr: BbqrService()));
 
-  Future<void> onScanned(String payload) async {
-    try {
-      emit(state.copyWith(error: null, transaction: null));
+  void clear() => emit(state.copyWith(error: null, transaction: null));
 
+  Future<void> onQrScanned(String payload) async {
+    try {
+      clear();
       final tx = await state.bbqr.scanTransaction(payload);
       if (tx != null) emit(state.copyWith(transaction: tx));
+    } catch (e) {
+      emit(state.copyWith(error: e.toString()));
+    }
+  }
+
+  Future<void> onNfcScanned(NFCTag tag) async {
+    clear();
+    try {
+      final ndefRecords = await FlutterNfcKit.readNDEFRecords();
+
+      if (ndefRecords.isEmpty) {
+        emit(state.copyWith(error: 'No NDEF records found'));
+        return;
+      }
+
+      final payload = ndefRecords.first.toString();
+      final uriRegex = RegExp('uri=([^ ]+)');
+      final match = uriRegex.firstMatch(payload);
+
+      if (match == null) {
+        emit(state.copyWith(error: 'No URI found'));
+        return;
+      }
+
+      final uriString = match.group(1)!;
+      final uri = Uri.parse(uriString);
+      final fragmentParams = Uri.splitQueryString(uri.fragment);
+
+      if (fragmentParams.isEmpty ||
+          !fragmentParams.keys.contains('t') ||
+          !fragmentParams.keys.contains('c')) {
+        emit(state.copyWith(error: 'No fragment params found'));
+        return;
+      }
+
+      final txBase64Url = base64Url.normalize(fragmentParams['t']!);
+      final checksumBase64Url = base64Url.normalize(fragmentParams['c']!);
+
+      final txBytesHex = hex.encode(base64Url.decode(txBase64Url));
+      final _ = base64Url.decode(checksumBase64Url);
+
+      await tryParseTransaction(txBytesHex);
     } catch (e) {
       emit(state.copyWith(error: e.toString()));
     }
@@ -28,20 +74,18 @@ class BroadcastSignedTxCubit extends Cubit<BroadcastSignedTxState> {
   Future<void> tryParseTransaction(String input) async {
     emit(state.copyWith(error: null));
     try {
-      final parsedTx = await PartiallySignedTransaction.fromString(input);
+      final tx = await TxEntity.fromPsbt(input);
       emit(
         state.copyWith(
-          transaction: (format: TxFormat.psbt, data: parsedTx.toString()),
+          transaction: (format: TxFormat.psbt, data: input, tx: tx),
         ),
       );
     } catch (e) {
       try {
-        final parsedTx = await Transaction.fromBytes(
-          transactionBytes: hex.decode(input),
-        );
+        final tx = await TxEntity.fromBytes(hex.decode(input));
         emit(
           state.copyWith(
-            transaction: (format: TxFormat.hex, data: parsedTx.toString()),
+            transaction: (format: TxFormat.hex, data: input, tx: tx),
           ),
         );
       } catch (e) {
--- lib/features/experimental/broadcast_signed_tx/presentation/broadcast_signed_tx_page.dart
+++ lib/features/experimental/broadcast_signed_tx/presentation/broadcast_signed_tx_page.dart
@@ -1,3 +1,4 @@
+import 'package:bb_mobile/core/widgets/transaction_details_widget.dart';
 import 'package:bb_mobile/features/experimental/broadcast_signed_tx/broadcast_signed_tx_router.dart';
 import 'package:bb_mobile/features/experimental/broadcast_signed_tx/presentation/broadcast_signed_tx_cubit.dart';
 import 'package:bb_mobile/features/experimental/broadcast_signed_tx/presentation/broadcast_signed_tx_state.dart';
@@ -29,52 +30,69 @@ class BroadcastSignedTxPage extends StatelessWidget {
         builder: (context, state) {
           final cubit = context.read<BroadcastSignedTxCubit>();
 
-          return Column(
-            children: [
-              PasteInput(
-                text: state.transaction?.data ?? '',
-                hint: 'Paste a PSBT or transaction HEX',
-                onChanged: cubit.tryParseTransaction,
-              ),
-
-              if (state.error != null)
-                BBText(
-                  state.error!,
-                  style: context.font.bodyMedium,
-                  color: context.colour.error,
-                ),
-
-              const Gap(32),
-              Padding(
-                padding: const EdgeInsets.only(left: 100, right: 100),
-                child: BBButton.big(
-                  label: 'Scan BBQR / Hex',
-                  onPressed:
-                      () => context.pushNamed(
-                        BroadcastSignedTxRoute.broadcastScan.name,
-                      ),
-                  bgColor: context.colour.onPrimary,
-                  textColor: context.colour.secondary,
-                  iconData: Icons.qr_code_scanner,
-                ),
-              ),
-
-              const Gap(32),
-
-              // Broadcast button
-              if (state.transaction != null) ...[
-                const SizedBox(height: 16),
-                Padding(
-                  padding: const EdgeInsets.only(left: 100, right: 100),
-                  child: BBButton.big(
-                    label: 'Broadcast',
-                    bgColor: context.colour.secondary,
-                    textColor: context.colour.onPrimary,
-                    onPressed: cubit.broadcastTransaction,
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                if (state.transaction == null) ...[
+                  PasteInput(
+                    text: state.transaction?.data ?? '',
+                    hint: 'Paste a PSBT or transaction HEX',
+                    onChanged: cubit.tryParseTransaction,
                   ),
-                ),
+                  const Gap(32),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 100, right: 100),
+                    child: BBButton.big(
+                      label: 'QR (BBQR / Hex)',
+                      onPressed:
+                          () => context.pushNamed(
+                            BroadcastSignedTxRoute.broadcastScanQr.name,
+                          ),
+                      bgColor: context.colour.onPrimary,
+                      textColor: context.colour.secondary,
+                      iconData: Icons.qr_code_scanner,
+                    ),
+                  ),
+                  const Gap(32),
+
+                  Padding(
+                    padding: const EdgeInsets.only(left: 100, right: 100),
+                    child: BBButton.big(
+                      label: 'NFC (PushTx)',
+                      onPressed:
+                          () => context.pushNamed(
+                            BroadcastSignedTxRoute.broadcastScanNfc.name,
+                          ),
+                      bgColor: context.colour.onPrimary,
+                      textColor: context.colour.secondary,
+                      iconData: Icons.nfc,
+                    ),
+                  ),
+                ],
+
+                if (state.error != null)
+                  BBText(
+                    state.error!,
+                    style: context.font.bodyMedium,
+                    color: context.colour.error,
+                  ),
+
+                // Broadcast button
+                if (state.transaction != null) ...[
+                  TransactionDetailsWidget(tx: state.transaction!.tx),
+                  const SizedBox(height: 16),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 100, right: 100),
+                    child: BBButton.big(
+                      label: 'Broadcast',
+                      bgColor: context.colour.secondary,
+                      textColor: context.colour.onPrimary,
+                      onPressed: cubit.broadcastTransaction,
+                    ),
+                  ),
+                ],
               ],
-            ],
+            ),
           );
         },
       ),
