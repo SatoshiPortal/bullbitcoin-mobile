615a61264d56e66fe9b87592fd71d4036e73ebf9
ethicnology <ethicnology@pm.me>
2025-07-19 10:25:05 -0400

    refactor: update integration tests to use `Bull.init` and improve test descriptions

--- integration_test/_test.dart
+++ integration_test/_test.dart
@@ -1,8 +1,6 @@
 import 'package:bb_mobile/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'bull_bitcoin_user_datasource_test.dart'
-    as bull_bitcoin_user_datasource_test;
 import 'exchange_rate_test.dart' as exchange_rate_test;
 import 'payjoin_test.dart' as payjoin_test;
 import 'sqlite_transactions_test.dart' as sqlite_transactions_test;
@@ -17,6 +15,5 @@ void main() async {
     payjoin_test.main(isInitialized: true);
     sqlite_transactions_test.main(isInitialized: true);
     sqlite_wallet_metadata_test.main(isInitialized: true);
-    bull_bitcoin_user_datasource_test.main(isInitialized: true);
   });
 }
