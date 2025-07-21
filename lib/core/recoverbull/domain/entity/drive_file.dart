73aa0c1018858b2d5d8784e1e07038e6f3409c1d
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-07-21 08:24:59 -0400

    feat: add backupId getter to DriveFile for enhanced file identification

--- lib/core/recoverbull/domain/entity/drive_file.dart
+++ lib/core/recoverbull/domain/entity/drive_file.dart
@@ -1,11 +1,32 @@
+import 'package:hex/hex.dart';
+
 class DriveFile {
   final String id;
   final String name;
   final DateTime createdTime;
 
-  DriveFile({
-    required this.id,
-    required this.name,
-    required this.createdTime,
-  });
+  DriveFile({required this.id, required this.name, required this.createdTime});
+  String get backupId {
+    try {
+      final parts = name.split('_');
+      if (parts.length < 2) {
+        return name.replaceAll('.json', '');
+      }
+
+      final lastPart = parts.last;
+      final cleaned = lastPart
+          .replaceAll('.json', '')
+          .replaceAll(RegExp(r'[\[\]\s]'), '');
+
+      if (!RegExp(r'^[\d,]+$').hasMatch(cleaned)) {
+        return name.replaceAll('.json', '');
+      }
+
+      final intList =
+          cleaned.split(',').where((e) => e.isNotEmpty).map(int.parse).toList();
+      return HEX.encode(intList);
+    } catch (e) {
+      return name.replaceAll('.json', '');
+    }
+  }
 }
