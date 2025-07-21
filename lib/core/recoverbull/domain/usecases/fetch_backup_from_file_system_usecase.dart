e7b3f4eac95cc8ff482d3f0c9fd04a62954901f6
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-07-21 08:46:40 -0400

    chore: code cleanup

--- lib/core/recoverbull/domain/usecases/fetch_backup_from_file_system_usecase.dart
+++ lib/core/recoverbull/domain/usecases/fetch_backup_from_file_system_usecase.dart
@@ -1,15 +1,22 @@
 import 'dart:io';
 
+import 'package:bb_mobile/core/recoverbull/domain/entity/backup_info.dart';
+
 class FetchBackupFromFileSystemUsecase {
   FetchBackupFromFileSystemUsecase();
 
-  Future<String> execute(String filePath) async {
+  Future<BackupInfo> execute(String filePath) async {
     try {
       final backupFile = File(filePath);
       if (!await backupFile.exists()) {
         throw 'Backup file does not exist';
       }
-      return await backupFile.readAsString();
+      final backupContent = await backupFile.readAsString();
+      final backupInfo = BackupInfo(backupFile: backupContent);
+      if (backupInfo.isCorrupted) {
+        throw 'Backup file is corrupted';
+      }
+      return backupInfo;
     } catch (e) {
       throw Exception(e.toString());
     }
