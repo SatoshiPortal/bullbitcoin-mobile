9a3c3427194e3da606a87782dfda38d27dd15641
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-07-21 20:24:28 -0400

    feat: update RecoverWalletError with new error classes for wallet existence and mismatch scenarios

--- lib/core/recoverbull/domain/errors/recover_wallet_error.dart
+++ lib/core/recoverbull/domain/errors/recover_wallet_error.dart
@@ -1,3 +1,5 @@
+import 'package:bb_mobile/core/recoverbull/domain/entity/recoverbull_wallet.dart';
+
 class RecoverWalletError implements Exception {
   const RecoverWalletError(this.message);
 
@@ -9,14 +11,20 @@ class RecoverWalletError implements Exception {
   }
 }
 
-class TestFlowDefaultWalletAlreadyExistsError extends RecoverWalletError {
-  const TestFlowDefaultWalletAlreadyExistsError()
+class DefaultWalletExistsError extends RecoverWalletError {
+  final RecoverBullWallet wallet;
+  const DefaultWalletExistsError(this.wallet)
     : super('This wallet already exists.');
 }
 
-class TestFlowWalletMismatchError extends RecoverWalletError {
-  const TestFlowWalletMismatchError()
+class WalletMismatchError extends RecoverWalletError {
+  const WalletMismatchError()
     : super(
         'A different default wallet already exists. You can only have one default wallet.',
       );
+}
+
+class WalletAlreadyExistsError extends RecoverWalletError {
+  const WalletAlreadyExistsError()
+    : super('A wallet with this fingerprint already exists.');
 }
