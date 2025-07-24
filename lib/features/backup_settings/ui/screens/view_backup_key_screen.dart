18be842dd42a85dfdae986c78214a591c318d265
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-07-24 17:15:07 -0400

    refactor:  integrate CopyInput widget for better secrecy

--- lib/features/backup_settings/ui/screens/view_backup_key_screen.dart
+++ lib/features/backup_settings/ui/screens/view_backup_key_screen.dart
@@ -2,13 +2,14 @@ import 'dart:async';
 
 import 'package:bb_mobile/core/mixins/privacy_screen.dart';
 import 'package:bb_mobile/core/themes/app_theme.dart';
-import 'package:bb_mobile/core/widgets/buttons/button.dart';
+import 'package:bb_mobile/core/utils/logger.dart';
+import 'package:bb_mobile/core/widgets/inputs/copy_input.dart';
 import 'package:bb_mobile/core/widgets/loading/fading_linear_progress.dart';
 import 'package:bb_mobile/core/widgets/navbar/top_bar.dart';
+import 'package:bb_mobile/core/widgets/text/text.dart';
 import 'package:bb_mobile/features/backup_settings/presentation/cubit/backup_settings_cubit.dart';
 import 'package:bb_mobile/locator.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
@@ -55,11 +56,13 @@ class _Screen extends StatelessWidget {
     return BlocConsumer<BackupSettingsCubit, BackupSettingsState>(
       listenWhen:
           (previous, current) =>
-              previous.error != current.error && current.error != null,
+              previous.error != current.error && current.error != null ||
+              previous.derivedBackupKey != current.derivedBackupKey ||
+              previous.status != current.status,
       listener: (context, state) {
-        ScaffoldMessenger.of(
-          context,
-        ).showSnackBar(SnackBar(content: Text(state.error.toString())));
+        if (state.error != null) {
+          log.severe('Failed to derive backup key: ${state.error}');
+        }
       },
       builder: (context, state) {
         return Scaffold(
@@ -76,68 +79,55 @@ class _Screen extends StatelessWidget {
           body: SafeArea(
             child: Column(
               children: [
-                FadingLinearProgress(
-                  trigger: state.status == BackupSettingsStatus.viewingKey,
-                  backgroundColor: context.colour.surface,
-                  foregroundColor: context.colour.primary,
-                ),
-                const Spacer(),
+                if (state.status == BackupSettingsStatus.viewingKey) ...[
+                  FadingLinearProgress(
+                    trigger: state.status == BackupSettingsStatus.viewingKey,
+                    backgroundColor: context.colour.secondary,
+                    height: 3,
+                    foregroundColor: context.colour.primary,
+                  ),
+                ] else ...[
+                  const Spacer(),
+                  if (state.derivedBackupKey != null) ...[
+                    Padding(
+                      padding: const EdgeInsets.all(24),
+                      child: CopyInput(
+                        text:
+                            state.derivedBackupKey != null &&
+                                    state.derivedBackupKey!.length >= 6
+                                ? state.derivedBackupKey!.substring(0, 6) +
+                                    '*' * (state.derivedBackupKey!.length - 6)
+                                : '',
+                        canShowValueModal: true,
+                        maxLines: 1,
 
-                if (state.derivedBackupKey != null) ...[
-                  Padding(
-                    padding: const EdgeInsets.all(24),
-                    child: SelectableText(
-                      state.derivedBackupKey!,
+                        clipboardText: state.derivedBackupKey,
+                        overflow: TextOverflow.clip,
+                        modalContent:
+                            state.derivedBackupKey
+                                ?.replaceAllMapped(
+                                  RegExp('.{1,4}'),
+                                  (match) => '${match.group(0)} ',
+                                )
+                                .trim(),
+                      ),
+                    ),
+                  ],
+                  if (state.error != null) ...[
+                    Icon(
+                      Icons.error_outline_rounded,
+                      size: 48,
+                      color: context.colour.error,
+                    ),
+                    const Gap(16),
+                    BBText(
+                      'Failed to derive the backup key',
+                      style: context.font.headlineMedium,
                       textAlign: TextAlign.center,
-                      style: context.font.displaySmall,
                     ),
-                  ),
+                  ],
+                  const Spacer(),
                 ],
-                if (state.error != null) ...[
-                  Icon(
-                    Icons.error_outline,
-                    size: 64,
-                    color: context.colour.error,
-                  ),
-                  // error text
-                  const Gap(16),
-                  const Text(
-                    'Failed to derive backup key',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  const Gap(8),
-                  Text(
-                    state.error.toString(),
-                    style: TextStyle(color: context.colour.error, fontSize: 16),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-                const Spacer(),
-                // Bottom button
-                if (state.derivedBackupKey != null)
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      left: 24,
-                      right: 24,
-                      bottom: 48,
-                    ),
-                    child: BBButton.big(
-                      label: 'Copy to Clipboard',
-                      onPressed: () {
-                        Clipboard.setData(
-                          ClipboardData(text: state.derivedBackupKey!),
-                        );
-                      },
-                      bgColor: context.colour.secondary,
-
-                      textColor: context.colour.onPrimary,
-                    ),
-                  ),
               ],
             ),
           ),
