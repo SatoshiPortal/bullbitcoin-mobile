dc740bae8bdaf86a67f96ed7c9fa9c5561e7d086
Vishal Menon <vishalmenon.92@gmail.com>
2025-07-28 18:25:03 +0530

    Added files

---
+++ lib/features/sell/domain/sell_error.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'sell_error.freezed.dart';
+
+@freezed
+sealed class SellError with _$SellError {
+  const factory SellError.unauthenticated() = UnauthenticatedSellError;
+  const factory SellError.belowMinAmount({required int minAmountSat}) =
+      BelowMinAmountSellError;
+  const factory SellError.aboveMaxAmount({required int maxAmountSat}) =
+      AboveMaxAmountSellError;
+  const factory SellError.orderNotFound() = OrderNotFoundSellError;
+  const factory SellError.orderAlreadyConfirmed() =
+      OrderAlreadyConfirmedSellError;
+  const factory SellError.unexpected({required String message}) =
+      UnexpectedSellError;
+}
