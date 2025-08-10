b3b794bb4c64001426d552026a946a01666b0d54
Vishal Menon <vishalmenon.92@gmail.com>
2025-08-10 17:22:52 +0530

    Added better logs for Secure Storage View

--- lib/core/storage/domain/usecase/get_all_secure_storage_values_usecase.dart
+++ lib/core/storage/domain/usecase/get_all_secure_storage_values_usecase.dart
@@ -1,4 +1,5 @@
 import 'package:bb_mobile/core/storage/data/repository/secure_storage_repository.dart';
+import 'package:bb_mobile/core/utils/logger.dart';
 
 class GetAllSecureStorageValuesUsecase {
   final SecureStorageRepository _repository;
@@ -6,6 +7,15 @@ class GetAllSecureStorageValuesUsecase {
   GetAllSecureStorageValuesUsecase(this._repository);
 
   Future<Map<String, String>> call() async {
-    return await _repository.getAllKeyValues();
+    try {
+      final result = await _repository.getAllKeyValues();
+      return result;
+    } catch (error) {
+      log.severe(
+        'Error occurred while fetching secure storage values',
+        error: error,
+      );
+      rethrow;
+    }
   }
 }
