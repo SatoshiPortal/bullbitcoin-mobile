2f3ae6e22208610b8a456c49f7f7d53d3cf37153
Vishal Menon <vishalmenon.92@gmail.com>
2025-08-14 17:47:38 +0530

    Remove get seed from isolate

--- lib/core/seed/data/repository/seed_repository.dart
+++ lib/core/seed/data/repository/seed_repository.dart
@@ -1,10 +1,7 @@
+import 'dart:typed_data';
 import 'package:bb_mobile/core/seed/data/datasources/seed_datasource.dart';
 import 'package:bb_mobile/core/seed/data/models/seed_model.dart';
 import 'package:bb_mobile/core/seed/domain/entity/seed.dart';
-import 'package:bb_mobile/core/storage/data/datasources/key_value_storage/impl/secure_storage_data_source_impl.dart';
-import 'package:bb_mobile/core/storage/secure_storage.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
 
 class SeedRepository {
   final SeedDatasource _source;
@@ -30,34 +27,11 @@ class SeedRepository {
   }
 
   Future<Seed> get(String fingerprint) async {
-    final rootToken = RootIsolateToken.instance!;
-    return await compute(
-      _getSeedInIsolate,
-      _IsolateParams(fingerprint: fingerprint, rootToken: rootToken),
-    );
+    final model = await _source.get(fingerprint);
+    return model.toEntity();
   }
 
   Future<bool> exists(String fingerprint) => _source.exists(fingerprint);
 
   Future<void> delete(String fingerprint) => _source.delete(fingerprint);
-}
-
-class _IsolateParams {
-  final String fingerprint;
-  final RootIsolateToken rootToken;
-
-  _IsolateParams({required this.fingerprint, required this.rootToken});
-}
-
-Future<Seed> _getSeedInIsolate(_IsolateParams params) async {
-  try {
-    BackgroundIsolateBinaryMessenger.ensureInitialized(params.rootToken);
-    final secureStorage = SecureStorageDatasourceImpl(SecureStorage.init());
-    final seedDatasource = SeedDatasource(secureStorage: secureStorage);
-    final model = await seedDatasource.get(params.fingerprint);
-    return model.toEntity();
-  } catch (e) {
-    if (e is SeedNotFoundException) rethrow;
-    throw Exception('Failed to get seed in isolate: $e');
-  }
 }
