eba2e7fd7288a79374753c7a11d2ba874160829f
ethicnology <ethicnology@pm.me>
2025-09-15 14:33:26 -0400

    feat: add `CreateArkSecretUsecase`, `FetchArkSecretUsecase` and `GetArkWalletUsecase`

---
+++ lib/core/ark/locator.dart
@@ -0,0 +1,20 @@
+import 'package:bb_mobile/core/ark/usecases/create_ark_secret_usecase.dart';
+import 'package:bb_mobile/core/ark/usecases/fetch_ark_secret_usecase.dart';
+import 'package:bb_mobile/core/bip85/data/bip85_repository.dart';
+import 'package:bb_mobile/core/seed/domain/usecases/get_default_seed_usecase.dart';
+import 'package:bb_mobile/locator.dart';
+
+class ArkLocator {
+  static void setup() {
+    locator.registerFactory<CreateArkSecretUsecase>(
+      () => CreateArkSecretUsecase(bip85Repository: locator<Bip85Repository>()),
+    );
+
+    locator.registerFactory<FetchArkSecretUsecase>(
+      () => FetchArkSecretUsecase(
+        bip85Repository: locator<Bip85Repository>(),
+        getDefaultSeedUsecase: locator<GetDefaultSeedUsecase>(),
+      ),
+    );
+  }
+}
