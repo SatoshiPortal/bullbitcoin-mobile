ed820da935ef96beee5766b5f18146c206eb6f48
kumulynja <b@kumuly.dev>
2025-09-22 02:30:45 +0200

    setup utxo management feature structure

---
+++ lib/features/utxo_management/frameworks/di/utxo_management_locator.dart
@@ -0,0 +1,15 @@
+import 'package:bb_mobile/locator.dart';
+
+class UtxoManagementLocator {
+  static void setup() {
+    registerBlocs();
+  }
+
+  static void registerBlocs() {
+    /*locator.registerLazySingleton<UtxoManagementBloc>(
+      () => UtxoManagementBloc(
+        
+      ),
+    );*/
+  }
+}
