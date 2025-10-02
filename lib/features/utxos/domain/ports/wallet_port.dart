862bc8df49ecba3b2f327b950f43355de5721d54
kumulynja <b@kumuly.dev>
2025-10-02 19:32:28 +0200

    remove commented out code in WalletPort

--- lib/features/utxos/domain/ports/wallet_port.dart
+++ lib/features/utxos/domain/ports/wallet_port.dart
@@ -2,6 +2,4 @@ import 'package:bb_mobile/core/wallet/domain/entities/wallet.dart';
 
 abstract class WalletPort {
   Future<Wallet?> getWallet(String walletId);
-  //Future<List<Wallet>> getAllWallets();
-  //Future<void> getDefaultWallets();
 }
