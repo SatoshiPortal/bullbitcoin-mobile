a8075574feb48afac614a8ea47572ac5a7403566
ethicnology <ethicnology@pm.me>
2025-10-09 15:22:49 -0400

    fix: you must be super user to enable dev mode

--- lib/features/settings/ui/screens/app_settings/app_settings_screen.dart
+++ lib/features/settings/ui/screens/app_settings/app_settings_screen.dart
@@ -1,7 +1,9 @@
 import 'package:bb_mobile/core/widgets/settings_entry_item.dart';
+import 'package:bb_mobile/features/settings/presentation/bloc/settings_cubit.dart';
 import 'package:bb_mobile/features/settings/ui/settings_router.dart';
 import 'package:bb_mobile/features/settings/ui/widgets/dev_mode_switch.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class AppSettingsScreen extends StatelessWidget {
@@ -9,6 +11,10 @@ class AppSettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isSuperuser = context.select(
+      (SettingsCubit cubit) => cubit.state.isSuperuser ?? false,
+    );
+
     return Scaffold(
       appBar: AppBar(title: const Text('App Settings')),
       body: SafeArea(
@@ -24,11 +30,12 @@ class AppSettingsScreen extends StatelessWidget {
                     context.pushNamed(SettingsRoute.logs.name);
                   },
                 ),
-                SettingsEntryItem(
-                  icon: Icons.developer_mode,
-                  title: 'Dev Mode',
-                  onTap: () => DevModeSwitch.show(context),
-                ),
+                if (isSuperuser)
+                  SettingsEntryItem(
+                    icon: Icons.developer_mode,
+                    title: 'Dev Mode',
+                    onTap: () => DevModeSwitch.show(context),
+                  ),
               ],
             ),
           ),
