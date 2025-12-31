63eb680a5226ec8d21424d87b6125e9e221bb01a
Vishal Menon <vishalmenon.92@gmail.com>
2025-12-31 19:03:50 +0530

    fix: exchange landing page theme fix

--- lib/features/exchange/ui/screens/exchange_landing_screen_v2.dart
+++ lib/features/exchange/ui/screens/exchange_landing_screen_v2.dart
@@ -25,10 +25,11 @@ class ExchangeLandingScreenV2 extends StatelessWidget {
         context.goNamed(WalletRoute.walletHome.name);
       },
       child: Scaffold(
-        backgroundColor: context.appColors.secondary,
+        backgroundColor: context.appColors.secondaryFixed,
         appBar: AppBar(
+          backgroundColor: Colors.transparent,
           leading: BackButton(
-            color: context.appColors.onSecondary,
+            color: context.appColors.onSecondaryFixed,
             onPressed: () => context.goNamed(WalletRoute.walletHome.name),
           ),
         ),
@@ -56,7 +57,7 @@ class ExchangeLandingScreenV2 extends StatelessWidget {
                         BBText(
                           context.loc.exchangeBrandName,
                           style: AppFonts.textTitleTheme.textStyle.copyWith(
-                            color: context.appColors.onSecondary,
+                            color: context.appColors.onSecondaryFixed,
                             fontSize: 64,
                           ),
                         ),
@@ -64,7 +65,7 @@ class ExchangeLandingScreenV2 extends StatelessWidget {
                         BBText(
                           context.loc.exchangeLandingRecommendedExchange,
                           style: context.font.headlineSmall?.copyWith(
-                            color: context.appColors.onSecondary,
+                            color: context.appColors.onSecondaryFixed,
                           ),
                         ),
                       ],
@@ -87,42 +88,42 @@ class ExchangeLandingScreenV2 extends StatelessWidget {
                         BBText(
                           context.loc.exchangeFeatureSelfCustody,
                           style: context.font.bodyLarge?.copyWith(
-                            color: context.appColors.onSecondary,
+                            color: context.appColors.onSecondaryFixed,
                           ),
                         ),
                         const Gap(12),
                         BBText(
                           context.loc.exchangeFeatureDcaOrders,
                           style: context.font.bodyLarge?.copyWith(
-                            color: context.appColors.onSecondary,
+                            color: context.appColors.onSecondaryFixed,
                           ),
                         ),
                         const Gap(12),
                         BBText(
                           context.loc.exchangeFeatureSellBitcoin,
                           style: context.font.bodyLarge?.copyWith(
-                            color: context.appColors.onSecondary,
+                            color: context.appColors.onSecondaryFixed,
                           ),
                         ),
                         const Gap(12),
                         BBText(
                           context.loc.exchangeFeatureBankTransfers,
                           style: context.font.bodyLarge?.copyWith(
-                            color: context.appColors.onSecondary,
+                            color: context.appColors.onSecondaryFixed,
                           ),
                         ),
                         const Gap(12),
                         BBText(
                           context.loc.exchangeFeatureCustomerSupport,
                           style: context.font.bodyLarge?.copyWith(
-                            color: context.appColors.onSecondary,
+                            color: context.appColors.onSecondaryFixed,
                           ),
                         ),
                         const Gap(12),
                         BBText(
                           context.loc.exchangeFeatureUnifiedHistory,
                           style: context.font.bodyLarge?.copyWith(
-                            color: context.appColors.onSecondary,
+                            color: context.appColors.onSecondaryFixed,
                           ),
                         ),
                       ],
@@ -141,7 +142,7 @@ class ExchangeLandingScreenV2 extends StatelessWidget {
                       children: [
                         Icon(
                           Icons.info_outline,
-                          color: context.appColors.onSecondary,
+                          color: context.appColors.onSecondaryFixed,
                           size: 20,
                         ),
                         const Gap(8),
@@ -154,7 +155,7 @@ class ExchangeLandingScreenV2 extends StatelessWidget {
                                     .loc
                                     .exchangeLandingDisclaimerNotAvailable,
                                 style: context.font.bodySmall?.copyWith(
-                                  color: context.appColors.onSecondary,
+                                  color: context.appColors.onSecondaryFixed,
                                 ),
                               ),
                             ],
@@ -181,7 +182,7 @@ class ExchangeLandingScreenV2 extends StatelessWidget {
                         }
                       },
                       bgColor: context.appColors.primary,
-                      textColor: context.appColors.onSecondary,
+                      textColor: context.appColors.onSecondaryFixed,
                     ),
                   ),
                   const Gap(32),
