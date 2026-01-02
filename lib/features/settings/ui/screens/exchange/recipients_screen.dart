3bb5dc4e16b4631b0ff54f73b69020150f376e59
mocodesmo <morteza4048@gmail.com>
2026-01-02 15:40:43 +0530

    feat: update ExchangeRecipientsScreen to handle recipient selection
    - Added functionality to navigate to the PayRoute with the selected recipient when a recipient is chosen.
    - Introduced RecipientViewModel to facilitate passing recipient data during navigation.

--- lib/features/settings/ui/screens/exchange/recipients_screen.dart
+++ lib/features/settings/ui/screens/exchange/recipients_screen.dart
@@ -1,8 +1,11 @@
+import 'package:bb_mobile/features/pay/ui/pay_router.dart';
 import 'package:bb_mobile/features/recipients/frameworks/ui/screens/recipients_screen.dart';
 import 'package:bb_mobile/features/recipients/interface_adapters/presenters/bloc/recipients_bloc.dart';
+import 'package:bb_mobile/features/recipients/interface_adapters/presenters/models/recipient_view_model.dart';
 import 'package:bb_mobile/locator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 class ExchangeRecipientsScreen extends StatelessWidget {
   const ExchangeRecipientsScreen({super.key});
@@ -12,7 +15,9 @@ class ExchangeRecipientsScreen extends StatelessWidget {
     return BlocProvider<RecipientsBloc>(
       create: (context) => locator<RecipientsBloc>(
         param1: null,
-        param2: null,
+        param2: (RecipientViewModel recipient) async {
+          context.pushNamed(PayRoute.pay.name, extra: recipient);
+        },
       )..add(const RecipientsEvent.started()),
       child: const RecipientsScreen(),
     );
