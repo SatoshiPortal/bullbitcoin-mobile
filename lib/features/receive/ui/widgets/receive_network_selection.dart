8e0eaae07e12dab5f3087176b65e48e814c7cd44
Pablo Jiménez Pascual <pablo@jimpas.me>
2026-01-08 02:12:35 +0100

    fix: default receive behavior to Lightning for Liquid wallets (#1587)

--- lib/features/receive/ui/widgets/receive_network_selection.dart
+++ lib/features/receive/ui/widgets/receive_network_selection.dart
@@ -1,6 +1,8 @@
 import 'package:bb_mobile/core/utils/build_context_x.dart';
 import 'package:bb_mobile/core/wallet/domain/entities/wallet.dart';
 import 'package:bb_mobile/core/widgets/segment/segmented_full.dart';
+import 'package:bb_mobile/features/receive/domain/enums/receive_network_type.dart';
+import 'package:bb_mobile/features/receive/domain/extensions/wallet_receive_extensions.dart';
 import 'package:bb_mobile/features/receive/ui/receive_router.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -10,41 +12,37 @@ class ReceiveNetworkSelection extends StatelessWidget {
 
   final Wallet? wallet;
 
+  static const _routes = {
+    ReceiveNetworkType.bitcoin: ReceiveRoute.receiveBitcoin,
+    ReceiveNetworkType.lightning: ReceiveRoute.receiveLightning,
+    ReceiveNetworkType.liquid: ReceiveRoute.receiveLiquid,
+  };
+
   @override
   Widget build(BuildContext context) {
-    final bitcoin = context.loc.receiveBitcoin;
-    final lightning = context.loc.receiveLightning;
-    final liquid = context.loc.receiveLiquid;
+    final labelToType = {
+      for (final type in wallet.availableReceiveNetworks)
+        _label(context, type): type,
+    };
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: BBSegmentFull(
-        items:
-            wallet == null
-                ? {bitcoin, lightning, liquid}
-                : wallet!.isLiquid
-                ? !wallet!.signsLocally
-                    ? {liquid}
-                    : {lightning, liquid}
-                : !wallet!.signsLocally
-                ? {bitcoin}
-                : {bitcoin, lightning},
-        onSelected: (c) {
-          if (c == bitcoin) {
-            context.goNamed(ReceiveRoute.receiveBitcoin.name, extra: wallet);
-          } else if (c == lightning) {
-            context.goNamed(ReceiveRoute.receiveLightning.name, extra: wallet);
-          } else if (c == liquid) {
-            context.goNamed(ReceiveRoute.receiveLiquid.name, extra: wallet);
-          }
+        items: labelToType.keys.toSet(),
+        initialValue: _label(context, wallet.defaultReceiveNetwork),
+        onSelected: (label) {
+          final route = _routes[labelToType[label]]!;
+          context.goNamed(route.name, extra: wallet);
         },
-        initialValue:
-            wallet == null
-                ? bitcoin
-                : wallet!.isLiquid
-                ? liquid
-                : bitcoin,
       ),
     );
+  }
+
+  String _label(BuildContext context, ReceiveNetworkType type) {
+    return switch (type) {
+      ReceiveNetworkType.bitcoin => context.loc.receiveBitcoin,
+      ReceiveNetworkType.lightning => context.loc.receiveLightning,
+      ReceiveNetworkType.liquid => context.loc.receiveLiquid,
+    };
   }
 }
