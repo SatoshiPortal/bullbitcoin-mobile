bdff4d82de96617df05e681456aa3917ab26c921
kumulynja <b@kumuly.dev>
2026-01-12 15:19:01 +0100

    if data not available in secure storage with first_unlock_this_device, check if saved with unlocked policy and migrate on demand

--- lib/core/storage/data/datasources/key_value_storage/impl/secure_storage_data_source_impl.dart
+++ lib/core/storage/data/datasources/key_value_storage/impl/secure_storage_data_source_impl.dart
@@ -12,18 +12,79 @@ class SecureStorageDatasourceImpl implements KeyValueStorageDatasource<String> {
   }
 
   @override
-  Future<Map<String, String>> getAll() {
-    return _storage.readAll();
+  Future<Map<String, String>> getAll() async {
+    final firstUnlockThisDeviceReadResults = await _storage.readAll();
+
+    // The following is needed because we used to have no background processing needs
+    // but now we do, so some values may have been stored with more restrictive accessibility
+    // (unlocked) then the current default (first unlock this device).
+    if (firstUnlockThisDeviceReadResults.isNotEmpty) {
+      return firstUnlockThisDeviceReadResults;
+    }
+
+    final unlockedReadResults = await _storage.readAll(
+      iOptions: IOSOptions(accessibility: KeychainAccessibility.unlocked),
+    );
+    for (final entry in unlockedReadResults.entries) {
+      // Re-save the values with the default accessibility for future reads and
+      // reads from background processes.
+      await _storage.write(key: entry.key, value: entry.value);
+    }
+
+    return unlockedReadResults;
   }
 
   @override
-  Future<String?> getValue(String key) {
-    return _storage.read(key: key);
+  Future<String?> getValue(String key) async {
+    final firstUnlockThisDeviceReadResult = await _storage.read(key: key);
+
+    // The following is needed because we used to have no background processing needs
+    // but now we do, so some values may have been stored with more restrictive accessibility
+    // (unlocked) then the current default (first unlock this device).
+    if (firstUnlockThisDeviceReadResult != null) {
+      return firstUnlockThisDeviceReadResult;
+    }
+
+    final unlockedReadResult = await _storage.read(
+      key: key,
+      iOptions: IOSOptions(accessibility: KeychainAccessibility.unlocked),
+    );
+    if (unlockedReadResult != null) {
+      // Re-save the value with the default accessibility for future reads and
+      // reads from background processes.
+      await _storage.write(key: key, value: unlockedReadResult);
+    }
+    return unlockedReadResult;
   }
 
   @override
-  Future<bool> hasValue(String key) {
-    return _storage.containsKey(key: key);
+  Future<bool> hasValue(String key) async {
+    final firstUnlockThisDeviceCheck = await _storage.containsKey(key: key);
+
+    // The following is needed because we used to have no background processing needs
+    // but now we do, so some values may have been stored with more restrictive accessibility
+    // (unlocked) then the current default (first unlock this device).
+    if (firstUnlockThisDeviceCheck) {
+      return true;
+    }
+
+    final unlockedCheck = await _storage.containsKey(
+      key: key,
+      iOptions: IOSOptions(accessibility: KeychainAccessibility.unlocked),
+    );
+
+    if (unlockedCheck) {
+      final value = await _storage.read(
+        key: key,
+        iOptions: IOSOptions(accessibility: KeychainAccessibility.unlocked),
+      );
+      if (value != null) {
+        // Re-save the value with the default accessibility for future reads and
+        // reads from background processes.
+        await _storage.write(key: key, value: value);
+      }
+    }
+    return unlockedCheck;
   }
 
   @override
