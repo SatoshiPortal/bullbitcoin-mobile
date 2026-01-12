0b44c40417e345f9bd8fe7754303a51878c42ddd
mocodesmo <morteza4048@gmail.com>
2026-01-12 17:29:40 +0530

    Remove unused image_picker import from file_share_datasource.dart

--- lib/core/exchange/data/datasources/file_share_datasource.dart
+++ lib/core/exchange/data/datasources/file_share_datasource.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'dart:typed_data';
 
-import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
 
@@ -20,4 +19,3 @@ class FileShareDatasource {
     );
   }
 }
-
