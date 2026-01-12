9ae3d240afd49b1e1839b511c5bb0dd615f486e4
kumulynja <b@kumuly.dev>
2026-01-12 23:20:16 +0100

    add secrets view screen

--- lib/features/secrets/presentation/blocs/secrets_view_event.dart
+++ lib/features/secrets/presentation/blocs/secrets_view_event.dart
@@ -1,11 +1,15 @@
-abstract class SecretsViewEvent {}
+abstract class SecretsViewEvent {
+  const SecretsViewEvent();
+}
 
-class SecretsViewLoadRequested extends SecretsViewEvent {}
+class SecretsViewLoadRequested extends SecretsViewEvent {
+  const SecretsViewLoadRequested();
+}
 
 class SecretsViewDeleteRequested extends SecretsViewEvent {
   final String fingerprint;
 
-  SecretsViewDeleteRequested({required this.fingerprint});
+  const SecretsViewDeleteRequested({required this.fingerprint});
 }
 
 // TODO: Add event for deleting legacy seeds too
