15557a863f388191ff9e7d7d502a63d0e4f8aee4
ethicnology <ethicnology@pm.me>
2026-01-20 17:16:23 -0500

    refactor: Closes #1803

--- lib/features/labels/application/labels_repository_port.dart
+++ lib/features/labels/application/labels_repository_port.dart
@@ -1,9 +1,6 @@
-import 'package:bb_mobile/core/storage/sqlite_database.dart';
 import 'package:bb_mobile/features/labels/domain/label_entity.dart';
 
 abstract class LabelsRepositoryPort {
-  LabelsRepositoryPort({required SqliteDatabase database});
-
   Future<void> store(List<LabelEntity> labels);
 
   Future<List<LabelEntity>> fetchByLabel(String label);
