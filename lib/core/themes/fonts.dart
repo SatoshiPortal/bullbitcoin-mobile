11836b738e5f10e5c603119d1ba3638e8b6e1ce5
i5hi <vishalmenon.92@gmail.com>
2026-05-05 15:55:03 +0530

    feat: add local golos font

--- lib/core/themes/fonts.dart
+++ lib/core/themes/fonts.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class AppFonts {
   static const _textTheme = TextTheme(
@@ -53,8 +52,8 @@ class AppFonts {
   );
 
   static ({String fontFamily, TextTheme textTheme}) textTheme = (
-    fontFamily: 'Golos',
-    textTheme: GoogleFonts.golosTextTextTheme(_textTheme),
+    fontFamily: 'Golos Text',
+    textTheme: _textTheme,
   );
 
   static ({String fontFamily, TextStyle textStyle}) textTitleTheme = (
