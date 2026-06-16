990489220d7a052e765dbd9a76e033c91495c770
wired-pasteque <wired_pasteque+git@proton.me>
2026-06-16 10:17:32 +0200

    fix: extract lnurl from non-standard URIs using regex

--- lib/core/utils/payment_request.dart
+++ lib/core/utils/payment_request.dart
@@ -78,6 +78,14 @@ sealed class PaymentRequest with _$PaymentRequest {
         if (result != null) return result;
       }
 
+      final re = RegExp(r'lnurl[0-9a-z]+', caseSensitive: false);
+      final m = re.firstMatch(trimmed);
+
+      if (m != null) {
+        final result = await _tryParseLnAddress(m.group(0)!);
+        if (result != null) return result;
+      }
+
       if (trimmed.toLowerCase().startsWith('lnbc') ||
           trimmed.toLowerCase().startsWith('lntb') ||
           trimmed.toLowerCase().startsWith('lightning:')) {
