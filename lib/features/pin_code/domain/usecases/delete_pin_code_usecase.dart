6296977de4a08e001fb89014baf725a360feba63
wired-pasteque <wired_pasteque+git@proton.me>
2026-06-17 11:27:34 +0200

    refactor: sanitize user-facing error messages

--- lib/features/pin_code/domain/usecases/delete_pin_code_usecase.dart
+++ lib/features/pin_code/domain/usecases/delete_pin_code_usecase.dart
@@ -1,9 +1,10 @@
+import 'package:bb_mobile/core/utils/result.dart';
 import 'package:bb_mobile/features/pin_code/data/repositories/pin_code_repository.dart';
 
 class DeletePinCodeUsecase {
   final PinCodeRepository _pinCodeRepository;
-
   DeletePinCodeUsecase({required this._pinCodeRepository});
 
-  Future<void> execute() async => await _pinCodeRepository.deletePinCode();
+  Future<Result<Null>> execute() =>
+      _pinCodeRepository.deletePinCode();
 }
