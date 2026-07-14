590a73154f6e0041ea8804461ebe6fb49f5b10d8
Francis Pouliot <francis@satoshiportal.com>
2026-07-14 12:32:48 -0600

    fix(deterministic-wallets): require result handling

--- lib/features/deterministic_wallets/domain/repositories/deterministic_wallet_repository.dart
+++ lib/features/deterministic_wallets/domain/repositories/deterministic_wallet_repository.dart
@@ -2,32 +2,39 @@ import 'package:bb_mobile/core/utils/result.dart';
 import 'package:bb_mobile/features/deterministic_wallets/domain/deterministic_wallet_failure.dart';
 import 'package:bb_mobile/features/deterministic_wallets/domain/deterministic_wallet_seed_material.dart';
 import 'package:bb_mobile/features/deterministic_wallets/domain/deterministic_wallets.dart';
+import 'package:meta/meta.dart';
 
 abstract interface class DeterministicWalletRepository {
+  @useResult
   Future<Result<PreparedDeterministicWallet?, DeterministicWalletFailure>>
   getMatchingWallet({
     required DeterministicWalletSeedMaterial seedMaterial,
     required DeterministicWalletSpec spec,
   });
 
+  @useResult
   Future<Result<bool, DeterministicWalletFailure>> childSeedExists(
     String fingerprint,
   );
 
+  @useResult
   Future<Result<void, DeterministicWalletFailure>> storeChildSeed(
     DeterministicWalletSeedMaterial seedMaterial,
   );
 
+  @useResult
   Future<Result<PreparedDeterministicWallet, DeterministicWalletFailure>>
   createWallet({
     required DeterministicWalletSeedMaterial seedMaterial,
     required DeterministicWalletSpec spec,
   });
 
+  @useResult
   Future<Result<void, DeterministicWalletFailure>> deleteWallet(
     String walletId,
   );
 
+  @useResult
   Future<Result<void, DeterministicWalletFailure>> deleteChildSeed(
     String fingerprint,
   );
