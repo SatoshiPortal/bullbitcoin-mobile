fe8e92094b08e4932ff862b5ddb885719f629cab
wired-pasteque <wired_pasteque+git@proton.me>
2026-07-24 17:19:22 -0400

    refactor(onboarding): sanitize user-facing error messages

---
+++ lib/features/onboarding/domain/usecases/create_onboarding_wallet_usecase.dart
@@ -0,0 +1,27 @@
+import 'package:bb_mobile/core/utils/logger.dart';
+import 'package:bb_mobile/core/utils/result.dart';
+import 'package:bb_mobile/core/wallet/domain/entities/wallet.dart';
+import 'package:bb_mobile/core/wallet/domain/usecases/create_default_wallets_usecase.dart';
+import 'package:bb_mobile/features/onboarding/domain/onboarding_failure.dart';
+import 'package:meta/meta.dart';
+
+class CreateOnboardingWalletUsecase {
+  final CreateDefaultWalletsUsecase _createDefaultWalletsUsecase;
+
+  CreateOnboardingWalletUsecase({required this._createDefaultWalletsUsecase});
+
+  @useResult
+  Future<Result<List<Wallet>, OnboardingFailure>> execute() async {
+    try {
+      final wallets = await _createDefaultWalletsUsecase.execute();
+      return Ok(wallets);
+    } catch (e, st) {
+      log.severe(
+        message: 'Onboarding: default wallet creation failed',
+        error: e,
+        trace: st,
+      );
+      return const Err(OnboardingWalletCreationFailure());
+    }
+  }
+}
