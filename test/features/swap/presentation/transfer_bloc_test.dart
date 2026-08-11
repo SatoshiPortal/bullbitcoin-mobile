46a5e6fff5cd0737280f598f2a6a2f730b63a66e
ethicnology <ethicnology@pm.me>
2026-08-11 12:57:05 -0400

    test(swap): cover the transfer bloc broadcast path

---
+++ test/features/swap/presentation/transfer_bloc_test.dart
@@ -0,0 +1,208 @@
+import 'package:bb_mobile/core/blockchain/domain/usecases/broadcast_bitcoin_transaction_usecase.dart';
+import 'package:bb_mobile/core/blockchain/domain/usecases/broadcast_liquid_transaction_usecase.dart';
+import 'package:bb_mobile/core/exchange/domain/usecases/convert_sats_to_currency_amount_usecase.dart';
+import 'package:bb_mobile/core/fees/domain/get_network_fees_usecase.dart';
+import 'package:bb_mobile/core/settings/domain/get_settings_usecase.dart';
+import 'package:bb_mobile/core/swaps/domain/entity/swap.dart';
+import 'package:bb_mobile/core/swaps/domain/usecases/verify_chain_swap_amount_send_usecase.dart';
+import 'package:bb_mobile/core/entities/signer_entity.dart';
+import 'package:bb_mobile/core/settings/domain/settings_entity.dart';
+import 'package:bb_mobile/core/utils/result.dart';
+import 'package:bb_mobile/core/wallet/domain/entities/wallet.dart';
+import 'package:bb_mobile/core/wallet/domain/usecases/prepare_bitcoin_send_usecase.dart';
+import 'package:bb_mobile/core/wallet/domain/usecases/calculate_bitcoin_absolute_fees_usecase.dart';
+import 'package:bb_mobile/core/wallet/domain/usecases/check_liquid_consolidation_usecase.dart';
+import 'package:bb_mobile/core/wallet/domain/usecases/get_receive_address_usecase.dart';
+import 'package:bb_mobile/core/wallet/domain/usecases/get_wallet_usecase.dart';
+import 'package:bb_mobile/core/wallet/domain/usecases/get_wallet_utxos_usecase.dart';
+import 'package:bb_mobile/core/wallet/domain/usecases/get_wallets_usecase.dart';
+import 'package:bb_mobile/features/send/domain/usecases/calculate_liquid_absolute_fees_usecase.dart';
+import 'package:bb_mobile/features/send/domain/usecases/detect_bitcoin_string_usecase.dart';
+import 'package:bb_mobile/features/send/domain/usecases/prepare_liquid_send_usecase.dart';
+import 'package:bb_mobile/features/send/domain/usecases/preview_bitcoin_fee_presets_usecase.dart';
+import 'package:bb_mobile/features/send/domain/usecases/preview_bitcoin_fee_usecase.dart';
+import 'package:bb_mobile/features/send/domain/usecases/sign_bitcoin_tx_usecase.dart';
+import 'package:bb_mobile/features/send/domain/usecases/sign_liquid_tx_usecase.dart';
+import 'package:bb_mobile/features/swap/domain/entities/order_swap_network.dart';
+import 'package:bb_mobile/features/swap/domain/entities/order_swap.dart';
+import 'package:bb_mobile/features/swap/domain/entities/order_swap_record.dart';
+import 'package:bb_mobile/features/swap/domain/usecases/create_order_swap_usecase.dart';
+import 'package:bb_mobile/features/swap/domain/usecases/get_order_swap_quote_usecase.dart';
+import 'package:bb_mobile/features/swap/domain/usecases/mark_order_swap_broadcast_unknown_usecase.dart';
+import 'package:bb_mobile/features/swap/domain/usecases/mark_order_swap_payin_broadcast_usecase.dart';
+import 'package:bb_mobile/features/swap/domain/usecases/replace_prepared_order_swap_payin_usecase.dart';
+import 'package:bb_mobile/features/swap/domain/usecases/save_prepared_order_swap_payin_usecase.dart';
+import 'package:bb_mobile/features/swap/domain/usecases/watch_order_swap_usecase.dart';
+import 'package:bb_mobile/features/swap/presentation/transfer_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class _MockGetSettings extends Mock implements GetSettingsUsecase {}
+class _MockGetWallets extends Mock implements GetWalletsUsecase {}
+class _MockGetNetworkFees extends Mock implements GetNetworkFeesUsecase {}
+class _MockPrepareBitcoin extends Mock implements PrepareBitcoinSendUsecase {}
+class _MockPrepareLiquid extends Mock implements PrepareLiquidSendUsecase {}
+class _MockCalculateBitcoin extends Mock implements CalculateBitcoinAbsoluteFeesUsecase {}
+class _MockCalculateLiquid extends Mock implements CalculateLiquidAbsoluteFeesUsecase {}
+class _MockGetWallet extends Mock implements GetWalletUsecase {}
+class _MockSignBitcoin extends Mock implements SignBitcoinTxUsecase {}
+class _MockSignLiquid extends Mock implements SignLiquidTxUsecase {}
+class _MockBroadcastBitcoin extends Mock implements BroadcastBitcoinTransactionUsecase {}
+class _MockBroadcastLiquid extends Mock implements BroadcastLiquidTransactionUsecase {}
+class _MockVerifyChain extends Mock implements VerifyChainSwapAmountSendUsecase {}
+class _MockGetQuote extends Mock implements GetOrderSwapQuoteUsecase {}
+class _MockCreateOrder extends Mock implements CreateOrderSwapUsecase {}
+class _MockSavePrepared extends Mock implements SavePreparedOrderSwapPayinUsecase {}
+class _MockReplacePrepared extends Mock implements ReplacePreparedOrderSwapPayinUsecase {}
+class _MockMarkUnknown extends Mock implements MarkOrderSwapBroadcastUnknownUsecase {}
+class _MockMarkBroadcast extends Mock implements MarkOrderSwapPayinBroadcastUsecase {}
+class _MockWatchOrder extends Mock implements WatchOrderSwapUsecase {}
+class _MockDetectBitcoin extends Mock implements DetectBitcoinStringUsecase {}
+class _MockGetReceiveAddress extends Mock implements GetReceiveAddressUsecase {}
+class _MockGetUtxos extends Mock implements GetWalletUtxosUsecase {}
+class _MockConvertSats extends Mock implements ConvertSatsToCurrencyAmountUsecase {}
+class _MockPreviewFee extends Mock implements PreviewBitcoinFeeUsecase {}
+class _MockPreviewPresets extends Mock implements PreviewBitcoinFeePresetsUsecase {}
+class _MockCheckConsolidation extends Mock implements CheckLiquidConsolidationUsecase {}
+
+void main() {
+  late _MockMarkUnknown markUnknown;
+  late _MockMarkBroadcast markBroadcast;
+  late _MockBroadcastBitcoin broadcastBitcoin;
+  late _MockGetWallet getWallet;
+  late OrderSwapRecord prepared;
+  late TransferBloc bloc;
+
+  setUp(() {
+    markUnknown = _MockMarkUnknown();
+    markBroadcast = _MockMarkBroadcast();
+    broadcastBitcoin = _MockBroadcastBitcoin();
+    getWallet = _MockGetWallet();
+    prepared = _prepared();
+    when(() => getWallet.execute('wallet-1', sync: true))
+        .thenAnswer((_) async => _wallet());
+
+    bloc = TransferBloc(
+      getSettingsUsecase: _MockGetSettings(),
+      getWalletsUsecase: _MockGetWallets(),
+      getNetworkFeesUsecase: _MockGetNetworkFees(),
+      prepareBitcoinSendUsecase: _MockPrepareBitcoin(),
+      prepareLiquidSendUsecase: _MockPrepareLiquid(),
+      calculateBitcoinAbsoluteFeesUsecase: _MockCalculateBitcoin(),
+      calculateLiquidAbsoluteFeesUsecase: _MockCalculateLiquid(),
+      getWalletUsecase: getWallet,
+      signBitcoinTxUsecase: _MockSignBitcoin(),
+      signLiquidTxUsecase: _MockSignLiquid(),
+      broadcastBitcoinTxUsecase: broadcastBitcoin,
+      broadcastLiquidTxUsecase: _MockBroadcastLiquid(),
+      verifyChainSwapAmountSendUsecase: _MockVerifyChain(),
+      getOrderSwapQuoteUsecase: _MockGetQuote(),
+      createOrderSwapUsecase: _MockCreateOrder(),
+      savePreparedOrderSwapPayinUsecase: _MockSavePrepared(),
+      replacePreparedOrderSwapPayinUsecase: _MockReplacePrepared(),
+      markOrderSwapBroadcastUnknownUsecase: markUnknown,
+      markOrderSwapPayinBroadcastUsecase: markBroadcast,
+      watchOrderSwapUsecase: _MockWatchOrder(),
+      detectBitcoinStringUsecase: _MockDetectBitcoin(),
+      getReceiveAddressUsecase: _MockGetReceiveAddress(),
+      getWalletUtxosUsecase: _MockGetUtxos(),
+      convertSatsToCurrencyAmountUsecase: _MockConvertSats(),
+      previewBitcoinFeeUsecase: _MockPreviewFee(),
+      previewBitcoinFeePresetsUsecase: _MockPreviewPresets(),
+      checkLiquidConsolidationUsecase: _MockCheckConsolidation(),
+    );
+  });
+
+  tearDown(() => bloc.close());
+
+  test('broadcasts a prepared order swap and emits its transaction id', () async {
+    final broadcasting = _prepared(status: OrderSwapLocalStatus.broadcastUnknown);
+    final broadcasted = _prepared(
+      status: OrderSwapLocalStatus.payinBroadcast,
+      transactionId: 'txid-1',
+    );
+    when(() => markUnknown.execute('local-1')).thenAnswer((_) async => Ok(broadcasting));
+    when(() => broadcastBitcoin.execute('signed-psbt', isPsbt: true))
+        .thenAnswer((_) async => 'txid-1');
+    when(() => markBroadcast.execute(localId: 'local-1', transactionId: 'txid-1'))
+        .thenAnswer((_) async => Ok(broadcasted));
+    bloc.emit(TransferState(
+        orderSwap: prepared,
+        signedPsbt: 'signed-psbt',
+        fromWallet: _wallet(),
+        swap: _swap(),
+    ));
+    final states = <TransferState>[];
+    final subscription = bloc.stream.listen(states.add);
+    final completed = bloc.stream.firstWhere((state) => state.txId == 'txid-1');
+    bloc.add(const TransferEvent.confirmed());
+    await completed;
+    await subscription.cancel();
+    verify(() => markUnknown.execute('local-1')).called(1);
+    verify(() => broadcastBitcoin.execute('signed-psbt', isPsbt: true)).called(1);
+    verify(() => markBroadcast.execute(localId: 'local-1', transactionId: 'txid-1')).called(1);
+    expect(bloc.state.txId, 'txid-1');
+    expect(states.any((state) => state.txId == 'txid-1'), isTrue);
+  });
+
+  test('surfaces a broadcast exception in the current state', () async {
+    when(() => markUnknown.execute('local-1')).thenAnswer((_) async => Ok(prepared));
+    when(() => broadcastBitcoin.execute('signed-psbt', isPsbt: true))
+        .thenThrow(Exception('broadcast failed'));
+    bloc.emit(TransferState(
+      orderSwap: prepared, signedPsbt: 'signed-psbt', fromWallet: _wallet(), swap: _swap(),
+    ));
+    bloc.add(const TransferEvent.confirmed());
+    await Future<void>.delayed(Duration.zero);
+    await Future<void>.delayed(Duration.zero);
+    expect(bloc.state.confirmTransactionException, isNotNull);
+    expect(bloc.state.confirmTransactionException!.message, contains('broadcast failed'));
+  });
+}
+
+OrderSwapRecord _prepared({
+  OrderSwapLocalStatus status = OrderSwapLocalStatus.readyToBroadcast,
+  String? transactionId,
+}) => OrderSwapRecord(
+  localId: 'local-1',
+  purpose: OrderSwapPurpose.transfer,
+  environment: OrderSwapEnvironment.testnet,
+  inNetwork: OrderSwapNetwork.bitcoin,
+  outNetwork: OrderSwapNetwork.liquid,
+  isInAmountFixed: false,
+  requestedAmountSat: BigInt.from(1000),
+  sourceWalletId: 'wallet-1',
+  destination: 'destination',
+  fallback: 'fallback',
+  order: _order(),
+  createdAt: DateTime.utc(2026),
+  localStatus: status,
+  localPayinTransactionId: transactionId,
+  signedPayinTransaction: 'signed-psbt',
+  payinIsPsbt: true,
+);
+
+OrderSwap _order() => OrderSwap(
+  orderId: 'order-1', orderNumber: 1,
+  inNetwork: OrderSwapNetwork.bitcoin, outNetwork: OrderSwapNetwork.liquid,
+  payinAmountSat: BigInt.from(1010), payoutAmountSat: BigInt.from(1000),
+  payinCurrency: 'BTC', payoutCurrency: 'LBTC', payinMethod: 'Bitcoin',
+  payoutMethod: 'Liquid', orderType: 'Swap', orderStatus: 'Awaiting payment',
+  payinStatus: 'In progress', payoutStatus: 'Not started', messageCode: 'ORDER_CREATED',
+  liquidAddress: 'liquid-address', createdAt: DateTime.utc(2026),
+  confirmationDeadline: DateTime.utc(2026, 1, 1, 0, 5),
+);
+
+ChainSwap _swap() => Swap.chain(
+  id: 'swap-1', keyIndex: 0, type: SwapType.bitcoinToLiquid,
+  status: SwapStatus.pending, environment: Environment.testnet,
+  creationTime: DateTime.utc(2026), sendWalletId: 'wallet-1',
+  paymentAddress: 'payin-address', paymentAmount: 1000,
+) as ChainSwap;
+
+Wallet _wallet() => Wallet(
+  origin: 'wallet-1', network: Network.bitcoinTestnet, xpubFingerprint: 'fingerprint',
+  scriptType: ScriptType.bip84, xpub: 'xpub', externalPublicDescriptor: 'external',
+  internalPublicDescriptor: 'internal', signer: SignerEntity.local, signerDevice: null,
+  balanceSat: BigInt.from(2000),
+);
