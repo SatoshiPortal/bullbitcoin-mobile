c83c4487e80a158c8a3e5e4fe2665a4b4ecc6e80
benk10 <ben.kaufman10@gmail.com>
2026-08-23 21:57:05 -0500

    feat(bitbox): support registered wallet policies

--- lib/features/bitbox/ui/bitbox_router.dart
+++ lib/features/bitbox/ui/bitbox_router.dart
@@ -1,9 +1,11 @@
 import 'package:bb_mobile/features/bitbox/bitbox_action.dart';
 import 'package:bb_mobile/features/bitbox/ui/screens/bitbox_action_screen.dart';
+import 'package:bb_mobile/features/bitbox/public/bitbox_facade.dart';
 import 'package:go_router/go_router.dart';
 
 enum BitBoxRoute {
   importBitBox('/bitbox-import'),
+  bitboxRegisterWalletPolicy('/bitbox-register-wallet-policy'),
   bitboxSignTransaction('/bitbox-sign-transaction'),
   bitboxVerifyAddress('/bitbox-verify-address');
 
@@ -26,13 +28,26 @@ class BitBoxRouter {
       },
     ),
     GoRoute(
+      name: BitBoxRoute.bitboxRegisterWalletPolicy.name,
+      path: BitBoxRoute.bitboxRegisterWalletPolicy.path,
+      builder: (context, state) {
+        final request = state.extra as RegisterBitBoxWalletPolicyRequest;
+        return BitBoxActionScreen(
+          action: const BitBoxAction.registerWalletPolicy(),
+          parameters: BitBoxRouteParams(walletPolicy: request),
+        );
+      },
+    ),
+    GoRoute(
       name: BitBoxRoute.bitboxSignTransaction.name,
       path: BitBoxRoute.bitboxSignTransaction.path,
       builder: (context, state) {
-        final extra = state.extra as BitBoxRouteParams?;
+        final extra = state.extra;
         return BitBoxActionScreen(
           action: const BitBoxAction.signTransaction(),
-          parameters: extra,
+          parameters: extra is SignBitBoxWalletPolicyRequest
+              ? BitBoxRouteParams(walletPolicy: extra)
+              : extra as BitBoxRouteParams?,
         );
       },
     ),
@@ -40,10 +55,12 @@ class BitBoxRouter {
       name: BitBoxRoute.bitboxVerifyAddress.name,
       path: BitBoxRoute.bitboxVerifyAddress.path,
       builder: (context, state) {
-        final extra = state.extra as BitBoxRouteParams?;
+        final extra = state.extra;
         return BitBoxActionScreen(
           action: const BitBoxAction.verifyAddress(),
-          parameters: extra,
+          parameters: extra is VerifyBitBoxWalletPolicyAddressRequest
+              ? BitBoxRouteParams(walletPolicy: extra)
+              : extra as BitBoxRouteParams?,
         );
       },
     ),
