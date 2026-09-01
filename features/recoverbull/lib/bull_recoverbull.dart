9b7b4fc47aaca5f10613d8cbd00a91a69c11eb6d
ethicnology <ethicnology@pm.me>
2026-09-01 14:33:19 -0400

    refactor(recoverbull): make the public API explicit

--- features/recoverbull/lib/bull_recoverbull.dart
+++ features/recoverbull/lib/bull_recoverbull.dart
@@ -3,31 +3,36 @@ library;
 export 'src/public/recoverbull.dart'
     show
         RecoverBullConfig,
-        RecoverBullDependencies,
         RecoverBullTiming,
         RecoverBullStatus,
         RecoverBullLifecycle,
         RecoverBullHealth,
+        RecoverBullServerSettings,
         RecoverBullRecoveryResult,
         RecoverBullAttemptMonitoringController,
         RecoverBullAttemptAlert,
         RecoverBullAttemptAlertKind,
         recoverBullDefaultServerUrl;
-export 'src/domain/entities/recoverbull_network.dart';
-export 'src/domain/entities/recoverbull_tor_settings.dart';
-export 'src/domain/entities/recoverbull_wallet.dart';
-export 'src/domain/entities/recoverbull_seed_material.dart';
-export 'src/domain/recoverbull_settings_port.dart';
-export 'src/domain/recoverbull_seed_port.dart';
-export 'src/domain/recoverbull_default_wallets_port.dart';
-export 'src/domain/recoverbull_lifecycle_port.dart';
-export 'src/domain/repositories/recoverbull_wallet_repository.dart';
-export 'src/domain/entities/encrypted_vault.dart';
-export 'generated/l10n/recoverbull_localizations.dart';
-export 'src/router/flow_type.dart';
+export 'src/domain/entities/recoverbull_network.dart' show RecoverBullNetwork;
+export 'src/domain/entities/recoverbull_tor_settings.dart'
+    show RecoverBullTorSettings;
+export 'src/domain/entities/recoverbull_wallet.dart' show RecoverBullWallet;
+export 'src/domain/entities/recoverbull_seed_material.dart'
+    show RecoverBullSeedMaterial;
+export 'src/domain/recoverbull_settings_port.dart' show RecoverBullSettingsPort;
+export 'src/domain/recoverbull_seed_port.dart' show RecoverBullSeedPort;
+export 'src/domain/recoverbull_default_wallets_port.dart'
+    show RecoverBullDefaultWalletsPort;
+export 'src/domain/recoverbull_lifecycle_port.dart'
+    show RecoverBullLifecyclePort;
+export 'src/domain/repositories/recoverbull_wallet_repository.dart'
+    show RecoverBullWalletRepository;
+export 'src/domain/entities/encrypted_vault.dart' show EncryptedVault;
+export 'generated/l10n/recoverbull_localizations.dart'
+    show RecoverBullLocalizations;
+export 'src/router/flow_type.dart' show RecoverBullFlow;
 export 'src/router/recoverbull_router.dart'
     show RecoverBullRoute, RecoverBullFlowsExtra, openRecoverBullFlow;
-export 'src/router/recoverbull_flow.dart' show RecoverBullFlowNavigator;
 export 'src/google_drive/recoverbull_google_drive_router.dart'
     show RecoverBullGoogleDriveRoute;
 export 'src/composition/recoverbull_composition.dart' show RecoverBullFeature;
